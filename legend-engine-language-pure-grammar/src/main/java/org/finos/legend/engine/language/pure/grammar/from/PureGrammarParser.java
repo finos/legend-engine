b94dddc193342883b37b42ab2959b69a135de603
Kevin Knight <kevin.knight@gs.com>
2021-02-17 15:54:22 -0500

    Undeprecate factory method for PureGrammarParser

--- legend-engine-language-pure-grammar/src/main/java/org/finos/legend/engine/language/pure/grammar/from/PureGrammarParser.java
+++ legend-engine-language-pure-grammar/src/main/java/org/finos/legend/engine/language/pure/grammar/from/PureGrammarParser.java
@@ -67,7 +67,6 @@ public class PureGrammarParser
         return new PureGrammarParser(extensions);
     }
 
-    @Deprecated
     public static PureGrammarParser newInstance()
     {
         return new PureGrammarParser(PureGrammarParserExtensions.fromAvailableExtensions());
