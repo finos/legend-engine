b538427021e50f99a914d058a8c3743fa7100210
Kevin Knight <57677197+kevin-m-knight-gs@users.noreply.github.com>
2021-02-18 18:46:17 -0500

    Generalize ObjectMapper factories and undeprecate PureGrammarParser factory method (#128)
    * Undeprecate factory method for PureGrammarParser
    
    * Generalize ObjectMapper factories to handle configure subtypes of ObjectMapper

--- legend-engine-protocol-pure/src/main/java/org/finos/legend/engine/protocol/pure/v1/PureProtocolObjectMapperFactory.java
+++ legend-engine-protocol-pure/src/main/java/org/finos/legend/engine/protocol/pure/v1/PureProtocolObjectMapperFactory.java
@@ -17,58 +17,54 @@ package org.finos.legend.engine.protocol.pure.v1;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.NamedType;
 import org.eclipse.collections.api.block.function.Function0;
-import org.eclipse.collections.impl.utility.ListIterate;
+import org.eclipse.collections.api.factory.Maps;
+import org.eclipse.collections.impl.utility.LazyIterate;
 import org.finos.legend.engine.protocol.pure.v1.extension.ProtocolSubTypeInfo;
 import org.finos.legend.engine.protocol.pure.v1.extension.PureProtocolExtension;
 import org.finos.legend.engine.protocol.pure.v1.extension.PureProtocolExtensionLoader;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class PureProtocolObjectMapperFactory
 {
-    public static ObjectMapper withPureProtocolExtensions(ObjectMapper objectMapper)
+    public static <T extends ObjectMapper> T withPureProtocolExtensions(T objectMapper)
     {
         PureProtocolExtensionLoader.logExtensionList();
         // register sub types
-        Map<ProtocolSubTypeInfo<?>, PureProtocolExtension> protocolSubTypeInfoByExtension = new HashMap<>();
-        Map<Class<?>, ProtocolSubTypeInfo<?>> superTypesWithDefaultRegisteredSubtype = new HashMap<>();
-        Map<Class<?>, ProtocolSubTypeInfo<?>> registeredSubTypes = new HashMap<>();
+        Map<ProtocolSubTypeInfo<?>, PureProtocolExtension> protocolSubTypeInfoByExtension = Maps.mutable.empty();
+        Map<Class<?>, ProtocolSubTypeInfo<?>> superTypesWithDefaultRegisteredSubtype = Maps.mutable.empty();
+        Map<Class<?>, ProtocolSubTypeInfo<?>> registeredSubTypes = Maps.mutable.empty();
         PureProtocolExtensionLoader.extensions().forEach(extension ->
-        {
-            List<ProtocolSubTypeInfo<?>> protocolSubTypeInfos = ListIterate.flatCollect(extension.getExtraProtocolSubTypeInfoCollectors(), Function0::value);
-            protocolSubTypeInfos.forEach(info ->
-            {
-                protocolSubTypeInfoByExtension.put(info, extension);
-                // register default subtype if specified
-                if (info.getDefaultSubType() != null)
+                LazyIterate.flatCollect(extension.getExtraProtocolSubTypeInfoCollectors(), Function0::value).forEach(info ->
                 {
-                    // verify that the default sub type is not already been registered by another extension, this will cause unexpected behavior
-                    if (superTypesWithDefaultRegisteredSubtype.containsKey(info.getSuperType()))
+                    protocolSubTypeInfoByExtension.put(info, extension);
+                    // register default subtype if specified
+                    if (info.getDefaultSubType() != null)
                     {
-                        ProtocolSubTypeInfo<?> found = superTypesWithDefaultRegisteredSubtype.get(info.getSuperType());
-                        throw new RuntimeException("Can't register default sub type '" + info.getDefaultSubType().getSimpleName() + "' for class '" + info.getSuperType().getSimpleName() + "' in extension '" + extension.getClass().getSimpleName() +
-                                "'. The default sub type for this class has already been registered as class '" + found.getDefaultSubType().getSimpleName() + "' by extension '" + protocolSubTypeInfoByExtension.get(found).getClass().getSimpleName() + "'");
+                        // verify that the default sub type is not already been registered by another extension, this will cause unexpected behavior
+                        if (superTypesWithDefaultRegisteredSubtype.containsKey(info.getSuperType()))
+                        {
+                            ProtocolSubTypeInfo<?> found = superTypesWithDefaultRegisteredSubtype.get(info.getSuperType());
+                            throw new RuntimeException("Can't register default sub type '" + info.getDefaultSubType().getSimpleName() + "' for class '" + info.getSuperType().getSimpleName() + "' in extension '" + extension.getClass().getSimpleName() +
+                                    "'. The default sub type for this class has already been registered as class '" + found.getDefaultSubType().getSimpleName() + "' by extension '" + protocolSubTypeInfoByExtension.get(found).getClass().getSimpleName() + "'");
+                        }
+                        superTypesWithDefaultRegisteredSubtype.put(info.getSuperType(), info);
+                        objectMapper.registerModule(info.registerDefaultSubType());
                     }
-                    superTypesWithDefaultRegisteredSubtype.put(info.getSuperType(), info);
-                    objectMapper.registerModule(info.registerDefaultSubType());
-                }
-                // register sub types by type ID
-                info.getSubTypes().forEach(subType ->
-                {
-                    // verify that the default sub type is not already been registered by another extension, this will cause unexpected behavior
-                    if (registeredSubTypes.containsKey(subType.getOne()))
+                    // register sub types by type ID
+                    info.getSubTypes().forEach(subType ->
                     {
-                        ProtocolSubTypeInfo<?> found = registeredSubTypes.get(subType.getOne());
-                        throw new RuntimeException("Can't register sub type '" + subType.getOne().getSimpleName() + "' for class '" + info.getSuperType().getSimpleName() + "' in extension '" + extension.getClass().getSimpleName() +
-                                "'. This sub type has already been registered by extension '" + protocolSubTypeInfoByExtension.get(found).getClass().getSimpleName() + "'");
-                    }
-                    registeredSubTypes.put(subType.getOne(), info);
-                    objectMapper.registerSubtypes(new NamedType(subType.getOne(), subType.getTwo()));
-                });
-            });
-        });
+                        // verify that the default sub type is not already been registered by another extension, this will cause unexpected behavior
+                        if (registeredSubTypes.containsKey(subType.getOne()))
+                        {
+                            ProtocolSubTypeInfo<?> found = registeredSubTypes.get(subType.getOne());
+                            throw new RuntimeException("Can't register sub type '" + subType.getOne().getSimpleName() + "' for class '" + info.getSuperType().getSimpleName() + "' in extension '" + extension.getClass().getSimpleName() +
+                                    "'. This sub type has already been registered by extension '" + protocolSubTypeInfoByExtension.get(found).getClass().getSimpleName() + "'");
+                        }
+                        registeredSubTypes.put(subType.getOne(), info);
+                        objectMapper.registerSubtypes(new NamedType(subType.getOne(), subType.getTwo()));
+                    });
+                }));
         return objectMapper;
     }
 
