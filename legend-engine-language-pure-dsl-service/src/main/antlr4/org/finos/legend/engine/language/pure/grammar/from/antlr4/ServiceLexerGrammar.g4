d940b42119b930829870d0592ebc955ff18c91fc
Mauricio Uyaguari <lmauricio12@gmail.com>
2021-03-10 17:24:13 -0500

    add dsl service (#153)
    * fixes to generation extensions
    
    * add dsl service

---
+++ legend-engine-language-pure-dsl-service/src/main/antlr4/org/finos/legend/engine/language/pure/grammar/from/antlr4/ServiceLexerGrammar.g4
@@ -0,0 +1,29 @@
+lexer grammar ServiceLexerGrammar;
+
+import M3LexerGrammar;
+
+
+// -------------------------------------- KEYWORD --------------------------------------
+
+SERVICE:                            'Service';
+IMPORT:                             'import';
+
+SERVICE_SINGLE:                     'Single';
+SERVICE_MULTI:                      'Multi';
+
+SERVICE_PATTERN:                    'pattern';
+SERVICE_OWNERS:                     'owners';
+SERVICE_DOCUMENTATION:              'documentation';
+SERVICE_AUTO_ACTIVATE_UPDATES:      'autoActivateUpdates';
+
+SERVICE_EXECUTION:                  'execution';
+SERVICE_EXECUTION_EXECUTIONS:       'executions';
+SERVICE_FUNCTION:                   'query';
+SERVICE_EXECUTION_KEY:              'key';
+SERVICE_MAPPING:                    'mapping';
+SERVICE_RUNTIME:                    'runtime';
+
+SERVICE_TEST:                       'test';
+SERVICE_TEST_TESTS:                 'tests';
+SERVICE_DATA:                       'data';
+SERVICE_ASSERTS:                    'asserts';
---
+++ legend-engine-language-pure-dsl-service/src/main/java/org/finos/legend/engine/protocol/pure/v1/model/packageableElement/service/PureSingleExecution.java
@@ -0,0 +1,26 @@
+// Copyright 2021 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.protocol.pure.v1.model.packageableElement.service;
+
+import org.finos.legend.engine.protocol.pure.v1.model.SourceInformation;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.runtime.Runtime;
+
+public class PureSingleExecution extends PureExecution
+{
+    // TODO: maybe it's better to have MappingPointer (with source information) instead of just the mapping string here
+    public String mapping;
+    public Runtime runtime;
+    public SourceInformation mappingSourceInformation;
+}
