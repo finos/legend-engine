452fd23b19754aac05a408b80b3105d99add5c8b
pierredebelen <pierre.debelen@gmail.com>
2021-05-10 13:31:32 -0400

    Fix dependencies so that execution doesn't have transitive dependencies to generation

--- legend-engine-language-pure-store-relational/src/test/java/org/finos/legend/engine/language/pure/grammar/test/TestRelationalGrammarComposer.java
+++ legend-engine-language-pure-store-relational/src/test/java/org/finos/legend/engine/language/pure/grammar/test/TestRelationalGrammarComposer.java
@@ -12,49 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.finos.legend.engine.protocol.pure.v1.test;
+package org.finos.legend.engine.language.pure.grammar.test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.finos.legend.engine.language.pure.grammar.to.PureGrammarComposer;
 import org.finos.legend.engine.language.pure.grammar.to.PureGrammarComposerContext;
 import org.finos.legend.engine.protocol.pure.v1.PureProtocolObjectMapperFactory;
 import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
-import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.domain.Class;
-import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.mapping.Mapping;
-import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.Store;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Objects;
 
-public class TestLoadRelationalModelAndMapping
+public class TestRelationalGrammarComposer
 {
     private static final ObjectMapper objectMapper = PureProtocolObjectMapperFactory.getNewObjectMapper();
 
     @Test
-    public void selfJoinTest() throws Exception 
+    public void selfJoinTest() throws Exception
     {
         PureModelContextData context = objectMapper.readValue(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("selfJoinTest.json")), PureModelContextData.class);
         PureGrammarComposer grammarTransformer = PureGrammarComposer.newInstance(PureGrammarComposerContext.Builder.newInstance().build());
         String formatted = grammarTransformer.renderPureModelContextData(context);
-        
+
         // Test checks that we do not output schema name before {target} - the grammar round trip tests do not exercise this code path as the graph
-        // produced for the roundtrip always sets the schema name in the table alias to default. 
+        // produced for the roundtrip always sets the schema name in the table alias to default.
         // This is testing out a graph as would be produced by tools such as Studio and checking that syntactically valid Pure is produced for self-joins
-        String expected = 
-                "###Relational\n" +  
-                "Database test::db\n" + "(\n" + 
-                "  Schema mySchema\n" +
-                "  (\n" + 
-                "    Table table1\n"+
-                "    (\n"+
-                "      col1 CHAR(32) PRIMARY KEY,\n"+
-                "      col2 CHAR(32)\n"+
-                "    )\n"+
-                "  )\n"+
-                "\n"+ 
-                "  Join selfJoin(mySchema.table1.col1 = {target}.col2)\n" +
-                ")\n";
+        String expected =
+                "###Relational\n" +
+                        "Database test::db\n" + "(\n" +
+                        "  Schema mySchema\n" +
+                        "  (\n" +
+                        "    Table table1\n" +
+                        "    (\n" +
+                        "      col1 CHAR(32) PRIMARY KEY,\n" +
+                        "      col2 CHAR(32)\n" +
+                        "    )\n" +
+                        "  )\n" +
+                        "\n" +
+                        "  Join selfJoin(mySchema.table1.col1 = {target}.col2)\n" +
+                        ")\n";
 
         Assert.assertEquals(expected, formatted);
     }
