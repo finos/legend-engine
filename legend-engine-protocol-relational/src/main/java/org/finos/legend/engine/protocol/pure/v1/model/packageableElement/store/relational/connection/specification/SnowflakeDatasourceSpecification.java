f873ac8241022ff7275498a8e17b85674424b95b
Hardik Maheshwari <19693874+hardikmaheshwari@users.noreply.github.com>
2021-05-27 20:16:09 +0530

    Revert "added support for snowflake proxy and vps and role (#210)" and related changes (#243)
    * Revert "added support for snowflake proxy and vps and role (#210)"
    
    This reverts commit f44376f9
    
    * add accountType back as service serialized with vX_X_X would have accountType in plan json

--- legend-engine-protocol-relational/src/main/java/org/finos/legend/engine/protocol/pure/v1/model/packageableElement/store/relational/connection/specification/SnowflakeDatasourceSpecification.java
+++ legend-engine-protocol-relational/src/main/java/org/finos/legend/engine/protocol/pure/v1/model/packageableElement/store/relational/connection/specification/SnowflakeDatasourceSpecification.java
@@ -3,19 +3,11 @@ package org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.
 public class SnowflakeDatasourceSpecification extends DatasourceSpecification
 {
     public String accountName;
+    public String accountType;
+
     public String region;
     public String warehouseName;
     public String databaseName;
-
-    public String role;
-
-    public String proxyHost;
-    public String proxyPort;
-    public String nonProxyHosts;
-
-    public String accountType;
-    public String organization;
-    public String cloudType;
 
     @Override
     public <T> T accept(DatasourceSpecificationVisitor<T> datasourceSpecificationVisitor)
