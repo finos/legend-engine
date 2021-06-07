e0881f868a2f4a8925ad852bee43212c659e0bdf
Mauricio Uyaguari <lmauricio12@gmail.com>
2021-06-07 14:56:17 -0400

    fix extensions collection package (#264)

--- legend-engine-extensions-collection-generation/src/test/java/org/finos/legend/engine/extensions/collection/generation/TestExtensions.java
+++ legend-engine-extensions-collection-generation/src/test/java/org/finos/legend/engine/extensions/collection/generation/TestExtensions.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.finos.legend.engine.extension.collection.generation;
+package org.finos.legend.engine.extensions.collection.generation;
 
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.ImmutableList;
