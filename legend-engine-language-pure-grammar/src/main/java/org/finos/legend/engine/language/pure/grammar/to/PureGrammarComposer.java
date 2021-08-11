c0d19e59b2bc8a4726cb96f1d4f21a206ea5f2f7
An Phi <aphi@skidmore.edu>
2021-08-11 17:03:44 -0400

    Make sure grammar composer print elements in order when there is no section index

--- legend-engine-language-pure-grammar/src/main/java/org/finos/legend/engine/language/pure/grammar/to/PureGrammarComposer.java
+++ legend-engine-language-pure-grammar/src/main/java/org/finos/legend/engine/language/pure/grammar/to/PureGrammarComposer.java
@@ -17,7 +17,6 @@ package org.finos.legend.engine.language.pure.grammar.to;
 import org.eclipse.collections.api.block.function.Function3;
 import org.eclipse.collections.api.block.predicate.Predicate;
 import org.eclipse.collections.api.factory.Lists;
-import org.eclipse.collections.api.factory.Maps;
 import org.eclipse.collections.api.factory.Sets;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.impl.utility.LazyIterate;
@@ -43,6 +42,7 @@ import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.section
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.section.SectionIndex;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -70,7 +70,9 @@ public class PureGrammarComposer
         MutableList<String> composedSections = Lists.mutable.empty();
         if (ListIterate.anySatisfy(elements, e -> e instanceof SectionIndex))
         {
-            Map<String, PackageableElement> elementByPath = Maps.mutable.empty();
+            // NOTE: we use a linked hash map to ensure the order in the case section index is not present, we keep the order
+            // of element as we see them in the model context data
+            Map<String, PackageableElement> elementByPath = new LinkedHashMap<>();
             // NOTE: here we handle duplication, first element with the duplicated path wins
             elements.forEach(element -> elementByPath.putIfAbsent(element.getPath(), element));
             LazyIterate.selectInstancesOf(elements, SectionIndex.class).forEach(sectionIndex -> this.renderSectionIndex(sectionIndex, elementByPath, elementsToCompose, composedSections));
@@ -89,7 +91,7 @@ public class PureGrammarComposer
 
         // FIXME: the following logic should be removed completely when we move to use extensions
         Predicate<PackageableElement> isDomainElement = e ->
-                (e instanceof Class) ||
+            (e instanceof Class) ||
                 (e instanceof Association) ||
                 (e instanceof Enumeration) ||
                 (e instanceof Function) ||
@@ -143,10 +145,10 @@ public class PureGrammarComposer
             if (!elements.isEmpty())
             {
                 builder.append(this.context.extraSectionComposers.stream().map(composer -> composer.value(elements, this.context, section.parserName)).filter(Objects::nonNull).findFirst()
-                        // NOTE: this is the old way (no-plugin) way to render section elements, this approach is not great since it does not enforce
-                        // the types of elements a section can have, the newer approach does the check and compose unsupported message when such violations occur
-                        // TO BE REMOVED when we moved everything to extensions
-                        .orElseGet(() -> LazyIterate.collect(elements, this::DEPRECATED_renderElement).makeString("\n\n")));
+                    // NOTE: this is the old way (no-plugin) way to render section elements, this approach is not great since it does not enforce
+                    // the types of elements a section can have, the newer approach does the check and compose unsupported message when such violations occur
+                    // TO BE REMOVED when we moved everything to extensions
+                    .orElseGet(() -> LazyIterate.collect(elements, this::DEPRECATED_renderElement).makeString("\n\n")));
                 builder.append("\n");
             }
             composedSections.add(builder.toString());
