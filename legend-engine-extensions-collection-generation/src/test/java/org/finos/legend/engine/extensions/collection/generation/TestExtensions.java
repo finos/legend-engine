14043de1d9c9c19af7efc8610b383420e2b3ad42
An Phi <aphi@skidmore.edu>
2021-09-14 16:35:14 -0400

    add DSL DataSpace (#369)

--- legend-engine-extensions-collection-generation/src/test/java/org/finos/legend/engine/extensions/collection/generation/TestExtensions.java
+++ legend-engine-extensions-collection-generation/src/test/java/org/finos/legend/engine/extensions/collection/generation/TestExtensions.java
@@ -43,6 +43,7 @@ public class TestExtensions
             .with(org.finos.legend.engine.protocol.pure.v1.RelationalProtocolExtension.class)
             .with(org.finos.legend.engine.protocol.pure.v1.ServiceProtocolExtension.class)
             .with(org.finos.legend.engine.protocol.pure.v1.TextProtocolExtension.class)
+            .with(org.finos.legend.engine.protocol.pure.v1.DataSpaceProtocolExtension.class)
             .toImmutable();
 
     // DO NOT DELETE ITEMS FROM THIS LIST (except when replacing them with something equivalent)
