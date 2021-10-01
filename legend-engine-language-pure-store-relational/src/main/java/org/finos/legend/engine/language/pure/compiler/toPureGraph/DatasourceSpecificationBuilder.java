dbabdf46f031b2dfd41222dd77fb2176e97ec16f
Elena Lopez-Castro <Elena.Lopez-Castro@ln.email.gs.com>
2021-10-01 09:19:11 +0100

    snowflake added support for proxy

--- legend-engine-language-pure-store-relational/src/main/java/org/finos/legend/engine/language/pure/compiler/toPureGraph/DatasourceSpecificationBuilder.java
+++ legend-engine-language-pure-store-relational/src/main/java/org/finos/legend/engine/language/pure/compiler/toPureGraph/DatasourceSpecificationBuilder.java
@@ -15,8 +15,24 @@
 package org.finos.legend.engine.language.pure.compiler.toPureGraph;
 
 import org.eclipse.collections.impl.list.mutable.FastList;
-import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.*;
-import org.finos.legend.pure.generated.*;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.BigQueryDatasourceSpecification;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.DatasourceSpecification;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.DatasourceSpecificationVisitor;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.EmbeddedH2DatasourceSpecification;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.LocalH2DatasourceSpecification;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.SnowflakeDatasourceSpecification;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.StaticDatasourceSpecification;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_BigQueryDatasourceSpecification;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_BigQueryDatasourceSpecification_Impl;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_DatasourceSpecification;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_EmbeddedH2DatasourceSpecification;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_EmbeddedH2DatasourceSpecification_Impl;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_LocalH2DatasourceSpecification;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_LocalH2DatasourceSpecification_Impl;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_SnowflakeDatasourceSpecification;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_SnowflakeDatasourceSpecification_Impl;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_StaticDatasourceSpecification;
+import org.finos.legend.pure.generated.Root_meta_pure_alloy_connections_alloy_specification_StaticDatasourceSpecification_Impl;
 
 public class DatasourceSpecificationBuilder implements DatasourceSpecificationVisitor<Root_meta_pure_alloy_connections_alloy_specification_DatasourceSpecification>
 {
@@ -32,33 +48,33 @@ public class DatasourceSpecificationBuilder implements DatasourceSpecificationVi
     {
         if (datasourceSpecification instanceof LocalH2DatasourceSpecification)
         {
-            LocalH2DatasourceSpecification localH2DatasourceSpecification = (LocalH2DatasourceSpecification) datasourceSpecification;
+            LocalH2DatasourceSpecification localH2DatasourceSpecification = (LocalH2DatasourceSpecification)datasourceSpecification;
             Root_meta_pure_alloy_connections_alloy_specification_LocalH2DatasourceSpecification local = new Root_meta_pure_alloy_connections_alloy_specification_LocalH2DatasourceSpecification_Impl("");
             local._testDataSetupCsv(localH2DatasourceSpecification.testDataSetupCsv);
             local._testDataSetupSqls(localH2DatasourceSpecification.testDataSetupSqls == null ? FastList.newList() : FastList.newList(localH2DatasourceSpecification.testDataSetupSqls));
             return local;
         }
-        else if(datasourceSpecification instanceof EmbeddedH2DatasourceSpecification)
+        else if (datasourceSpecification instanceof EmbeddedH2DatasourceSpecification)
         {
-            EmbeddedH2DatasourceSpecification embeddedH2DatasourceSpecification = (EmbeddedH2DatasourceSpecification) datasourceSpecification;
+            EmbeddedH2DatasourceSpecification embeddedH2DatasourceSpecification = (EmbeddedH2DatasourceSpecification)datasourceSpecification;
             Root_meta_pure_alloy_connections_alloy_specification_EmbeddedH2DatasourceSpecification embedded = new Root_meta_pure_alloy_connections_alloy_specification_EmbeddedH2DatasourceSpecification_Impl("");
             embedded._autoServerMode(embeddedH2DatasourceSpecification.autoServerMode);
             embedded._databaseName(embeddedH2DatasourceSpecification.databaseName);
             embedded._directory(embeddedH2DatasourceSpecification.directory);
             return embedded;
         }
-        else if(datasourceSpecification instanceof StaticDatasourceSpecification)
+        else if (datasourceSpecification instanceof StaticDatasourceSpecification)
         {
-            StaticDatasourceSpecification staticDatasourceSpecification = (StaticDatasourceSpecification) datasourceSpecification;
+            StaticDatasourceSpecification staticDatasourceSpecification = (StaticDatasourceSpecification)datasourceSpecification;
             Root_meta_pure_alloy_connections_alloy_specification_StaticDatasourceSpecification _static = new Root_meta_pure_alloy_connections_alloy_specification_StaticDatasourceSpecification_Impl("");
             _static._host(staticDatasourceSpecification.host);
             _static._port(staticDatasourceSpecification.port);
             _static._databaseName(staticDatasourceSpecification.databaseName);
             return _static;
         }
-        else if(datasourceSpecification instanceof SnowflakeDatasourceSpecification)
+        else if (datasourceSpecification instanceof SnowflakeDatasourceSpecification)
         {
-            SnowflakeDatasourceSpecification snowflakeDatasourceSpecification = (SnowflakeDatasourceSpecification) datasourceSpecification;
+            SnowflakeDatasourceSpecification snowflakeDatasourceSpecification = (SnowflakeDatasourceSpecification)datasourceSpecification;
             Root_meta_pure_alloy_connections_alloy_specification_SnowflakeDatasourceSpecification _snowflake = new Root_meta_pure_alloy_connections_alloy_specification_SnowflakeDatasourceSpecification_Impl("");
             _snowflake._accountName(snowflakeDatasourceSpecification.accountName);
             _snowflake._region(snowflakeDatasourceSpecification.region);
@@ -66,11 +82,21 @@ public class DatasourceSpecificationBuilder implements DatasourceSpecificationVi
             _snowflake._databaseName(snowflakeDatasourceSpecification.databaseName);
             _snowflake._cloudType(snowflakeDatasourceSpecification.cloudType);
             _snowflake._quotedIdentifiersIgnoreCase(snowflakeDatasourceSpecification.quotedIdentifiersIgnoreCase);
+            _snowflake._proxyHost(snowflakeDatasourceSpecification.proxyHost);
+            _snowflake._proxyPort(snowflakeDatasourceSpecification.proxyPort);
+            _snowflake._nonProxyHosts(snowflakeDatasourceSpecification.nonProxyHosts);
+            if (snowflakeDatasourceSpecification.accountType != null)
+            {
+                _snowflake._accountType(this.context.pureModel.getEnumValue("meta::pure::alloy::connections::alloy::specification::SnowflakeAccountType", snowflakeDatasourceSpecification.accountType));
+            }
+            _snowflake._organization(snowflakeDatasourceSpecification.organization);
+
+
             return _snowflake;
         }
-        else if(datasourceSpecification instanceof BigQueryDatasourceSpecification)
+        else if (datasourceSpecification instanceof BigQueryDatasourceSpecification)
         {
-            BigQueryDatasourceSpecification bigQueryDatasourceSpecification = (BigQueryDatasourceSpecification) datasourceSpecification;
+            BigQueryDatasourceSpecification bigQueryDatasourceSpecification = (BigQueryDatasourceSpecification)datasourceSpecification;
             Root_meta_pure_alloy_connections_alloy_specification_BigQueryDatasourceSpecification _bigquery = new Root_meta_pure_alloy_connections_alloy_specification_BigQueryDatasourceSpecification_Impl("");
             _bigquery._projectId(bigQueryDatasourceSpecification.projectId);
             _bigquery._defaultDataset(bigQueryDatasourceSpecification.defaultDataset);
--- legend-engine-language-pure-store-relational/src/test/java/org/finos/legend/engine/language/pure/grammar/test/TestRelationalConnectionGrammarParser.java
+++ legend-engine-language-pure-store-relational/src/test/java/org/finos/legend/engine/language/pure/grammar/test/TestRelationalConnectionGrammarParser.java
@@ -136,6 +136,49 @@ public class TestRelationalConnectionGrammarParser extends TestGrammarParser.Tes
                 "    account: 'account';\n" +
                 "    warehouse: 'warehouseName';\n" +
                 "    region: 'us-east2';\n" +
+                "    proxyHost: 'sampleHost';\n" +
+                "    proxyPort: 'samplePort';\n" +
+                "    nonProxyHosts: 'sample';\n" +
+                "    accountType: MultiTenant;\n" +
+                "    organization: 'sampleOrganization';\n" +
+                "  };\n" +
+                "  auth: SnowflakePublic\n" +
+                "  {" +
+                "       publicUserName: 'name';\n" +
+                "       privateKeyVaultReference: 'name';\n" +
+                "       passPhraseVaultReference: 'name';\n" +
+                "  };\n" +
+                "}\n");
+        test("###Connection\n" +
+                "RelationalDatabaseConnection meta::mySimpleConnection\n" +
+                "{\n" +
+                "  store: store::Store;\n" +
+                "  type: Snowflake;\n" +
+                "  specification: Snowflake\n" +
+                "  {\n" +
+                "    name: 'test';\n" +
+                "    account: 'account';\n" +
+                "    warehouse: 'warehouseName';\n" +
+                "    region: 'us-east2';\n" +
+                "  };\n" +
+                "  auth: SnowflakePublic\n" +
+                "  {" +
+                "       publicUserName: 'name';\n" +
+                "       privateKeyVaultReference: 'name';\n" +
+                "       passPhraseVaultReference: 'name';\n" +
+                "  };\n" +
+                "}\n");
+        test("###Connection\n" +
+                "RelationalDatabaseConnection meta::mySimpleConnection\n" +
+                "{\n" +
+                "  store: store::Store;\n" +
+                "  type: Snowflake;\n" +
+                "  specification: Snowflake\n" +
+                "  {\n" +
+                "    name: 'test';\n" +
+                "    account: 'account';\n" +
+                "    warehouse: 'warehouseName';\n" +
+                "    region: 'us-east2';\n" +
                 "  };\n" +
                 "  auth: SnowflakePublic\n" +
                 "  {" +
