c256b0343a23b371fdf900a0558cc25ac2569578
gs-ssh16 <SaiSriharsha.Annepu@gs.com>
2021-11-21 22:41:13 +0530

    Make graph fetch cross key caches serializable

--- legend-engine-executionPlan-execution/src/main/java/org/finos/legend/engine/plan/execution/cache/graphFetch/GraphFetchCacheKey.java
+++ legend-engine-executionPlan-execution/src/main/java/org/finos/legend/engine/plan/execution/cache/graphFetch/GraphFetchCacheKey.java
@@ -14,7 +14,9 @@
 
 package org.finos.legend.engine.plan.execution.cache.graphFetch;
 
-public abstract class GraphFetchCacheKey
+import java.io.Serializable;
+
+public abstract class GraphFetchCacheKey implements Serializable
 {
     protected abstract int hash();
 
