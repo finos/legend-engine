c74644e84813b96d7d845e28d8b79008d374f8d5
Dave Wathen <44839735+dave-wathen@users.noreply.github.com>
2021-11-24 02:19:07 +0000

    Fix whitespace handling in delimited drivers (#452)

--- legend-engine-external-format-flatdata-shared/src/test/java/org/finos/legend/engine/external/format/flatdata/shared/driver/TestDelimitedQuoting.java
+++ legend-engine-external-format-flatdata-shared/src/test/java/org/finos/legend/engine/external/format/flatdata/shared/driver/TestDelimitedQuoting.java
@@ -21,106 +21,103 @@ import org.junit.Test;
 
 import java.util.List;
 
-public class TestDelimitedQuoting extends AbstractDriverTest
-{
+public class TestDelimitedQuoting extends AbstractDriverTest {
     @Test
-    public void simpleQuoting()
-    {
-        runTest("Hello", "World", "'Hello','World'");
+    public void simpleQuoting() {
+        runTest("Hello", "The", "World", "'Hello','The','World'");
     }
 
     @Test
-    public void ifQuotesAreUsedThereCanBeWhitespaceAroundDelimiters()
-    {
-        runTest("Hello", "World", "'Hello'\t ,  'World'");
+    public void ifQuotesAreUsedThereCanBeWhitespaceAroundDelimiters() {
+        runTest("Hello", "The", "World", "'Hello'\t , 'The' ,  'World'");
     }
 
     @Test
-    public void ifQuotesAreNotUsedWhitespaceAroundDelimitersIsPartOfValue()
-    {
-        runTest("Hello\t", "  World", "Hello\t,  World");
+    public void ifQuotesAreNotUsedWhitespaceAroundDelimitersIsPartOfValue() {
+        runTest("Hello\t", " The ", "  World", "Hello\t, The ,  World");
     }
 
     @Test
-    public void quoteInsideValueIsJustAChar()
+    public void ifQuotesAreNotUsedAndOnlyWhitespaceIsBetweenDelimitersTheWhitespaceIsTheValue()
     {
-        runTest("Hello 'World'", "Bye", "Hello 'World','Bye'");
+        runTest("Hello", " ", "World", "Hello, ,World");
+        runTest("Hello", "  ", "World", "Hello,  ,World");
     }
 
     @Test
-    public void delimiterInQuotesIsPartOfValue()
-    {
-        runTest("Hello, World", "Bye", "'Hello, World','Bye'");
+    public void quoteInsideValueIsJustAChar() {
+        runTest("Hello 'World'", "and", "Bye", "Hello 'World','and','Bye'");
     }
 
     @Test
-    public void eolInQuotesIsPartOfValue()
-    {
-        runTest("Hello\r\nWorld", "Bye", "'Hello\r\nWorld','Bye'");
-        runTest("Hello\nWorld", "Bye", "'Hello\nWorld','Bye'");
-        runTest("Hello\rWorld", "Bye", "'Hello\rWorld','Bye'");
+    public void delimiterInQuotesIsPartOfValue() {
+        runTest("Hello, World", "and", "Bye", "'Hello, World','and','Bye'");
     }
 
     @Test
-    public void twoQuotesIsAnEscapedQuote()
-    {
-        runTest("'", "X", "'''','X'");
-        runTest("''", "X", "'''''','X'");
-        runTest("Hello, 'World'", "Bye", "'Hello, ''World''','Bye'");
+    public void eolInQuotesIsPartOfValue() {
+        runTest("Hello\r\nWorld", "and", "Bye", "'Hello\r\nWorld','and','Bye'");
+        runTest("Hello\nWorld", "and", "Bye", "'Hello\nWorld','and','Bye'");
+        runTest("Hello\rWorld", "and", "Bye", "'Hello\rWorld','and','Bye'");
     }
 
     @Test
-    public void quotedFieldShouldBeFollowedByDelimiter()
-    {
+    public void twoQuotesIsAnEscapedQuote() {
+        runTest("'", "X", "Y", "'''','X','Y'");
+        runTest("''", "X", "Y","'''''','X','Y'");
+        runTest("Hello, 'World'", "and", "Bye", "'Hello, ''World''','and','Bye'");
+    }
+
+    @Test
+    public void quotedFieldShouldBeFollowedByDelimiter() {
         runTestInvalid("Unexpected text after closing quote in value 1 at line 2", "'Hello' World,'Bye'");
     }
 
     @Test
-    public void quotesShouldBeTerminated()
-    {
+    public void quotesShouldBeTerminated() {
         runTestInvalid("Unclosed quotes in value 2 at line 2", "'Hello','W");
     }
 
-    private void runTest(String expectedField1, String expectedField2, String rawLine)
-    {
+    private void runTest(String expectedField1, String expectedField2, String expectedField3, String rawLine) {
         FlatData flatData = parseFlatData("section default: DelimitedWithHeadings\n" +
-                                                  "{\n" +
-                                                  "  scope.untilEof;\n" +
-                                                  "  delimiter       : ',';\n" +
-                                                  "  quoteChar       : '\\'';\n" +
-                                                  "  nullString      : '';\n" +
-                                                  "\n" +
-                                                  "  Record\n" +
-                                                  "  {\n" +
-                                                  "     FIELD1 : STRING;\n" +
-                                                  "     FIELD2 : STRING;\n" +
-                                                  "  }\n" +
-                                                  "}\n"
+                "{\n" +
+                "  scope.untilEof;\n" +
+                "  delimiter       : ',';\n" +
+                "  quoteChar       : '\\'';\n" +
+                "  nullString      : '';\n" +
+                "\n" +
+                "  Record\n" +
+                "  {\n" +
+                "     FIELD1 : STRING;\n" +
+                "     FIELD2 : STRING;\n" +
+                "     FIELD3 : STRING;\n" +
+                "  }\n" +
+                "}\n"
         );
 
-        List<IChecked<Data>> records = deserialize(Data.class, flatData, data("\n", "FIELD1,FIELD2", rawLine));
+        List<IChecked<Data>> records = deserialize(Data.class, flatData, data("\n", "FIELD1,FIELD2,FIELD3", rawLine));
         Assert.assertEquals(1, records.size());
         IChecked<Data> record = records.get(0);
         assertNoDefects(record);
         Assert.assertEquals(expectedField1, record.getValue().field1);
         Assert.assertEquals(expectedField2, record.getValue().field2);
+        Assert.assertEquals(expectedField3, record.getValue().field3);
     }
 
-    private void runTestInvalid(String expectedDefect, String rawLine)
-    {
+    private void runTestInvalid(String expectedDefect, String rawLine) {
         FlatData flatData = parseFlatData("section default: DelimitedWithHeadings\n" +
-                                                  "{\n" +
-                                                  "  scope.untilEof;\n" +
-                                                  "  delimiter       : ',';\n" +
-                                                  "  quoteChar       : '\\'';\n" +
-                                                  "  nullString      : '';\n" +
-                                                  "\n" +
-                                                  "  Record\n" +
-                                                  "  {\n" +
-                                                  "     FIELD1 : STRING;\n" +
-                                                  "     FIELD2 : STRING;\n" +
-                                                  "  }\n" +
-                                                  "}\n"
+                "{\n" +
+                "  scope.untilEof;\n" +
+                "  delimiter       : ',';\n" +
+                "  quoteChar       : '\\'';\n" +
+                "  nullString      : '';\n" +
+                "\n" +
+                "  Record\n" +
+                "  {\n" +
+                "     FIELD1 : STRING;\n" +
+                "     FIELD2 : STRING;\n" +
+                "  }\n" +
+                "}\n"
         );
 
         List<IChecked<Data>> records = deserialize(Data.class, flatData, data("\n", "FIELD1,FIELD2", rawLine));
@@ -128,9 +125,9 @@ public class TestDelimitedQuoting extends AbstractDriverTest
         assertHasDefect("Critical", expectedDefect, records.get(0));
     }
 
-    public static class Data
-    {
+    public static class Data {
         public String field1;
         public String field2;
+        public String field3;
     }
 }
