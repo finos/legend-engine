123296dc1dd69f1b698342b6936b4f2d684270b7
Hardik Maheshwari <19693874+hardikmaheshwari@users.noreply.github.com>
2022-01-18 14:49:05 +0530

    Revert "Cache common imports instead of resolving them each time (#482)"
    This reverts commit 6c19889239b28b00ed48972387fcdcf5ac960b54.

--- legend-engine-language-pure-compiler/src/main/java/org/finos/legend/engine/language/pure/compiler/toPureGraph/CompileContext.java
+++ legend-engine-language-pure-compiler/src/main/java/org/finos/legend/engine/language/pure/compiler/toPureGraph/CompileContext.java
@@ -59,7 +59,6 @@ public class CompileContext
     private static final ImmutableSet<String> SPECIAL_TYPES = _Package.SPECIAL_TYPES;
     private static final String PACKAGE_SEPARATOR = org.finos.legend.pure.m3.navigation.PackageableElement.PackageableElement.DEFAULT_PATH_SEPARATOR;
     private static final String META_PACKAGE_NAME = "meta";
-
     // NOTE: this list is taken from m3.pure in PURE
     private static final ImmutableSet<String> META_IMPORTS = Sets.immutable.with(
             "meta::pure::metamodel",
@@ -178,12 +177,6 @@ public class CompileContext
         if (path.contains(PACKAGE_SEPARATOR))
         {
             return resolver.apply(path);
-        }
-
-        // Resolve import if available in commonMetaPaths and auto-imports were not added
-        if (this.pureModel.getCommonPaths().containsKey(path))
-        {
-            return resolver.apply(this.pureModel.getCommonPaths().get(path));
         }
 
         // NOTE: here we make the assumption that we have populated the indices properly so the same element
@@ -414,12 +407,6 @@ public class CompileContext
             return functionHandlerMap.get(extractedFunctionName);
         }
 
-        // Resolve import if available in commonMetaPaths and auto-imports were not added
-        if (this.pureModel.getCommonPaths().containsKey(extractedFunctionName))
-        {
-            return functionHandlerMap.get(this.pureModel.getCommonPaths().get(extractedFunctionName));
-        }
-
         MutableMap<String, FunctionExpressionBuilder> results = searchImports(extractedFunctionName, functionHandlerMap::get);
         switch (results.size())
         {
@@ -476,10 +463,6 @@ public class CompileContext
                 results.put(fullPath, result);
             }
         });
-        if (results.size() == 1)
-        {
-            this.pureModel.addCommonPath(name, results.keysView().getAny());
-        }
         return results;
     }
 }
