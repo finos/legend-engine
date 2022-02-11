9e2b61d902cb8589eb6717e7cbc2aee0cf778c74
Pierre De Belen <pierre.debelen@gmail.com>
2022-02-11 09:00:30 -0500

    Upgrade Pure and Shared and add External tests (#547)

---
+++ legend-engine-server/src/test/java/org/finos/legend/engine/server/test/pureClient/other/Test_ExternalFormat_UsingPureClient.java
@@ -0,0 +1,27 @@
+package org.finos.legend.engine.server.test.pureClient.other;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.finos.legend.engine.server.test.shared.PureTestHelper;
+import org.finos.legend.pure.m3.execution.test.TestCollection;
+import org.finos.legend.pure.runtime.java.compiled.execution.CompiledExecutionSupport;
+
+public class Test_ExternalFormat_UsingPureClient
+{
+    public static Test suite()
+    {
+        return PureTestHelper.wrapSuite(
+                () -> PureTestHelper.initClientVersionIfNotAlreadySet("vX_X_X"),
+                () -> {
+                    CompiledExecutionSupport executionSupport = PureTestHelper.getClassLoaderExecutionSupport();
+                    TestSuite suite = new TestSuite();
+                    // NOTE: temporarily ignore these tests until we bring extensions back into Legend
+                    suite.addTest(PureTestHelper.buildSuite(TestCollection.collectTests("meta::external::format", executionSupport.getProcessorSupport(), ci -> PureTestHelper.satisfiesConditions(ci, executionSupport.getProcessorSupport())), executionSupport));
+                    suite.addTest(PureTestHelper.buildSuite(TestCollection.collectTests("meta::external::language", executionSupport.getProcessorSupport(), ci -> PureTestHelper.satisfiesConditions(ci, executionSupport.getProcessorSupport())), executionSupport));
+                    suite.addTest(PureTestHelper.buildSuite(TestCollection.collectTests("meta::external::shared", executionSupport.getProcessorSupport(), ci -> PureTestHelper.satisfiesConditions(ci, executionSupport.getProcessorSupport())), executionSupport));
+                    suite.addTest(PureTestHelper.buildSuite(TestCollection.collectTests("meta::external::store", executionSupport.getProcessorSupport(), ci -> PureTestHelper.satisfiesConditions(ci, executionSupport.getProcessorSupport())), executionSupport));
+                    return suite;
+                }
+        );
+    }
+}
