390bc2910e02be5eb562c349e2df6c3cbb3d1874
pierredebelen <pierre.debelen@gmail.com>
2022-02-11 08:14:24 -0500

    Upgrade Pure and Shared and add External tests

--- legend-engine-external-format-protobuf/src/main/java/org/finos/legend/engine/external/format/protobuf/schema/generations/ProtobufGenerationConfig.java
+++ legend-engine-external-format-protobuf/src/main/java/org/finos/legend/engine/external/format/protobuf/schema/generations/ProtobufGenerationConfig.java
@@ -15,22 +15,37 @@
 package org.finos.legend.engine.external.format.protobuf.schema.generations;
 
 import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.impl.factory.Lists;
+import org.eclipse.collections.impl.utility.Iterate;
 import org.eclipse.collections.impl.utility.ListIterate;
+import org.finos.legend.engine.external.format.protobuf.CustomOption;
 import org.finos.legend.engine.external.shared.format.generations.GenerationConfiguration;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
-import org.finos.legend.pure.generated.Root_meta_external_format_protobuf_generation_ProtobufConfig;
-import org.finos.legend.pure.generated.core_external_format_protobuf_integration;
-import org.finos.legend.pure.generated.core_pure_corefunctions_metaExtension;
+import org.finos.legend.pure.generated.*;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.PackageableElement;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ProtobufGenerationConfig extends GenerationConfiguration
 {
+    public Options options;
+
     public Root_meta_external_format_protobuf_generation_ProtobufConfig transformToPure(PureModel pureModel)
     {
         Root_meta_external_format_protobuf_generation_ProtobufConfig generationConfiguration = core_external_format_protobuf_integration.Root_meta_external_format_protobuf_generation_defaultConfig__ProtobufConfig_1_(pureModel.getExecutionSupport());
         List<PackageableElement> scopeElements = ListIterate.collect(this.generationScope(), e -> core_pure_corefunctions_metaExtension.Root_meta_pure_functions_meta_pathToElement_String_1__PackageableElement_1_(e, pureModel.getExecutionSupport()));
+        if (options != null)
+        {
+            generationConfiguration._options(new Root_meta_external_format_protobuf_metamodel_Options_Impl("")
+                    ._javaPackage(options.javaPackage)
+                    ._javaOuterClassname(options.javaOuterClassname)
+                    ._javaMultipleFiles(options.javaMultipleFiles)
+                    ._customOptions(Lists.mutable.withAll(Iterate.collect(options.customOptions, o-> new Root_meta_external_format_protobuf_metamodel_CustomOption_Impl(""))))
+                    ._optimizeFor(options.optimizeFor == null ? null : pureModel.getEnumValue("meta::protocols::pure::vX_X_X::metamodel::invocation::generation::protobuf::OptimizeMode", options.optimizeFor.name()))
+            );
+        }
+
         return generationConfiguration._scopeElements((RichIterable<? extends PackageableElement>) scopeElements);
     }
 }
