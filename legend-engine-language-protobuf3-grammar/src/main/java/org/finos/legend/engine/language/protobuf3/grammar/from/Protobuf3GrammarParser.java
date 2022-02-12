d2169ad906b961a663186eef16f3289b9ec7e98d
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-02-12 10:28:40 -0500

    Initial modules for protobuf3

---
+++ legend-engine-language-protobuf3-grammar/src/main/java/org/finos/legend/engine/language/protobuf3/grammar/from/Protobuf3GrammarParser.java
@@ -0,0 +1,4 @@
+package org.finos.legend.engine.language.protobuf3.grammar.from;
+
+public class Protobuf3GrammarParser {
+}
---
+++ legend-engine-language-protobuf3-grammar/src/main/java/org/finos/legend/engine/language/protobuf3/grammar/to/Protobuf3GrammarComposer.java
@@ -0,0 +1,4 @@
+package org.finos.legend.engine.language.protobuf3.grammar.to;
+
+public class Protobuf3GrammarComposer {
+}
