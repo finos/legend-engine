342fbec9ecec9c731537bcaecc0bd1bf0bba3ee8
Mohammed Ibrahim <mibrahim1771@gmail.com>
2022-02-22 10:27:19 -0500

    Change SDLC info Service (#552)
    * Add build info plugin
    
    * Change info service and add deployment info model
    
    * Clean up info
    
    * Fix landing page

---
+++ legend-engine-shared-core/src/main/java/org/finos/legend/engine/shared/core/deployment/DeploymentVersionInfo.java
@@ -0,0 +1,34 @@
+package org.finos.legend.engine.shared.core.deployment;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DeploymentVersionInfo {
+    @JsonProperty(value="git.branch", required = true)
+    public String branch;
+
+    @JsonProperty(value="git.build.time", required = true)
+    public String buildTime;
+
+    @JsonProperty(value="git.build.version", required = true)
+    public String buildVersion;
+
+    @JsonProperty(value="git.closest.tag.name", required = true)
+    public String buildTag;
+
+    @JsonProperty(value="git.commit.id", required = true)
+    public String commitId;
+
+    @JsonProperty(value="git.commit.id.abbrev")
+    public String commitIdAbbreviated;
+
+    @JsonProperty(value="git.commit.time")
+    public String commitTime;
+
+    @JsonProperty(value="git.total.commit.count")
+    public String commitCount;
+
+}
