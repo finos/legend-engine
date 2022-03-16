d4b781eb231bca516c63276a45b1a5b42522cb1b
Prateek Garg <Prateek.x.Garg@gs.com>
2022-03-16 17:57:53 +0530

    extend Server configuration for test Server

--- legend-engine-server/src/test/java/org/finos/legend/engine/server/test/shared/TestServer.java
+++ legend-engine-server/src/test/java/org/finos/legend/engine/server/test/shared/TestServer.java
@@ -14,18 +14,21 @@
 
 package org.finos.legend.engine.server.test.shared;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+
 import io.dropwizard.setup.Environment;
 import org.finos.legend.engine.plan.execution.stores.relational.connection.tests.api.ConnectionTestManager;
 import org.finos.legend.engine.plan.execution.stores.relational.connection.tests.api.DynamicTestConnection;
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.DatabaseType;
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.RelationalDatabaseConnection;
 import org.finos.legend.engine.server.Server;
-import org.finos.legend.engine.server.ServerConfiguration;
 import org.finos.legend.engine.shared.core.url.EngineUrlStreamHandlerFactory;
 
-import java.util.*;
-
-public class TestServer extends Server
+public class TestServer extends Server<TestServerConfiguration>
 {
     private final List<DynamicTestConnection> dynamicTestConnections = new ArrayList<>();
 
@@ -36,13 +39,13 @@ public class TestServer extends Server
     }
 
     @Override
-    public void run(ServerConfiguration serverConfiguration, Environment environment)
+    public void run(TestServerConfiguration serverConfiguration, Environment environment)
     {
         super.run(serverConfiguration, environment);
         environment.jersey().register(new TestConnectionProviderApi(getTestConnections(serverConfiguration)));
     }
 
-    public Map<DatabaseType, RelationalDatabaseConnection> getTestConnections (ServerConfiguration serverConfiguration)
+    public Map<DatabaseType, RelationalDatabaseConnection> getTestConnections (TestServerConfiguration serverConfiguration)
     {
         Map<DatabaseType, RelationalDatabaseConnection> testConnections = new HashMap<>();
 
