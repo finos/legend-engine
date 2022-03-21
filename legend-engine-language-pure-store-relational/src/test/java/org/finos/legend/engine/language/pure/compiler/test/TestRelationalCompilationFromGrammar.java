f433e3250ffa405cc2f4a8414f121d0c12242ad3
emilia-sokol-gs <emilia.sokol@gs.com>
2022-03-21 11:09:56 +0100

    Add tests

--- legend-engine-language-pure-store-relational/src/test/java/org/finos/legend/engine/language/pure/compiler/test/TestRelationalCompilationFromGrammar.java
+++ legend-engine-language-pure-store-relational/src/test/java/org/finos/legend/engine/language/pure/compiler/test/TestRelationalCompilationFromGrammar.java
@@ -516,7 +516,7 @@ public class TestRelationalCompilationFromGrammar extends TestCompilationFromGra
         );
 
         // association mapping on milestoned models
-        test("\n" +
+        PureModel model = test("\n" +
                 "Class model::Firm\n" +
                 "{\n" +
                 "   legalName: String[1];\n" +
@@ -550,7 +550,9 @@ public class TestRelationalCompilationFromGrammar extends TestCompilationFromGra
                 "     )\n" +
                 "  }" +
                 ")"
-        );
+        ).getTwo();
+        Assert.assertNotNull(model.getMapping("model::myRelationalMapping")._associationMappings().getAny()._stores());
+        Assert.assertEquals("dbInc", model.getMapping("model::myRelationalMapping")._associationMappings().getAny()._stores().getAny()._name());
 
         // Mapping on milestoned properties
         test("\n" +
@@ -1214,6 +1216,70 @@ public class TestRelationalCompilationFromGrammar extends TestCompilationFromGra
                 "    firm: Binding simple::TestBinding: [simple::dbInc]@personSelfJoin | [simple::dbInc]personTable.FIRM\n" +
                 "  }\n" +
                 ")\n");
+    }
+
+    @Test
+    public void testLocalProperties() throws Exception
+    {
+        PureModel model = test("Class model::Person {\n" +
+                "   name:String[1];\n" +
+                "}\n" +
+                "###Relational\n" +
+                "Database model::store::db\n" +
+                "(\n" +
+                "   Table myTable(name VARCHAR(200))\n" +
+                ")\n" +
+                "###Mapping\n" +
+                "Mapping \n" +
+                "model::mapping::myMap\n" +
+                "(\n" +
+                "   model::Person: Relational\n" +
+                "   {\n" +
+                "      name : [model::store::db]myTable.name,\n" +
+                "      + localProp : String[1] : [model::store::db]myTable.name\n" +
+                "   }\n" +
+                ")"
+        ).getTwo();
+        org.finos.legend.pure.m3.coreinstance.meta.pure.mapping.Mapping mmMapping = model.getMapping("model::mapping::myMap");
+        RootRelationalInstanceSetImplementation rootRelationalInstanceSetImplementation = ((RootRelationalInstanceSetImplementation) mmMapping._classMappings().getFirst());
+
+        Assert.assertTrue(rootRelationalInstanceSetImplementation._propertyMappings().getLast()._localMappingProperty());
+
+        Assert.assertNotNull(rootRelationalInstanceSetImplementation._propertyMappings().getLast()._localMappingPropertyMultiplicity());
+        Assert.assertNotNull(rootRelationalInstanceSetImplementation._propertyMappings().getLast()._localMappingPropertyType());
+        Assert.assertEquals("String", rootRelationalInstanceSetImplementation._propertyMappings().getLast()._localMappingPropertyType()._name());
+    }
+
+    @Test
+    public void testFilerName() throws Exception
+    {
+        PureModel model = test("Class model::Person {\n" +
+                "   name:String[1];\n" +
+                "}\n" +
+                "###Relational\n" +
+                "Database model::store::db\n" +
+                "(\n" +
+                "   Table myTable(name VARCHAR(200))\n" +
+                "   Filter myFilter(myTable.name = 'A')\n" +
+                ")\n" +
+                "###Mapping\n" +
+                "Mapping \n" +
+                "model::mapping::myMap\n" +
+                "(\n" +
+                "   model::Person: Relational\n" +
+                "   {\n" +
+                "      ~filter [model::store::db]myFilter \n" +
+                "      name : [model::store::db]myTable.name,\n" +
+                "      + localProp : String[1] : [model::store::db]myTable.name\n" +
+                "   }\n" +
+                ")"
+        ).getTwo();
+        org.finos.legend.pure.m3.coreinstance.meta.pure.mapping.Mapping mmMapping = model.getMapping("model::mapping::myMap");
+        RootRelationalInstanceSetImplementation rootRelationalInstanceSetImplementation = ((RootRelationalInstanceSetImplementation) mmMapping._classMappings().getFirst());
+
+        Assert.assertNotNull(rootRelationalInstanceSetImplementation._filter()._filterName());
+        Assert.assertEquals("myFilter", rootRelationalInstanceSetImplementation._filter()._filterName());
+
     }
 
     @Test
