243e9278255a6c27b7705b808524b071f52b7faf
anusha2802 <anusha.chheda@ny.email.gs.com>
2022-03-22 13:04:57 -0500

    Fixing bug created by name change to awspersistence

--- legend-engine-external-format-awspersistence/src/test/java/org/finos/legend/engine/external/format/awspersistence/tests/TestAwsPersistenceFileGeneration.java
+++ legend-engine-external-format-awspersistence/src/test/java/org/finos/legend/engine/external/format/awspersistence/tests/TestAwsPersistenceFileGeneration.java
@@ -1,7 +1,14 @@
 package org.finos.legend.engine.external.format.awspersistence.tests;
 
 import org.apache.commons.io.IOUtils;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.multimap.list.MutableListMultimap;
+import org.eclipse.collections.impl.utility.Iterate;
+import org.finos.legend.engine.external.format.awspersistence.extension.AwsPersistenceGenerationConfigFromFileGenerationSpecificationBuilder;
 import org.finos.legend.engine.external.format.awspersistence.extension.AwsPersistenceGenerationExtension;
+import org.finos.legend.engine.external.format.awspersistence.model.AwsPersistenceGenerationConfig;
+import org.finos.legend.engine.external.shared.format.extension.GenerationExtension;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
 import org.finos.legend.engine.language.pure.grammar.from.PureGrammarParser;
 import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
@@ -16,6 +23,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.ServiceLoader;
 import java.util.stream.Collectors;
 
 public class TestAwsPersistenceFileGeneration
@@ -25,12 +33,19 @@ public class TestAwsPersistenceFileGeneration
     public void testAwsPersistenceGenerationOutput() throws IOException
     {
         AwsPersistenceGenerationExtension generationExtension = new AwsPersistenceGenerationExtension();
-
         String pureCode = IOUtils.toString(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("org/finos/legend/engine/external/format/awspersistence/tests/TestAwsPersistenceGeneration.pure")), StandardCharsets.UTF_8);
+
+        //debugging
+        MutableList<String> extensions = Iterate.addAllTo(ServiceLoader.load(GenerationExtension.class), Lists.mutable.empty()).collect(x -> x.getKey());
+        System.out.println(extensions);
+
         PureModelContextData pureModelContextData = PureGrammarParser.newInstance().parseModel(pureCode);
+        //System.out.println(pureModelContextData.getElements()); --works
+        //AwsPersistenceGenerationConfig awspersistenceConfig = AwsPersistenceGenerationConfigFromFileGenerationSpecificationBuilder.build(fileGeneration);
+        //System.out.println(awspersistenceConfig);
+
         PureModel pureModel = new PureModel(pureModelContextData, null, DeploymentMode.TEST);
         FileGenerationSpecification fileGeneration = pureModelContextData.getElementsOfType(FileGenerationSpecification.class).get(0);
-
         List<Root_meta_pure_generation_metamodel_GenerationOutput> outputs = generationExtension.generateFromElement(fileGeneration, pureModel.getContext());
         Map<String, String> fileToContent = outputs.stream().collect(Collectors.toMap(Root_meta_pure_generation_metamodel_GenerationOutput::_fileName, Root_meta_pure_generation_metamodel_GenerationOutput::_content));
 
