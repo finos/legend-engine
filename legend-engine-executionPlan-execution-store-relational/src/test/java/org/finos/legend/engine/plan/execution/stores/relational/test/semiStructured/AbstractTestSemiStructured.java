d26af04622aee5916d8bd9c7d826890467219710
Sai Sriharsha Annepu <72639930+gs-ssh16@users.noreply.github.com>
2022-03-24 00:34:29 +0530

    H2 semi-structured navigation using user defined function (#601)

--- legend-engine-executionPlan-execution-store-relational/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/semiStructured/AbstractTestSemiStructured.java
+++ legend-engine-executionPlan-execution-store-relational/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/semiStructured/AbstractTestSemiStructured.java
@@ -20,14 +20,20 @@ import org.finos.legend.engine.language.pure.compiler.Compiler;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.HelperValueSpecificationBuilder;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
 import org.finos.legend.engine.language.pure.grammar.from.PureGrammarParser;
+import org.finos.legend.engine.plan.execution.PlanExecutor;
+import org.finos.legend.engine.plan.execution.stores.relational.result.RelationalResult;
+import org.finos.legend.engine.plan.execution.stores.relational.serialization.RelationalResultToCSVSerializer;
 import org.finos.legend.engine.plan.generation.PlanGenerator;
+import org.finos.legend.engine.plan.generation.transformers.LegendPlanTransformers;
 import org.finos.legend.engine.plan.platform.PlanPlatform;
 import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
+import org.finos.legend.engine.protocol.pure.v1.model.executionPlan.SingleExecutionPlan;
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.domain.Function;
 import org.finos.legend.pure.generated.*;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -35,6 +41,7 @@ public abstract class AbstractTestSemiStructured
 {
     private final PureModelContextData contextData;
     private final PureModel pureModel;
+    private final PlanExecutor planExecutor = PlanExecutor.newPlanExecutorWithAvailableStoreExecutors();
 
     public AbstractTestSemiStructured()
     {
@@ -46,9 +53,7 @@ public abstract class AbstractTestSemiStructured
 
     protected String buildExecutionPlanString(String function, String mapping, String runtime)
     {
-        MutableList<Root_meta_pure_router_extension_RouterExtension> extensions = Lists.mutable.empty();
-        extensions.addAll(Lists.mutable.withAll(core_relational_relational_router_router_extension.Root_meta_pure_router_extension_defaultRelationalExtensions__RouterExtension_MANY_(pureModel.getExecutionSupport())));
-        extensions.addAll(core_external_shared_extension.Root_meta_external_shared_format_routerExtensions_String_1__ExternalFormatExtension_MANY__RouterExtension_MANY_("externalFormat", Lists.mutable.with(core_external_format_json_extension.Root_meta_external_format_json_jsonFormatExtension__ExternalFormatExtension_1_(pureModel.getExecutionSupport())), pureModel.getExecutionSupport()).toList());
+        MutableList<Root_meta_pure_router_extension_RouterExtension> extensions = getRouterExtensions();
 
         Function functionObject = Objects.requireNonNull(contextData.getElementsOfType(Function.class).stream().filter(x -> function.equals(x._package + "::" + x.name)).findFirst().orElse(null));
 
@@ -64,6 +69,35 @@ public abstract class AbstractTestSemiStructured
         );
 
         return core_pure_executionPlan_executionPlan_print.Root_meta_pure_executionPlan_toString_planToString_ExecutionPlan_1__Boolean_1__RouterExtension_MANY__String_1_(executionPlan, true, extensions, pureModel.getExecutionSupport());
+    }
+
+    protected String executeFunction(String function, String mapping, String runtime)
+    {
+        Function functionObject = Objects.requireNonNull(contextData.getElementsOfType(Function.class).stream().filter(x -> function.equals(x._package + "::" + x.name)).findFirst().orElse(null));
+
+        SingleExecutionPlan executionPlan = PlanGenerator.generateExecutionPlan(
+                HelperValueSpecificationBuilder.buildLambda(functionObject.body, functionObject.parameters, pureModel.getContext()),
+                pureModel.getMapping(mapping),
+                pureModel.getRuntime(runtime),
+                null,
+                pureModel,
+                "vX_X_X",
+                PlanPlatform.JAVA,
+                null,
+                getRouterExtensions(),
+                LegendPlanTransformers.transformers
+        );
+
+        RelationalResult result = (RelationalResult) this.planExecutor.execute(executionPlan);
+        return new String(new RelationalResultToCSVSerializer(result).flush().toByteArray(), StandardCharsets.UTF_8);
+    }
+
+    private MutableList<Root_meta_pure_router_extension_RouterExtension> getRouterExtensions()
+    {
+        MutableList<Root_meta_pure_router_extension_RouterExtension> extensions = Lists.mutable.empty();
+        extensions.addAll(Lists.mutable.withAll(core_relational_relational_router_router_extension.Root_meta_pure_router_extension_defaultRelationalExtensions__RouterExtension_MANY_(pureModel.getExecutionSupport())));
+        extensions.addAll(core_external_shared_extension.Root_meta_external_shared_format_routerExtensions_String_1__ExternalFormatExtension_MANY__RouterExtension_MANY_("externalFormat", Lists.mutable.with(core_external_format_json_extension.Root_meta_external_format_json_jsonFormatExtension__ExternalFormatExtension_1_(pureModel.getExecutionSupport())), pureModel.getExecutionSupport()).toList());
+        return extensions;
     }
 
     private String readModelContentFromResource(String resourcePath)
--- legend-engine-executionPlan-execution-store-relational/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/semiStructured/TestSimpleSemiStructuredMapping.java
+++ legend-engine-executionPlan-execution-store-relational/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/semiStructured/TestSimpleSemiStructuredMapping.java
@@ -22,6 +22,9 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
     private static final String snowflakeMapping = "simple::mapping::SnowflakeMapping";
     private static final String snowflakeRuntime = "simple::runtime::SnowflakeRuntime";
 
+    private static final String h2Mapping = "simple::mapping::H2Mapping";
+    private static final String h2Runtime = "simple::runtime::H2Runtime";
+
     @Test
     public void testSingleSemiStructuredPropertyAccess()
     {
@@ -34,6 +37,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::singleSemiStructuredPropertyAccess", h2Mapping, h2Runtime);
+        Assert.assertEquals("Firm X\n" +
+                "Firm X\n" +
+                "Firm X\n" +
+                "Firm X\n" +
+                "Firm A\n" +
+                "Firm B\n" +
+                "Firm B\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -48,6 +60,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::combinedPrimitiveAndSemiStructuredPropertyAccessParallel", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter,Firm X\n" +
+                "John,Firm X\n" +
+                "John,Firm X\n" +
+                "Anthony,Firm X\n" +
+                "Fabrice,Firm A\n" +
+                "Oliver,Firm B\n" +
+                "David,Firm B\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -62,6 +83,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::combinedPrimitiveAndSemiStructuredPropertyAccess", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter : Firm X\n" +
+                "John : Firm X\n" +
+                "John : Firm X\n" +
+                "Anthony : Firm X\n" +
+                "Fabrice : Firm A\n" +
+                "Oliver : Firm B\n" +
+                "David : Firm B\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -76,6 +106,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::combinedComplexAndSemiStructuredPropertyAccessParallel", h2Mapping, h2Runtime);
+        Assert.assertEquals("John,Firm X\n" +
+                "Anthony,Firm X\n" +
+                "John,Firm X\n" +
+                ",Firm X\n" +
+                ",Firm A\n" +
+                "David,Firm B\n" +
+                ",Firm B\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -90,6 +129,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::combinedComplexAndSemiStructuredPropertyAccess", h2Mapping, h2Runtime);
+        Assert.assertEquals("John : Firm X\n" +
+                "Anthony : Firm X\n" +
+                "John : Firm X\n" +
+                "NULL : Firm X\n" +
+                "NULL : Firm A\n" +
+                "David : Firm B\n" +
+                "NULL : Firm B\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -104,6 +152,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::nestedSemiStructuredPropertyAccess", h2Mapping, h2Runtime);
+        Assert.assertEquals("A1\n" +
+                "A1\n" +
+                "A1\n" +
+                "A1\n" +
+                "A2\n" +
+                "A3\n" +
+                "A3\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -118,6 +175,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::multipleSemiStructuredPropertyAccess", h2Mapping, h2Runtime);
+        Assert.assertEquals("Firm X,A1\n" +
+                "Firm X,A1\n" +
+                "Firm X,A1\n" +
+                "Firm X,A1\n" +
+                "Firm A,A2\n" +
+                "Firm B,A3\n" +
+                "Firm B,A3\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -132,6 +198,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::multipleSemiStructuredPropertyAccessCombined", h2Mapping, h2Runtime);
+        Assert.assertEquals("Firm XA1\n" +
+                "Firm XA1\n" +
+                "Firm XA1\n" +
+                "Firm XA1\n" +
+                "Firm AA2\n" +
+                "Firm BA3\n" +
+                "Firm BA3\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -146,6 +221,12 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::filterWithSemiStructuredProperty", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter\n" +
+                "John\n" +
+                "John\n" +
+                "Anthony\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -160,6 +241,11 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::groupByWithSemiStructuredProperty", h2Mapping, h2Runtime);
+        Assert.assertEquals("A1,Peter;John;John;Anthony\n" +
+                "A2,Fabrice\n" +
+                "A3,Oliver;David\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -174,6 +260,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::sortByWithSemiStructuredProperty", h2Mapping, h2Runtime);
+        Assert.assertEquals("Fabrice\n" +
+                "Oliver\n" +
+                "David\n" +
+                "Peter\n" +
+                "John\n" +
+                "John\n" +
+                "Anthony\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -188,6 +283,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::isEmptyCheckOnSemiStructuredPrimitivePropertyAccess", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter,S1\n" +
+                "John,S1\n" +
+                "John,S1\n" +
+                "Anthony,S1\n" +
+                "Fabrice,NULL\n" +
+                "Oliver,S2\n" +
+                "David,NULL\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -202,6 +306,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::isEmptyCheckOnSemiStructuredPropertyAccessAfterAt", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter,D3\n" +
+                "John,D3\n" +
+                "John,D3\n" +
+                "Anthony,D3\n" +
+                "Fabrice,NULL\n" +
+                "Oliver,NULL\n" +
+                "David,NULL\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -216,6 +329,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::semiStructuredDifferentDataTypePropertyAccess", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter,Firm X,4,true,2010-03-04,2022-01-16 01:00:00.0,S1\n" +
+                "John,Firm X,4,true,2010-03-04,2022-01-16 01:00:00.0,S1\n" +
+                "John,Firm X,4,true,2010-03-04,2022-01-16 01:00:00.0,S1\n" +
+                "Anthony,Firm X,4,true,2010-03-04,2022-01-16 01:00:00.0,S1\n" +
+                "Fabrice,Firm A,1,false,2012-11-13,2022-02-14 03:00:00.0,\n" +
+                "Oliver,Firm B,2,true,2017-07-07,2022-09-01 06:00:00.0,S2\n" +
+                "David,Firm B,2,true,2017-07-07,2022-09-01 06:00:00.0,\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -230,6 +352,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::semiStructuredArrayElementAccessPrimitive", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter,O1,O2\n" +
+                "John,O1,O2\n" +
+                "John,O1,O2\n" +
+                "Anthony,O1,O2\n" +
+                "Fabrice,O3,O4\n" +
+                "Oliver,O5,O6\n" +
+                "David,O5,O6\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -244,6 +375,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::semiStructuredArrayElementAccessComplex", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter,D1,D2,D3,\n" +
+                "John,D1,D2,D3,\n" +
+                "John,D1,D2,D3,\n" +
+                "Anthony,D1,D2,D3,\n" +
+                "Fabrice,D4,D5,,\n" +
+                "Oliver,D5,D6,,\n" +
+                "David,D5,D6,,\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -258,6 +398,15 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::semiStructuredPropertyAccessAtNestedProperty", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter,Firm X,Firm X,Firm X,\n" +
+                "John,Firm X,Firm X,,\n" +
+                "John,Firm X,Firm X,Firm X,\n" +
+                "Anthony,Firm X,,,\n" +
+                "Fabrice,Firm A,,,\n" +
+                "Oliver,Firm B,Firm B,,\n" +
+                "David,Firm B,,,\n", h2Result.replace("\r\n", "\n"));
     }
 
     @Test
@@ -272,6 +421,10 @@ public class TestSimpleSemiStructuredMapping extends AbstractTestSemiStructured
                 "  connection = RelationalDatabaseConnection(type = \"Snowflake\")\n" +
                 ")\n";
         Assert.assertEquals(snowflakeExpected, snowflakePlan);
+
+        String h2Result = this.executeFunction("simple::filterWithSemiStructuredPropertyAccessAtNestedProperty", h2Mapping, h2Runtime);
+        Assert.assertEquals("Peter\n" +
+                "John\n", h2Result.replace("\r\n", "\n"));
     }
 
     public String modelResourcePath()
