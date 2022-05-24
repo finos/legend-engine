cde08f8d0b6ad184fe30251598f7dc1d4e38e4b1
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-05-23 22:16:39 -0400

    Add skeleton DAML format extension

---
+++ legend-engine-xt-daml-model/src/main/java/org/finos/legend/engine/external/format/daml/DamlFormatExtension.java
@@ -0,0 +1,83 @@
+package org.finos.legend.engine.external.format.daml;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.collections.api.factory.Lists;
+import org.finos.legend.engine.external.format.daml.toModel.DamlToModelConfiguration;
+import org.finos.legend.engine.external.shared.format.model.ExternalFormatExtension;
+import org.finos.legend.engine.external.shared.format.model.ExternalSchemaCompileContext;
+import org.finos.legend.engine.external.shared.format.model.fromModel.ModelToSchemaConfiguration;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.CompileContext;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.protocol.haskell.metamodel.HaskellModule;
+import org.finos.legend.engine.protocol.haskell.metamodel.Translator;
+import org.finos.legend.pure.generated.Root_meta_external_shared_format_binding_Binding;
+import org.finos.legend.pure.generated.Root_meta_external_shared_format_binding_validation_BindingDetail;
+import org.finos.legend.pure.generated.Root_meta_external_shared_format_metamodel_SchemaSet;
+import org.finos.legend.pure.generated.Root_meta_external_language_haskell_binding_toPure_HaskellModuleContainer;
+import org.finos.legend.pure.generated.Root_meta_external_language_haskell_binding_toPure_HaskellModuleContainer_Impl;
+
+import java.util.List;
+
+public class DamlFormatExtension implements ExternalFormatExtension<Root_meta_external_language_haskell_binding_toPure_HaskellModuleContainer, DamlToModelConfiguration, ModelToSchemaConfiguration>
+{
+
+    @Override
+    public String getFormat() {
+        return "DAML";
+    }
+
+    @Override
+    public List<String> getContentTypes() {
+        return null;
+    }
+
+    @Override
+    public Root_meta_external_language_haskell_binding_toPure_HaskellModuleContainer compileSchema(ExternalSchemaCompileContext context) {
+        try
+        {
+            return new Root_meta_external_language_haskell_binding_toPure_HaskellModuleContainer_Impl("")
+                    ._module(
+                            new Translator().translate(
+                                    new ObjectMapper().readValue(context.getContent(), HaskellModule.class),
+                                    context.getPureModel()
+                            )
+                    );
+        }
+        catch (JsonProcessingException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Root_meta_external_shared_format_binding_validation_BindingDetail bindDetails(Root_meta_external_shared_format_binding_Binding binding, CompileContext context) {
+        return null;
+    }
+
+    @Override
+    public String metamodelToText(Root_meta_external_language_haskell_binding_toPure_HaskellModuleContainer schemaDetail) {
+        return null;
+    }
+
+    @Override
+    public boolean supportsModelGeneration()
+    {
+        return true;
+    }
+
+    @Override
+    public Root_meta_external_shared_format_binding_Binding generateModel(Root_meta_external_shared_format_metamodel_SchemaSet schema, DamlToModelConfiguration damlToModelConfiguration, PureModel pureModel) {
+        return null;
+    }
+
+    @Override
+    public Root_meta_external_shared_format_binding_Binding generateSchema(ModelToSchemaConfiguration modelToSchemaConfiguration, PureModel pureModel) {
+        return null;
+    }
+
+    @Override
+    public List<String> getRegisterablePackageableElementNames() {
+        return Lists.fixedSize.of();
+    }
+}
---
+++ legend-engine-xt-daml-model/src/main/java/org/finos/legend/engine/external/format/daml/toModel/DamlToModelConfiguration.java
@@ -0,0 +1,6 @@
+package org.finos.legend.engine.external.format.daml.toModel;
+
+import org.finos.legend.engine.external.shared.format.model.toModel.SchemaToModelConfiguration;
+
+public class DamlToModelConfiguration extends SchemaToModelConfiguration {
+}
