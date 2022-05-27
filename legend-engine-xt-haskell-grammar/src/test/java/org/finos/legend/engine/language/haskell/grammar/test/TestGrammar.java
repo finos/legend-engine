cc2dc8c0775372e47e59d0b51ef9b0f12cbad48f
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-05-27 16:39:29 -0400

    Add initial pureToHaskell transformation function

--- legend-engine-xt-haskell-grammar/src/test/java/org/finos/legend/engine/language/haskell/grammar/test/TestGrammar.java
+++ legend-engine-xt-haskell-grammar/src/test/java/org/finos/legend/engine/language/haskell/grammar/test/TestGrammar.java
@@ -19,6 +19,16 @@ public class TestGrammar {
     }
 
     @Test
+    public void testTypeWithListRoundtrip()
+    {
+        check("module Gs.Finance\n" +
+                "  where\n" +
+                "\n"+
+                "data Trade = Trade { id :: Int, notionals :: [Double] }\n" +
+                "    deriving (Eq, Ord, Show)\n");
+    }
+
+    @Test
     public void testEnumRoundtrip()
     {
         check("module Gs.Finance\n" +
