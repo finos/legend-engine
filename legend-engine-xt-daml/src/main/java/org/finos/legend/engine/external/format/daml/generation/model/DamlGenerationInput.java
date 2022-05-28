d2a21e721aa09bfcdcfeff5f9379401a299c55b8
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-05-28 17:17:38 -0400

    DAML generation service

---
+++ legend-engine-xt-daml/src/main/java/org/finos/legend/engine/external/format/daml/generation/model/DamlGenerationInput.java
@@ -0,0 +1,8 @@
+package org.finos.legend.engine.external.format.daml.generation.model;
+
+import org.finos.legend.engine.external.shared.format.generations.GenerationInput;
+
+public class DamlGenerationInput extends GenerationInput
+{
+    public DamlGenerationConfig config;
+}
