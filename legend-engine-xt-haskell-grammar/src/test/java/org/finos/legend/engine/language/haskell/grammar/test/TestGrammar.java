9b6265f1a0d91ddc95526e5834912b6e0823f5f2
jackie-h <10471950+jackie-h@users.noreply.github.com>
2022-06-06 15:49:14 -0400

    Checkstyle fixes

--- legend-engine-xt-haskell-grammar/src/test/java/org/finos/legend/engine/language/haskell/grammar/test/TestGrammar.java
+++ legend-engine-xt-haskell-grammar/src/test/java/org/finos/legend/engine/language/haskell/grammar/test/TestGrammar.java
@@ -1,3 +1,17 @@
+//  Copyright 2022 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package org.finos.legend.engine.language.haskell.grammar.test;
 
 import org.finos.legend.engine.language.haskell.grammar.from.HaskellGrammarParser;
@@ -6,14 +20,14 @@ import org.finos.legend.engine.protocol.haskell.metamodel.HaskellModule;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class TestGrammar {
-
+public class TestGrammar
+{
     @Test
     public void testTypeRoundtrip()
     {
         check("module Gs.Finance\n" +
                 "  where\n" +
-                "\n"+
+                "\n" +
                 "data Person = Person { id :: Int, name :: String }\n" +
                 "    deriving (Eq, Ord, Show)\n");
     }
@@ -23,7 +37,7 @@ public class TestGrammar {
     {
         check("module Gs.Finance\n" +
                 "  where\n" +
-                "\n"+
+                "\n" +
                 "data Trade = Trade { id :: Int, notionals :: [Double] }\n" +
                 "    deriving (Eq, Ord, Show)\n");
     }
@@ -33,7 +47,7 @@ public class TestGrammar {
     {
         check("module Gs.Finance\n" +
                 "  where\n" +
-                "\n"+
+                "\n" +
                 "data Trade = Trade { id :: Optional Int, notionals :: Optional [Double] }\n" +
                 "    deriving (Eq, Ord, Show)\n");
     }
@@ -43,7 +57,7 @@ public class TestGrammar {
     {
         check("module Gs.Finance\n" +
                 "  where\n" +
-                "\n"+
+                "\n" +
                 "data Color = Red\n" +
                 "  | Blue\n" +
                 "  | Green\n" +
