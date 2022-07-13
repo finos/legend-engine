71b17d32e43dc8affed32213147a87efc7f6315f
David Lee <david.x.lee@ny.email.gs.com>
2022-07-13 15:25:30 -0400

    Introduce persistence cloud extension modules (#798)
    * Separate persistence cloud model into its own module.
    
    * Create protocol module for cloud persistence.
    
    * Create grammar module for cloud persistence.
    
    * Add parser test for cloud persistence grammar.
    
    * Add roundtrip test for cloud persistence grammar.
    
    * Add compiler test for cloud persistence grammar.
    
    * Add cloud persistence grammar to engine server and test extensions.
    
    * Add copyright headers for cloud persistence tests.
    
    * Set source information correctly when delegating parsing of persistence platform.
    
    * Remove OS metadata files.
    
    * Update to accommodate legend-pure and legend-engine changes.

---
+++ legend-engine-xt-persistence-cloud-grammar/src/main/antlr4/org/finos/legend/engine/language/pure/grammar/from/antlr4/PersistenceCloudLexerGrammar.g4
@@ -0,0 +1,11 @@
+lexer grammar PersistenceCloudLexerGrammar;
+
+import M3LexerGrammar;
+
+// -------------------------------------- KEYWORD --------------------------------------
+
+//**********
+// PLATFORM
+//**********
+
+AWS_GLUE_DPU_COUNT:                         'dataProcessingUnits';
---
+++ legend-engine-xt-persistence-cloud-grammar/src/test/java/org/finos/legend/engine/pure/dsl/persistence/cloud/compiler/test/TestPersistenceCloudCompilationFromGrammar.java
@@ -0,0 +1,187 @@
+// Copyright 2022 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.pure.dsl.persistence.cloud.compiler.test;
+
+import org.eclipse.collections.api.tuple.Pair;
+import org.finos.legend.engine.language.pure.compiler.test.TestCompilationFromGrammar;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
+import org.finos.legend.pure.generated.Root_meta_external_persistence_aws_metamodel_AwsGluePersistencePlatform;
+import org.finos.legend.pure.generated.Root_meta_pure_persistence_metamodel_PersistenceContext;
+import org.finos.legend.pure.generated.Root_meta_pure_persistence_metamodel_context_PersistencePlatform;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.PackageableElement;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class TestPersistenceCloudCompilationFromGrammar extends TestCompilationFromGrammar.TestCompilationFromGrammarTestSuite
+{
+    @Override
+    protected String getDuplicatedElementTestCode()
+    {
+        return "Class test::TestPersistenceContext {}\n" +
+                "\n" +
+                "###Persistence\n" +
+                "\n" +
+                "PersistenceContext test::TestPersistenceContext\n" +
+                "{\n" +
+                "  persistence: test::TestPersistence;\n" +
+                "  platform: AwsGlue\n" +
+                "  #{\n" +
+                "    dataProcessingUnits: 10;\n" +
+                "  }#;\n" +
+                "}\n";
+    }
+
+    @Override
+    protected String getDuplicatedElementTestExpectedErrorMessage()
+    {
+        return "COMPILATION error at [5:1-12:1]: Duplicated element 'test::TestPersistenceContext'";
+    }
+
+    @Test
+    public void persistencePlatformDataProcessingUnits()
+    {
+        Pair<PureModelContextData, PureModel> result = test("Class test::Person\n" +
+                "{\n" +
+                "  name: String[1];\n" +
+                "}\n" +
+                "\n" +
+                "Class test::ServiceResult\n" +
+                "{\n" +
+                "   deleted: String[1];\n" +
+                "   dateTimeIn: String[1];\n" +
+                "}\n" +
+                "\n" +
+                "###Mapping\n" +
+                "Mapping test::Mapping ()\n" +
+                "\n" +
+                "###Service\n" +
+                "Service test::Service \n" +
+                "{\n" +
+                "  pattern : 'test';\n" +
+                "  documentation : 'test';\n" +
+                "  autoActivateUpdates: true;\n" +
+                "  execution: Single\n" +
+                "  {\n" +
+                "    query: src: test::Person[1]|$src.name;\n" +
+                "    mapping: test::Mapping;\n" +
+                "    runtime:\n" +
+                "    #{\n" +
+                "      connections: [];\n" +
+                "    }#;\n" +
+                "  }\n" +
+                "  test: Single\n" +
+                "  {\n" +
+                "    data: 'test';\n" +
+                "    asserts: [];\n" +
+                "  }\n" +
+                "}\n" +
+                "\n" +
+                "###Relational\n" +
+                "Database test::Database\n" +
+                "(\n" +
+                "  Table personTable\n" +
+                "  (\n" +
+                "    ID INTEGER PRIMARY KEY,\n" +
+                "    NAME VARCHAR(100)\n" +
+                "  )\n" +
+                ")" +
+                "\n" +
+                "###Connection" +
+                "\n" +
+                "RelationalDatabaseConnection test::ServiceConnection\n" +
+                "{\n" +
+                "  store: test::TestDatabase;\n" +
+                "  type: Snowflake;\n" +
+                "  specification: LocalH2\n" +
+                "  {\n" +
+                "  };\n" +
+                "  auth: Test;\n" +
+                "}\n" +
+                "\n" +
+                "RelationalDatabaseConnection test::SinkConnection\n" +
+                "{\n" +
+                "  store: test::TestDatabase;\n" +
+                "  type: MemSQL;\n" +
+                "  specification: LocalH2\n" +
+                "  {\n" +
+                "  };\n" +
+                "  auth: Test;\n" +
+                "}\n" +
+                "\n" +
+                "###Persistence\n" +
+                "\n" +
+                "PersistenceContext test::TestPersistenceContext \n" +
+                "\n" +
+                "{\n" +
+                "  persistence: test::TestPersistence;\n" +
+                "  platform: AwsGlue\n" +
+                "  #{\n" +
+                "    dataProcessingUnits: 10;\n" +
+                "  }#;\n" +
+                "}\n" +
+                "\n" +
+                "Persistence test::TestPersistence \n" +
+                "{\n" +
+                "  doc: 'This is test documentation.';\n" +
+                "  trigger: Manual;\n" +
+                "  service: test::Service;\n" +
+                "  persister: Batch\n" +
+                "  {\n" +
+                "    sink: Relational\n" +
+                "    {\n" +
+                "      database: test::Database;" +
+                "    }\n" +
+                "    ingestMode: UnitemporalDelta\n" +
+                "    {\n" +
+                "      mergeStrategy: NoDeletes;\n" +
+                "      transactionMilestoning: DateTime\n" +
+                "      {\n" +
+                "        dateTimeInName: 'IN_Z';\n" +
+                "        dateTimeOutName: 'OUT_Z';\n" +
+                "        derivation: SourceSpecifiesInDateTime\n" +
+                "        {\n" +
+                "          sourceDateTimeInField: 'dateTimeIn';\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "    targetShape: Flat\n" +
+                "    {\n" +
+                "      targetName: 'TestDataset1';\n" +
+                "      modelClass: test::ServiceResult;\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n");
+
+        PureModel model = result.getTwo();
+
+        // persistence context
+        PackageableElement packageableElement = model.getPackageableElement("test::TestPersistenceContext");
+        assertNotNull(packageableElement);
+        assertTrue(packageableElement instanceof Root_meta_pure_persistence_metamodel_PersistenceContext);
+
+        Root_meta_pure_persistence_metamodel_PersistenceContext context = (Root_meta_pure_persistence_metamodel_PersistenceContext) packageableElement;
+
+        Root_meta_pure_persistence_metamodel_context_PersistencePlatform persistencePlatform = context._platform();
+        assertNotNull(persistencePlatform);
+        assertTrue(persistencePlatform instanceof Root_meta_external_persistence_aws_metamodel_AwsGluePersistencePlatform);
+
+        Root_meta_external_persistence_aws_metamodel_AwsGluePersistencePlatform awsGluePlatform = (Root_meta_external_persistence_aws_metamodel_AwsGluePersistencePlatform) persistencePlatform;
+        assertEquals(10, awsGluePlatform._dataProcessingUnits());
+    }
+}
