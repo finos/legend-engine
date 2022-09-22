8ee77a587ac4b8b5b7f97a0f31089660f1e12c9d
Ashutosh <prasar.ashutosh@gmail.com>
2022-09-22 14:49:36 +0800

    add mapper files

---
+++ legend-engine-xt-persistence-test-runner/src/main/java/org/finos/legend/engine/testable/persistence/mapper/BitemporalDeltaMapper.java
@@ -0,0 +1,35 @@
+// Copyright 2022 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.testable.persistence.mapper;
+
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.persister.ingestmode.delta.BitemporalDelta;
+
+import java.util.Arrays;
+
+import static org.finos.legend.engine.testable.persistence.mapper.IngestModeMapper.DIGEST_FIELD_DEFAULT;
+
+public class BitemporalDeltaMapper
+{
+    public static org.finos.legend.engine.persistence.components.ingestmode.BitemporalDelta from(BitemporalDelta bitemporalDelta, String[] pkFields)
+    {
+        return org.finos.legend.engine.persistence.components.ingestmode.BitemporalDelta.builder()
+                .digestField(DIGEST_FIELD_DEFAULT)
+                .mergeStrategy(bitemporalDelta.mergeStrategy.accept(MappingVisitors.MAP_TO_COMPONENT_MERGE_STRATEGY))
+                .addAllKeyFields(Arrays.asList(pkFields))
+                .transactionMilestoning(bitemporalDelta.transactionMilestoning.accept(MappingVisitors.MAP_TO_COMPONENT_TRANSACTION_MILESTONING))
+                .validityMilestoning(bitemporalDelta.validityMilestoning.accept(MappingVisitors.MAP_TO_COMPONENT_VALIDITY_MILESTONING))
+                .build();
+    }
+}
