4dff98fa5dabaeb759b7f519a0534adf219f9025
Ivan Kyosev <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-10-04 17:49:43 +0100

    Specifying GCP region

--- .gcp/cloud-build/build.sh
+++ .gcp/cloud-build/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# We have 64GB of available memory
-# (4 * 14 GB) + 6 GB = 62 GB
-# This leaves 2GB spare for system stability
-
 # printing tool versions
 java --version
 mvn --version
@@ -13,4 +9,4 @@ gpg --version
 export MAVEN_OPTS="-Xms6g -Xmx6g"
 
 # running the build
-mvn -B -e -T 4 install -DargLine="-Xms14g -Xmx14g"
+mvn -B -e -T 4 install -DargLine="-Xms12g -Xmx12g"
