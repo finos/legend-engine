7a2396e327b3dd32b8593b12a98ff6c9ecab1ac7
Ivan Kyosev <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-10-04 19:53:13 +0100

    Updating GCP build script

--- .gcp/cloud-build/build.sh
+++ .gcp/cloud-build/build.sh
@@ -5,8 +5,10 @@ java --version
 mvn --version
 gpg --version
 
+grep MemTotal /proc/meminfo
+
 # setting env variables
-export MAVEN_OPTS="-Xms6g -Xmx6g"
+export MAVEN_OPTS="-Xms10g -Xmx10g"
 
 # running the build
-mvn -B -e -T 4 install -DargLine="-Xms10g -Xmx10g"
+mvn -B -e -T 4 install -DargLine="-Xms12g -Xmx12g"
