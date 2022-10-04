6ab4aae4fd06c7ea7560372a81aaa83761e24b7b
ivan-kyosev-gs <89931850+ivan-kyosev-gs@users.noreply.github.com>
2022-10-04 16:52:37 +0100

    Updating GCP build config (#1010)

--- .gcp/cloud-build/build.sh
+++ .gcp/cloud-build/build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# We have 64GB of available memory
+# (4 * 14 GB) + 6 GB = 62 GB
+# This leaves 2GB spare for system stability
+
 # printing tool versions
 java --version
 mvn --version
 gpg --version
 
 # setting env variables
-export MAVEN_OPTS="-Xms14g -Xmx14g"
+export MAVEN_OPTS="-Xms6g -Xmx6g"
 
 # running the build
-mvn -B -e -T 4 install
+mvn -B -e -T 4 install -DargLine="-Xms14g -Xmx14g"
