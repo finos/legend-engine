6e84fadfa09cf55139c86c0b9869ba5d29fbf96f
Vignesh Manickavasagam <69285843+gs-manvig@users.noreply.github.com>
2022-10-18 14:15:23 -0500

    Add test query builder

---
+++ legend-engine-xt-sql-compiler/src/test/java/org/finos/legend/engine/protocol/sql/GenerateTestQueryBuilder.java
@@ -0,0 +1,36 @@
+// Copyright 2022 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.protocol.sql;
+
+import org.eclipse.collections.impl.factory.Lists;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.language.sql.grammar.from.SQLGrammarParser;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
+import org.finos.legend.engine.protocol.sql.metamodel.Node;
+import org.finos.legend.engine.protocol.sql.metamodel.Translator;
+import org.finos.legend.engine.shared.core.deployment.DeploymentMode;
+
+public class GenerateTestQueryBuilder
+{
+    public static void main(String[] args)
+    {
+        PureModel pureModel = new PureModel(PureModelContextData.newBuilder().build(), Lists.mutable.empty(), DeploymentMode.TEST);
+        String query = "SELECT * FROM table";
+        SQLGrammarParser parser = SQLGrammarParser.newInstance();
+        Node node = parser.parseStatement(query);
+        String res = org.finos.legend.pure.generated.core_pure_protocol_generation_builder_generation.Root_meta_protocols_generation_builder_builderGeneration_Any_1__String_1_(new Translator().translate(node, pureModel), pureModel.getExecutionSupport());
+        System.out.println(res);
+    }
+}
