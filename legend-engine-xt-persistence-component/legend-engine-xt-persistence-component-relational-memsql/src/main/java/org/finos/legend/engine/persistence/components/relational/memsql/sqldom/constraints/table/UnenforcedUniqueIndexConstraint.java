8ddbec71dde87d1950bb97f0861dec0c8a5a97b2
Mythreyi <Mythreyi.Rengarajan@ny.email.gs.com>
2022-10-25 11:20:40 +0800

    Support for memsql create statement with shards and column store specification

---
+++ legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-memsql/src/main/java/org/finos/legend/engine/persistence/components/relational/memsql/sqldom/constraints/table/UnenforcedUniqueIndexConstraint.java
@@ -0,0 +1,67 @@
+// Copyright 2022 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.persistence.components.relational.memsql.sqldom.constraints.table;
+
+import org.finos.legend.engine.persistence.components.relational.memsql.sqldom.common.Clause;
+import org.finos.legend.engine.persistence.components.relational.sqldom.SqlDomException;
+import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.TableConstraint;
+import org.finos.legend.engine.persistence.components.relational.sqldom.utils.SqlGenUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UnenforcedUniqueIndexConstraint extends TableConstraint
+{
+    private static final String INDEX_NAME = "UNENFORCED_UNIQUE_INDEX";
+    private final List<String> columnNames;
+    private final String quoteIdentifier;
+
+    public UnenforcedUniqueIndexConstraint(List<String> columnNames, String quoteIdentifier)
+    {
+        this.columnNames = columnNames;
+        this.quoteIdentifier = quoteIdentifier;
+    }
+
+    public List<String> getColumnNames()
+    {
+        return columnNames;
+    }
+
+    public UnenforcedUniqueIndexConstraint withColumnNames(List<String> columnNames)
+    {
+        return new UnenforcedUniqueIndexConstraint(columnNames, quoteIdentifier);
+    }
+
+    @Override
+    public void genSql(StringBuilder builder) throws SqlDomException
+    {
+        validate();
+        String uniqueColumns = columnNames.stream().map(column -> SqlGenUtils.getQuotedField(column, quoteIdentifier)).collect(Collectors.joining(SqlGenUtils.COMMA + SqlGenUtils.WHITE_SPACE));
+        builder.append(Clause.UNIQUE + SqlGenUtils.WHITE_SPACE + Clause.KEY + SqlGenUtils.WHITE_SPACE);
+        builder.append(INDEX_NAME + SqlGenUtils.WHITE_SPACE);
+        builder.append(SqlGenUtils.OPEN_PARENTHESIS);
+        builder.append(uniqueColumns);
+        builder.append(SqlGenUtils.CLOSING_PARENTHESIS + SqlGenUtils.WHITE_SPACE);
+        builder.append(Clause.UNENFORCED);
+    }
+
+    void validate() throws SqlDomException
+    {
+        if (columnNames == null || columnNames.isEmpty())
+        {
+            throw new SqlDomException("columnNames is empty");
+        }
+    }
+}
