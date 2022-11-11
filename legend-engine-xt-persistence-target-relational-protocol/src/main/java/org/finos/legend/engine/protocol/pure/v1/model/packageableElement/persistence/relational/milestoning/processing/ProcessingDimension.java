553431b137b634526c7de03a3e4613bdd396372a
David Lee <david.x.lee@ny.email.gs.com>
2022-11-11 13:23:10 -0500

    Add protocol module for relational persistence target (#1085)

---
+++ legend-engine-xt-persistence-target-relational-protocol/src/main/java/org/finos/legend/engine/protocol/pure/v1/model/packageableElement/persistence/relational/milestoning/processing/ProcessingDimension.java
@@ -0,0 +1,29 @@
+// Copyright 2022 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.milestoning.processing;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "_type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = BatchId.class, name = "batchId"),
+        @JsonSubTypes.Type(value = ProcessingTime.class, name = "processingTime"),
+        @JsonSubTypes.Type(value = BatchIdAndProcessingTime.class, name = "batchIdAndProcessingTime")
+})
+public abstract class ProcessingDimension
+{
+    public abstract <T> T accept(ProcessingDimensionVisitor<T> visitor);
+}
