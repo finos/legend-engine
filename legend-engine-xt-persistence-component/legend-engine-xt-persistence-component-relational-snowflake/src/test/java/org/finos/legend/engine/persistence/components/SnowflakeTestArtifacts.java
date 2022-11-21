b05f02755ab455a5b405777805915b25f58bbcde
prasar-ashutosh <prasar.ashutosh@gmail.com>
2022-11-22 01:23:54 +0800

    Persistence: Test Code Restructuring for Unitemporal Delta (#1142)
    * Persistence Component: Test refactoring for unitemp delta
    
    * Persistence Component: Test refactoring for unitemp delta
    
    * Persistence Component: Test refactoring for unitemp delta
    
    * Persistence Component: Add REFERENCE to create statements for Memsql
    
    * Persistence Component: Test refactoring for unitemp delta
    
    * Persistence Component: Test refactoring for unitemp delta
    
    * Persistence Component: Add REFERENCE to create statements for Memsql
    
    * Remove the old test classes
    
    * Code review Comments
    
    * Code review Comments For clean up staging test cases

---
+++ legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-snowflake/src/test/java/org/finos/legend/engine/persistence/components/SnowflakeTestArtifacts.java
@@ -0,0 +1,24 @@
+// Copyright 2022 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.persistence.components;
+
+public class SnowflakeTestArtifacts
+{
+    public static String expectedMetadataTableIngestQuery = "INSERT INTO batch_metadata (\"table_name\", \"table_batch_id\", \"batch_start_ts_utc\", \"batch_end_ts_utc\", \"batch_status\")" +
+            " (SELECT 'main',(SELECT COALESCE(MAX(batch_metadata.\"table_batch_id\"),0)+1 FROM batch_metadata as batch_metadata WHERE batch_metadata.\"table_name\" = 'main'),'2000-01-01 00:00:00',SYSDATE(),'DONE')";
+
+    public static String expectedMetadataTableIngestQueryWithUpperCase = "INSERT INTO BATCH_METADATA (\"TABLE_NAME\", \"TABLE_BATCH_ID\", \"BATCH_START_TS_UTC\", \"BATCH_END_TS_UTC\", \"BATCH_STATUS\")" +
+            " (SELECT 'main',(SELECT COALESCE(MAX(batch_metadata.\"TABLE_BATCH_ID\"),0)+1 FROM BATCH_METADATA as batch_metadata WHERE batch_metadata.\"TABLE_NAME\" = 'main'),'2000-01-01 00:00:00',SYSDATE(),'DONE')";
+}
