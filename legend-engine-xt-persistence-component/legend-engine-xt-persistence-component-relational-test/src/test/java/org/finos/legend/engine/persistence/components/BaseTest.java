d6a5383397f3529dea77379d984f20f80c801509
Ashutosh <prasar.ashutosh@gmail.com>
2022-12-01 11:05:09 +0800

    Add Bitemp test scenarios and test cases!

--- legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-test/src/test/java/org/finos/legend/engine/persistence/components/BaseTest.java
+++ legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-test/src/test/java/org/finos/legend/engine/persistence/components/BaseTest.java
@@ -446,7 +446,6 @@ public class BaseTest
             .addFields(deleteIndicator)
             .build();
 
-
     protected SchemaDefinition bitemporalFromOnlyStagingTableSchema = SchemaDefinition.builder()
             .addFields(id)
             .addFields(name)
