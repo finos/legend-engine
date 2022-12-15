ad885253f8b486de6f1c9bc72476a566f593ceca
Kevin Knight <kevin.knight@gs.com>
2022-12-15 09:16:41 -0500

    Add unsafe native Pure functions for compiling and executing Java code

---
+++ legend-engine-xt-java-runtime-compiler/src/main/java/org/finos/legend/engine/external/language/java/runtime/compiler/shared/CompileAndExecuteJava.java
@@ -0,0 +1,214 @@
+//  Copyright 2022 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.finos.legend.engine.external.language.java.runtime.compiler.shared;
+
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Maps;
+import org.eclipse.collections.api.factory.Sets;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.set.MutableSet;
+import org.eclipse.collections.impl.map.mutable.SynchronizedMutableMap;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.Collection;
+import java.util.Set;
+import javax.tools.DiagnosticCollector;
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
+
+public class CompileAndExecuteJava
+{
+    public static CompilationResult compile(Collection<? extends JavaFileObject> javaSources, Iterable<String> options)
+    {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        MemoryFileManager fileManager = new MemoryFileManager(compiler);
+        if (javaSources.isEmpty())
+        {
+            return CompilationResult.success(new MemoryClassLoader(fileManager));
+        }
+
+        DiagnosticCollector<JavaFileObject> diagnosticCollector = new DiagnosticCollector<>();
+        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnosticCollector, options, null, javaSources);
+        return task.call() ?
+                CompilationResult.success(new MemoryClassLoader(fileManager)) :
+                CompilationResult.error(diagnosticCollector);
+    }
+
+    public static ExecutionResult execute(CompilationResult compilationResult, String className, String methodName)
+    {
+        if (!compilationResult.isSuccess())
+        {
+            throw new IllegalArgumentException("Cannot execute: compilation was not successful");
+        }
+        try
+        {
+            Class<?> cls = compilationResult.getClassLoader().loadClass(className);
+            Method method = cls.getMethod(methodName);
+            return ExecutionResult.success(method.invoke(null));
+        }
+        catch (Exception e)
+        {
+            return ExecutionResult.error(e);
+        }
+    }
+
+    private static class MemoryClassLoader extends ClassLoader
+    {
+        private final MemoryFileManager manager;
+        private final MutableSet<String> names = Sets.mutable.empty();
+
+        public MemoryClassLoader(MemoryFileManager manager)
+        {
+            super(null);
+            this.manager = manager;
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException
+        {
+            synchronized (this.names)
+            {
+                if (!this.names.contains(name))
+                {
+                    MemoryJavaClassFileObject mc = this.manager.getClassJavaFile(name);
+                    if (mc != null)
+                    {
+                        this.names.add(name);
+                        byte[] array = mc.getBytes();
+                        return defineClass(name, array, 0, array.length);
+                    }
+                }
+            }
+            return super.findClass(name);
+        }
+    }
+
+    private static class MemoryFileManager extends ForwardingJavaFileManager<StandardJavaFileManager>
+    {
+        private final SynchronizedMutableMap<String, MemoryJavaClassFileObject> codeByName = SynchronizedMutableMap.of(Maps.mutable.empty());
+
+        private MemoryFileManager(StandardJavaFileManager fileManager)
+        {
+            super(fileManager);
+        }
+
+        private MemoryFileManager(JavaCompiler compiler)
+        {
+            this(compiler.getStandardFileManager(null, null, null));
+        }
+
+        @Override
+        public Iterable<JavaFileObject> list(Location location, String packageName, Set<JavaFileObject.Kind> kinds, boolean recurse) throws IOException
+        {
+            if ((location != StandardLocation.CLASS_PATH) || !kinds.contains(JavaFileObject.Kind.CLASS))
+            {
+                return super.list(location, packageName, kinds, recurse);
+            }
+
+            MutableList<JavaFileObject> result = Lists.mutable.withAll(super.list(location, packageName, kinds, recurse));
+            String packagePrefix = packageName + '.';
+            this.codeByName.forEachKeyValue((className, classFile) ->
+            {
+                if (className.startsWith(packagePrefix))
+                {
+                    result.add(classFile);
+                }
+            });
+            return result;
+        }
+
+        @Override
+        public String inferBinaryName(Location location, JavaFileObject file)
+        {
+            if (file instanceof MemoryJavaClassFileObject)
+            {
+                return ((MemoryJavaClassFileObject) file).inferBinaryName();
+            }
+            return super.inferBinaryName(location, file);
+        }
+
+        @Override
+        public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind, FileObject sibling) throws IOException
+        {
+            if (kind == JavaFileObject.Kind.CLASS)
+            {
+                return this.codeByName.getIfAbsentPutWithKey(className, MemoryJavaClassFileObject::new);
+            }
+            return super.getJavaFileForOutput(location, className, kind, sibling);
+        }
+
+        private MemoryJavaClassFileObject getClassJavaFile(String name)
+        {
+            return this.codeByName.get(name);
+        }
+    }
+
+    private static class MemoryJavaClassFileObject extends SimpleJavaFileObject
+    {
+        private byte[] bytes = new byte[0];
+
+        private MemoryJavaClassFileObject(String className)
+        {
+            super(URI.create("memo:///" + className.replace('.', '/') + Kind.CLASS.extension), Kind.CLASS);
+        }
+
+        @Override
+        public InputStream openInputStream()
+        {
+            return new ByteArrayInputStream(getBytes());
+        }
+
+        @Override
+        public OutputStream openOutputStream()
+        {
+            return new ByteArrayOutputStream(1024)
+            {
+                @Override
+                public synchronized void close()
+                {
+                    setBytes(toByteArray());
+                }
+            };
+        }
+
+        private synchronized byte[] getBytes()
+        {
+            return this.bytes;
+        }
+
+        private synchronized void setBytes(byte[] bytes)
+        {
+            this.bytes = bytes;
+        }
+
+        private String inferBinaryName()
+        {
+            String fileName = getName();
+            return fileName.substring(0, fileName.lastIndexOf('.')).replace('/','.');
+        }
+    }
+}
---
+++ legend-engine-xt-java-runtime-compiler/src/main/java/org/finos/legend/engine/external/language/java/runtime/compiler/shared/PureCompileAndExecuteJava.java
@@ -0,0 +1,173 @@
+//  Copyright 2022 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.finos.legend.engine.external.language.java.runtime.compiler.shared;
+
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.ListIterable;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.impl.utility.Iterate;
+import org.finos.legend.pure.m3.navigation.M3Paths;
+import org.finos.legend.pure.m3.navigation.M3Properties;
+import org.finos.legend.pure.m3.navigation.PrimitiveUtilities;
+import org.finos.legend.pure.m3.navigation.ProcessorSupport;
+import org.finos.legend.pure.m3.navigation._package._Package;
+import org.finos.legend.pure.m4.coreinstance.CoreInstance;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+
+public class PureCompileAndExecuteJava
+{
+    private static final String PURE_COMP_RESULT = "meta::external::language::java::compiler::CompilationResult";
+    private static final String PURE_EXEC_RESULT = "meta::external::language::java::compiler::ExecutionResult";
+    private static final String PURE_COMP_AND_EXEC_RESULT = "meta::external::language::java::compiler::CompileAndExecuteResult";
+
+    public static CoreInstance compilePure(Iterable<? extends CoreInstance> javaSources, CoreInstance config, ProcessorSupport processorSupport)
+    {
+        CompilationResult javaCompResult = CompileAndExecuteJava.compile(Iterate.collect(javaSources, PureCompileAndExecuteJava::toJavaFileObject), toCompileOptions(config));
+        return toPureCompilationResult(javaCompResult, processorSupport);
+    }
+
+    public static CoreInstance compileAndExecutePure(Iterable<? extends CoreInstance> javaSources, CoreInstance compConfig, CoreInstance execConfig, ProcessorSupport processorSupport)
+    {
+        CompilationResult javaCompResult = CompileAndExecuteJava.compile(Iterate.collect(javaSources, PureCompileAndExecuteJava::toJavaFileObject), toCompileOptions(compConfig));
+        ExecutionResult javaExecResult = javaCompResult.isSuccess() ? CompileAndExecuteJava.execute(javaCompResult, getExecClassName(execConfig), getExecMethodName(execConfig)) : null;
+        return toPureCompileAndExecuteResult(javaCompResult, javaExecResult, processorSupport);
+    }
+
+    private static JavaFileObject toJavaFileObject(CoreInstance javaSource)
+    {
+        String packageName = PrimitiveUtilities.getStringValue(javaSource.getValueForMetaPropertyToOne("package"));
+        String className = PrimitiveUtilities.getStringValue(javaSource.getValueForMetaPropertyToOne("name"));
+        String content = PrimitiveUtilities.getStringValue(javaSource.getValueForMetaPropertyToOne("content"));
+        return new SimpleJavaFileObject(URI.create("string:///" + packageName.replace('.', '/') + "/" + className + JavaFileObject.Kind.SOURCE.extension), JavaFileObject.Kind.SOURCE)
+        {
+            @Override
+            public CharSequence getCharContent(boolean ignoreEncodingErrors)
+            {
+                return content;
+            }
+
+            @Override
+            public InputStream openInputStream()
+            {
+                return new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
+            }
+        };
+    }
+
+    private static Iterable<String> toCompileOptions(CoreInstance compilationConfig)
+    {
+        if (compilationConfig == null)
+        {
+            return Lists.immutable.empty();
+        }
+
+        ListIterable<? extends CoreInstance> classpath = compilationConfig.getValueForMetaPropertyToMany("classpath");
+        MutableList<String> options = Lists.mutable.empty();
+        if (classpath.notEmpty())
+        {
+            options.add("-classpath");
+            if (File.separatorChar == '\\')
+            {
+                String escaped = "\\" + File.pathSeparator;
+                options.add(classpath.asLazy().collect(e -> PrimitiveUtilities.getStringValue(e).replace(File.pathSeparator, escaped)).makeString(File.pathSeparator));
+            }
+            else
+            {
+                options.add(classpath.asLazy().collect(PrimitiveUtilities::getStringValue).makeString(File.pathSeparator));
+            }
+        }
+        return options;
+    }
+
+    private static String getExecClassName(CoreInstance execConfig)
+    {
+        return PrimitiveUtilities.getStringValue(execConfig.getValueForMetaPropertyToOne("class"));
+    }
+
+    private static String getExecMethodName(CoreInstance execConfig)
+    {
+        return PrimitiveUtilities.getStringValue(execConfig.getValueForMetaPropertyToOne("method"));
+    }
+
+    private static CoreInstance toPureCompilationResult(CompilationResult javaCompResult, ProcessorSupport processorSupport)
+    {
+        CoreInstance pureCompResult = processorSupport.newCoreInstance(null, PURE_COMP_RESULT, null);
+
+        CoreInstance success = processorSupport.newCoreInstance(Boolean.toString(javaCompResult.isSuccess()), M3Paths.Boolean, null);
+        pureCompResult.setKeyValues(toPropertyPath(PURE_COMP_RESULT, "successful"), Lists.immutable.with(success));
+
+        if (!javaCompResult.isSuccess())
+        {
+            ListIterable<CoreInstance> errorMessages = javaCompResult.getErrorMessages().collect(e -> processorSupport.newCoreInstance(e, M3Paths.String, null));
+            pureCompResult.setKeyValues(toPropertyPath(PURE_COMP_RESULT, "errors"), errorMessages);
+        }
+
+        return pureCompResult;
+    }
+
+    private static CoreInstance toPureExecutionResult(ExecutionResult javaExecResult, ProcessorSupport processorSupport)
+    {
+        CoreInstance pureExecResult = processorSupport.newCoreInstance(null, PURE_EXEC_RESULT, null);
+
+        CoreInstance success = processorSupport.newCoreInstance(Boolean.toString(javaExecResult.isSuccess()), M3Paths.Boolean, null);
+        pureExecResult.setKeyValues(toPropertyPath(PURE_EXEC_RESULT, "successful"), Lists.immutable.with(success));
+
+        if (!javaExecResult.isSuccess())
+        {
+            Throwable error = javaExecResult.getError();
+            if (error != null)
+            {
+                StringWriter stringWriter = new StringWriter();
+                try (PrintWriter printWriter = new PrintWriter(stringWriter))
+                {
+                    error.printStackTrace(printWriter);
+                }
+                CoreInstance errorMessage = processorSupport.newCoreInstance(stringWriter.toString(), M3Paths.String, null);
+                pureExecResult.setKeyValues(toPropertyPath(PURE_EXEC_RESULT, "error"), Lists.immutable.with(errorMessage));
+            }
+        }
+
+        return pureExecResult;
+    }
+
+    private static CoreInstance toPureCompileAndExecuteResult(CompilationResult javaCompResult, ExecutionResult javaExecResult, ProcessorSupport processorSupport)
+    {
+        CoreInstance pureCompAndExecResult = processorSupport.newCoreInstance(null, PURE_COMP_AND_EXEC_RESULT, null);
+
+        CoreInstance pureCompResult = toPureCompilationResult(javaCompResult, processorSupport);
+        pureCompAndExecResult.setKeyValues(toPropertyPath(PURE_COMP_AND_EXEC_RESULT, "compilationResult"), Lists.immutable.with(pureCompResult));
+
+        if (javaExecResult != null)
+        {
+            CoreInstance pureExecResult = toPureExecutionResult(javaExecResult, processorSupport);
+            pureCompAndExecResult.setKeyValues(toPropertyPath(PURE_COMP_AND_EXEC_RESULT, "executionResult"), Lists.immutable.with(pureExecResult));
+        }
+        return pureCompAndExecResult;
+    }
+
+    private static ListIterable<String> toPropertyPath(String classPath, String propertyName)
+    {
+        return _Package.convertM3PathToM4(classPath).with(M3Properties.properties).with(propertyName);
+    }
+}
