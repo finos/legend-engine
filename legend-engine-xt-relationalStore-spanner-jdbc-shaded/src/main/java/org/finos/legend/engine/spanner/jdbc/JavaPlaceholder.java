2955e5b088320a8a946a7949a61db4a4d5386f17
pavel yurkin <woodythegreat@yandex.ru>
2022-12-16 14:25:02 +0300

    add JavaPlaceholder

---
+++ legend-engine-xt-relationalStore-spanner-jdbc-shaded/src/main/java/org/finos/legend/engine/spanner/jdbc/JavaPlaceholder.java
@@ -0,0 +1,11 @@
+package org.finos.legend.engine.spanner.jdbc;
+
+/**
+ * This blank is needed to satisfy sonartype which is rejected to load module without java doc jar
+ */
+public class JavaPlaceholder
+{
+    public JavaPlaceholder()
+    {
+    }
+}
