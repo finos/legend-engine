550c9b4e38beaa50be55158260241122a85cb5c9
Sai Sriharsha Annepu <72639930+gs-ssh16@users.noreply.github.com>
2022-12-28 11:22:34 +0530

    Update JavaPlaceholder.java

--- legend-engine-xt-relationalStore-spanner-jdbc-shaded/src/main/java/org/finos/legend/engine/spanner/jdbc/JavaPlaceholder.java
+++ legend-engine-xt-relationalStore-spanner-jdbc-shaded/src/main/java/org/finos/legend/engine/spanner/jdbc/JavaPlaceholder.java
@@ -15,7 +15,7 @@
 package org.finos.legend.engine.spanner.jdbc;
 
 /**
- * This blank is needed to satisfy sonartype which is rejected to load module without java doc jar
+ * Placeholder class required for sonatype requirements of sources and javadoc jars
  */
 public class JavaPlaceholder
 {
