2a273109b19346ca9b7dc7e0bb25aa70284de9aa
Ashutosh <prasar.ashutosh@gmail.com>
2023-01-13 14:04:10 +0800

    Code refactoring of Case Optimizers to use stratgey and reduce code duplication

--- legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-ansi/src/main/java/org/finos/legend/engine/persistence/components/relational/ansi/optimizer/LowerCaseOptimizer.java
+++ legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-ansi/src/main/java/org/finos/legend/engine/persistence/components/relational/ansi/optimizer/LowerCaseOptimizer.java
@@ -14,104 +14,10 @@
 
 package org.finos.legend.engine.persistence.components.relational.ansi.optimizer;
 
-import org.finos.legend.engine.persistence.components.optimizer.CaseConversionOptimizer;
-import org.finos.legend.engine.persistence.components.physicalplan.PhysicalPlanNode;
-import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.ForeignKeyTableConstraint;
-import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.PrimaryKeyTableConstraint;
-import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.TableIndexConstraint;
-import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.UniqueTableConstraint;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.Column;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.expresssions.table.Table;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.statements.ShowCommand;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.statements.TruncateTable;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.values.Field;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.values.Value;
-
-import java.util.stream.Collectors;
-
-public class LowerCaseOptimizer implements CaseConversionOptimizer
+public class LowerCaseOptimizer extends StringCaseOptimizer
 {
-
-    @Override
-    public PhysicalPlanNode optimize(PhysicalPlanNode component)
+    public LowerCaseOptimizer()
     {
-        if (component instanceof Field)
-        {
-            Field field = (Field) component;
-            field.setName(field.getName() != null ? field.getName().toLowerCase() : field.getName());
-            field.setAlias(field.getAlias() != null ? field.getAlias().toLowerCase() : field.getAlias());
-            return field;
-        }
-        else if (component instanceof Value)
-        {
-            Value value = (Value) component;
-            value.setAlias(value.getAlias() != null ? value.getAlias().toLowerCase() : value.getAlias());
-        }
-        else if (component instanceof Column)
-        {
-            Column column = (Column) component;
-            column.setColumnName(column.getColumnName() != null ? column.getColumnName().toLowerCase() : column.getColumnName());
-            return column;
-        }
-        else if (component instanceof Table)
-        {
-            Table table = (Table) component;
-            table.setDb(table.getDb() != null ? table.getDb().toLowerCase() : table.getDb());
-            table.setSchema(table.getSchema() != null ? table.getSchema().toLowerCase() : table.getSchema());
-            table.setTable(table.getTable() != null ? table.getTable().toLowerCase() : table.getTable());
-            return table;
-        }
-        else if (component instanceof TruncateTable)
-        {
-            TruncateTable truncateTable = (TruncateTable) component;
-            if (truncateTable.getTable() != null)
-            {
-                Table table = truncateTable.getTable();
-                table.setDb(table.getDb() != null ? table.getDb().toLowerCase() : table.getDb());
-                table.setSchema(table.getSchema() != null ? table.getSchema().toLowerCase() : table.getSchema());
-                table.setTable(table.getTable() != null ? table.getTable().toLowerCase() : table.getTable());
-            }
-            return truncateTable;
-        }
-        else if (component instanceof ForeignKeyTableConstraint)
-        {
-            ForeignKeyTableConstraint foreignKeyTableConstraint = (ForeignKeyTableConstraint) component;
-            return foreignKeyTableConstraint.withColumnNames(foreignKeyTableConstraint.getColumnNames().stream()
-                .map(String::toLowerCase)
-                .collect(Collectors.toList()));
-        }
-        else if (component instanceof PrimaryKeyTableConstraint)
-        {
-            PrimaryKeyTableConstraint primaryKeyTableConstraint = (PrimaryKeyTableConstraint) component;
-            return primaryKeyTableConstraint.withColumnNames(primaryKeyTableConstraint.getColumnNames().stream()
-                .map(String::toLowerCase)
-                .collect(Collectors.toList()));
-        }
-        else if (component instanceof TableIndexConstraint)
-        {
-            TableIndexConstraint tableIndexConstraint = (TableIndexConstraint) component;
-            return tableIndexConstraint.withColumnNamesAndIndexName(
-                tableIndexConstraint.getColumnNames()
-                    .stream()
-                    .map(String::toLowerCase)
-                    .collect(Collectors.toList()),
-                tableIndexConstraint.getIndexName() == null
-                    ? tableIndexConstraint.getIndexName()
-                    : tableIndexConstraint.getIndexName().toLowerCase());
-        }
-        else if (component instanceof UniqueTableConstraint)
-        {
-            UniqueTableConstraint uniqueTableConstraint = (UniqueTableConstraint) component;
-            return uniqueTableConstraint.withColumnNames(uniqueTableConstraint.getColumnNames().stream()
-                .map(String::toLowerCase)
-                .collect(Collectors.toList()));
-        }
-        else if (component instanceof ShowCommand)
-        {
-            ShowCommand command = (ShowCommand) component;
-            command.setSchemaName(command.getSchemaName() != null ? command.getSchemaName().toLowerCase() : command.getSchemaName());
-            return command;
-        }
-        return component;
+        super(String::toLowerCase);
     }
 }
--- legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-ansi/src/main/java/org/finos/legend/engine/persistence/components/relational/ansi/optimizer/UpperCaseOptimizer.java
+++ legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-ansi/src/main/java/org/finos/legend/engine/persistence/components/relational/ansi/optimizer/UpperCaseOptimizer.java
@@ -14,104 +14,10 @@
 
 package org.finos.legend.engine.persistence.components.relational.ansi.optimizer;
 
-import org.finos.legend.engine.persistence.components.optimizer.CaseConversionOptimizer;
-import org.finos.legend.engine.persistence.components.physicalplan.PhysicalPlanNode;
-import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.ForeignKeyTableConstraint;
-import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.PrimaryKeyTableConstraint;
-import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.TableIndexConstraint;
-import org.finos.legend.engine.persistence.components.relational.sqldom.constraints.table.UniqueTableConstraint;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.Column;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.expresssions.table.Table;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.statements.ShowCommand;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.statements.TruncateTable;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.values.Field;
-import org.finos.legend.engine.persistence.components.relational.sqldom.schemaops.values.Value;
-
-import java.util.stream.Collectors;
-
-public class UpperCaseOptimizer implements CaseConversionOptimizer
+public class UpperCaseOptimizer extends StringCaseOptimizer
 {
-
-    @Override
-    public PhysicalPlanNode optimize(PhysicalPlanNode component)
+    public UpperCaseOptimizer()
     {
-        if (component instanceof Field)
-        {
-            Field field = (Field) component;
-            field.setName(field.getName() != null ? field.getName().toUpperCase() : field.getName());
-            field.setAlias(field.getAlias() != null ? field.getAlias().toUpperCase() : field.getAlias());
-            return field;
-        }
-        else if (component instanceof Value)
-        {
-            Value value = (Value) component;
-            value.setAlias(value.getAlias() != null ? value.getAlias().toUpperCase() : value.getAlias());
-        }
-        else if (component instanceof Column)
-        {
-            Column column = (Column) component;
-            column.setColumnName(column.getColumnName() != null ? column.getColumnName().toUpperCase() : column.getColumnName());
-            return column;
-        }
-        else if (component instanceof Table)
-        {
-            Table table = (Table) component;
-            table.setDb(table.getDb() != null ? table.getDb().toUpperCase() : table.getDb());
-            table.setSchema(table.getSchema() != null ? table.getSchema().toUpperCase() : table.getSchema());
-            table.setTable(table.getTable() != null ? table.getTable().toUpperCase() : table.getTable());
-            return table;
-        }
-        else if (component instanceof TruncateTable)
-        {
-            TruncateTable truncateTable = (TruncateTable) component;
-            if (truncateTable.getTable() != null)
-            {
-                Table table = truncateTable.getTable();
-                table.setDb(table.getDb() != null ? table.getDb().toUpperCase() : table.getDb());
-                table.setSchema(table.getSchema() != null ? table.getSchema().toUpperCase() : table.getSchema());
-                table.setTable(table.getTable() != null ? table.getTable().toUpperCase() : table.getTable());
-            }
-            return truncateTable;
-        }
-        else if (component instanceof ForeignKeyTableConstraint)
-        {
-            ForeignKeyTableConstraint foreignKeyTableConstraint = (ForeignKeyTableConstraint) component;
-            return foreignKeyTableConstraint.withColumnNames(foreignKeyTableConstraint.getColumnNames().stream()
-                .map(String::toUpperCase)
-                .collect(Collectors.toList()));
-        }
-        else if (component instanceof PrimaryKeyTableConstraint)
-        {
-            PrimaryKeyTableConstraint primaryKeyTableConstraint = (PrimaryKeyTableConstraint) component;
-            return primaryKeyTableConstraint.withColumnNames(primaryKeyTableConstraint.getColumnNames().stream()
-                .map(String::toUpperCase)
-                .collect(Collectors.toList()));
-        }
-        else if (component instanceof TableIndexConstraint)
-        {
-            TableIndexConstraint tableIndexConstraint = (TableIndexConstraint) component;
-            return tableIndexConstraint.withColumnNamesAndIndexName(
-                tableIndexConstraint.getColumnNames()
-                    .stream()
-                    .map(String::toUpperCase)
-                    .collect(Collectors.toList()),
-                tableIndexConstraint.getIndexName() == null
-                    ? null
-                    : tableIndexConstraint.getIndexName().toUpperCase());
-        }
-        else if (component instanceof UniqueTableConstraint)
-        {
-            UniqueTableConstraint uniqueTableConstraint = (UniqueTableConstraint) component;
-            return uniqueTableConstraint.withColumnNames(uniqueTableConstraint.getColumnNames().stream()
-                .map(String::toUpperCase)
-                .collect(Collectors.toList()));
-        }
-        else if (component instanceof ShowCommand)
-        {
-            ShowCommand command = (ShowCommand) component;
-            command.setSchemaName(command.getSchemaName() != null ? command.getSchemaName().toUpperCase() : command.getSchemaName());
-            return command;
-        }
-        return component;
+        super(String::toUpperCase);
     }
 }
