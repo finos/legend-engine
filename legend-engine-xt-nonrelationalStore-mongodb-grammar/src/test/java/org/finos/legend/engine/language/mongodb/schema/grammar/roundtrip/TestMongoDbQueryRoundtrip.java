74b36f7783b7d47023e82e58fd2dbdf3eae45bd5
Hugo Goncalves <Hugo.Goncalves@ln.email.gs.com>
2023-01-30 13:14:34 +0000

    removed mongo in-memory test setup, will go in a separate branch and module

--- legend-engine-xt-nonrelationalStore-mongodb-grammar/src/test/java/org/finos/legend/engine/language/mongodb/schema/grammar/roundtrip/TestMongoDbQueryRoundtrip.java
+++ legend-engine-xt-nonrelationalStore-mongodb-grammar/src/test/java/org/finos/legend/engine/language/mongodb/schema/grammar/roundtrip/TestMongoDbQueryRoundtrip.java
@@ -17,7 +17,6 @@ package org.finos.legend.engine.language.mongodb.schema.grammar.roundtrip;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import de.bwaldvogel.mongo.MongoServer;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.finos.legend.engine.language.mongodb.query.grammar.from.antlr4.MongoDbQueryBaseListener;
@@ -28,13 +27,9 @@ import org.finos.legend.engine.language.mongodb.schema.grammar.from.AntlrThrowin
 import org.finos.legend.engine.language.mongodb.schema.grammar.from.MongoDbQueryParseTreeWalker;
 import org.finos.legend.engine.language.mongodb.schema.grammar.to.MongoDbQueryComposer;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.DatabaseCommand;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import utils.InMemoryMongoDbServer;
 import utils.CustomJSONPrettyPrinter;
-import utils.MongoDbClient;
 
 import static utils.TestUtils.resourceAsString;
 import static org.junit.Assert.assertEquals;
@@ -45,56 +40,34 @@ public class TestMongoDbQueryRoundtrip
     private final ObjectMapper mapper = new ObjectMapper().setDefaultPrettyPrinter(new CustomJSONPrettyPrinter())
             .enable(SerializationFeature.INDENT_OUTPUT)
             .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-    private MongoDbClient mongoDbClient;
-    private MongoServer mongoServer;
 
-    @Before
-    public void setUp()
-    {
-        this.mongoServer = InMemoryMongoDbServer.startServer();
-        this.mongoDbClient = new MongoDbClient();
-
-        String firmsJson = resourceAsString("mongoData/firms.json");
-        this.mongoDbClient.insertCollectionItemAsJsonString("firm", firmsJson);
-    }
-
-    @After
-    public void tearDown() throws Exception
-    {
-        this.mongoServer.shutdown();
-        this.mongoDbClient.shutDown();
-    }
 
     @Test
     public void testEmptyAggregate()
     {
-        testParseComposeRoundtrip("empty_pipeline_input.json",
-                "empty_pipeline_output.json",
-                "empty_pipeline_expected_query_result.json");
+        testRoundtrip("empty_pipeline_input.json",
+                "empty_pipeline_output.json");
     }
 
     @Test
     public void testAggregateWithEmptyMatch()
     {
-        testParseComposeRoundtrip("empty_match_input.json",
-                "empty_match_output.json",
-                "empty_match_expected_query_result.json");
+        testRoundtrip("empty_match_input.json",
+                "empty_match_output.json");
     }
 
     @Test
     public void testAggregateWithMatchSimpleExpression()
     {
-        testParseComposeRoundtrip("match_simple_expression_input.json",
-                "match_simple_expression_output.json",
-                "match_simple_expression_expected_query_result.json");
+        testRoundtrip("match_simple_expression_input.json",
+                "match_simple_expression_output.json");
     }
 
     @Test
     public void testAggregateWithMatchExpressionWithOperator()
     {
-        testParseComposeRoundtrip("match_with_operator_input.json",
-                "match_with_operator_output.json",
-                "match_with_operator_expected_query_result.json");
+        testRoundtrip("match_with_operator_input.json",
+                "match_with_operator_output.json");
     }
 
 
@@ -102,33 +75,29 @@ public class TestMongoDbQueryRoundtrip
     @Ignore
     public void testAggregateWithMatchWithVariousComparisonOperatorFormats()
     {
-        testParseComposeRoundtrip("match_with_various_comparison_expression_formats_input.json",
-                "match_with_various_comparison_expression_formats_output.json",
-                "match_with_various_comparison_expression_formats_expected_query_result.json");
+        testRoundtrip("match_with_various_comparison_expression_formats_input.json",
+                "match_with_various_comparison_expression_formats_output.json");
     }
 
     @Test
     public void testAggregateWithMultiMatchExpressionWithoutAndOperator()
     {
-        testParseComposeRoundtrip("two_match_with_operators_input.json",
-                "two_match_with_operators_output.json",
-                "two_match_with_operators_expected_query_result.json");
+        testRoundtrip("two_match_with_operators_input.json",
+                "two_match_with_operators_output.json");
     }
 
     @Test
     public void testAggregateWithMatchExpressionWithOrOperator() throws Exception
     {
-        testParseComposeRoundtrip("match_with_or_operator_input.json",
-                "match_with_or_operator_output.json",
-                "match_with_or_operator_expected_query_result.json");
+        testRoundtrip("match_with_or_operator_input.json",
+                "match_with_or_operator_output.json");
     }
 
     @Test
     public void testAggregateWithMatchExpressionWithAndOperator()
     {
-        testParseComposeRoundtrip("match_with_and_operator_input.json",
-                "match_with_and_operator_output.json",
-                "match_with_and_operator_expected_query_result.json");
+        testRoundtrip("match_with_and_operator_input.json",
+                "match_with_and_operator_output.json");
     }
 
 
@@ -136,73 +105,64 @@ public class TestMongoDbQueryRoundtrip
     @Ignore
     public void testAggregateWithMatchExpressionWithLogicalExpressionAndRegularMatchExpressions()
     {
-        testParseComposeRoundtrip("multi_match_with_gt_with_without_operator_input.json",
-                "multi_match_with_gt_with_without_operator_output.json",
-                "multi_match_with_gt_with_without_operator_expected_query_result.json");
+        testRoundtrip("multi_match_with_gt_with_without_operator_input.json",
+                "multi_match_with_gt_with_without_operator_output.json");
     }
 
     @Test
     public void testAggregateWithMultiMatchExpressionWithEmptyArrayWithoutOperator()
     {
-        testParseComposeRoundtrip("match_with_empty_array_input.json",
-                "match_with_empty_array_output.json",
-                "match_with_empty_array_expected_query_result.json");
+        testRoundtrip("match_with_empty_array_input.json",
+                "match_with_empty_array_output.json");
     }
 
     @Test
     public void testAggregateWithMultiMatchExpressionWithNonEmptyArraysWithAndWithoutOperators()
     {
-        testParseComposeRoundtrip("multi_match_non_empty_array_input.json",
-                "multi_match_non_empty_array_output.json",
-                "multi_match_non_empty_array_expected_query_result.json");
+        testRoundtrip("multi_match_non_empty_array_input.json",
+                "multi_match_non_empty_array_output.json");
     }
 
     @Test
     public void testAggregateWithMultiMatchExpressionWithManyValueTypes() throws Exception
     {
-        testParseComposeRoundtrip("match_many_types_input.json",
-                "match_many_types_output.json",
-                "match_many_types_expected_query_result.json");
+        testRoundtrip("match_many_types_input.json",
+                "match_many_types_output.json");
     }
 
     @Test
     public void testAggregateMatchWithNestedObjectStructure() throws Exception
     {
-        testParseComposeRoundtrip("match_with_nested_object_input.json",
-                "match_with_nested_object_output.json",
-                "match_with_nested_object_expected_query_result.json");
+        testRoundtrip("match_with_nested_object_input.json",
+                "match_with_nested_object_output.json");
     }
 
     @Test
     public void testProjectWithSingleInclusionFilter() throws Exception
     {
-        testParseComposeRoundtrip("project_with_single_inclusion_filter_input.json",
-                "project_with_single_inclusion_filter_output.json",
-                "project_with_single_inclusion_filter_expected_query_result.json");
+        testRoundtrip("project_with_single_inclusion_filter_input.json",
+                "project_with_single_inclusion_filter_output.json");
     }
 
     @Test
     public void testProjectWithSingleComputedField() throws Exception
     {
-        testParseComposeRoundtrip("project_with_single_computed_field_input.json",
-                "project_with_single_computed_field_output.json",
-                "project_with_single_computed_field_expected_query_result.json");
+        testRoundtrip("project_with_single_computed_field_input.json",
+                "project_with_single_computed_field_output.json");
     }
 
     @Test
     public void testProjectWithMultipleFilters() throws Exception
     {
-        testParseComposeRoundtrip("project_with_multiple_filters_input.json",
-                "project_with_multiple_filters_output.json",
-                "project_with_multiple_filters_expected_query_result.json");
+        testRoundtrip("project_with_multiple_filters_input.json",
+                "project_with_multiple_filters_output.json");
     }
 
     @Test
     public void testProjectWithMultipleComplexFilters() throws Exception
     {
-        testParseComposeRoundtrip("project_with_multiple_complex_filters_input.json",
-                "project_with_multiple_complex_filters_output.json",
-                "project_with_multiple_complex_filters_expected_query_result.json");
+        testRoundtrip("project_with_multiple_complex_filters_input.json",
+                "project_with_multiple_complex_filters_output.json");
     }
 
     @Test
@@ -213,24 +173,7 @@ public class TestMongoDbQueryRoundtrip
         assertEquals(exception.getMessage(), "visitProjectFilterValue error");
     }
 
-
-    private void testValidMongoQuery(String mongoQuery, String expectedJsonDataResult)
-    {
-
-        try
-        {
-            String results = this.mongoDbClient.executeNativeQuery(mongoQuery);
-            assertEquals("The result data for MQL string (input) database execution is different from expected.",
-                    mapper.readTree(expectedJsonDataResult).toPrettyString(),
-                    mapper.readTree(results).toPrettyString());
-        }
-        catch (JsonProcessingException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void testParseComposeRoundtrip(String inputMongoQuery, String expectedParsedMongoQuery)
+    private void test(String inputMongoQuery, String expectedParsedMongoQuery)
     {
 
         DatabaseCommand databaseCommand = parseAndWalkDatabaseCommand(inputMongoQuery);
@@ -259,20 +202,15 @@ public class TestMongoDbQueryRoundtrip
         }
     }
 
-    private void testParseComposeRoundtrip(String inputMongoQueryFileLoc, String expectedParsedMongoQueryFileLoc,
-                                           String expectedMongoQueryResultFileLoc)
+    private void testRoundtrip(String inputMongoQueryFileLoc, String expectedParsedMongoQueryFileLoc)
     {
 
         String inputMongoQuery = resourceAsString(inputMongoQueryFileLoc);
         String expectedParsedMongoQuery = resourceAsString(expectedParsedMongoQueryFileLoc);
-        String expectedMongoQueryResult = resourceAsString(expectedMongoQueryResultFileLoc);
-
-        // Test if input mongo query string is valid and that data returned is as expected
-        testValidMongoQuery(inputMongoQuery, expectedMongoQueryResult);
 
         // Test if input mongo query string can be walked into a java model representation and if we can compose this model back
         // into the exact initial input mongo query string
-        testParseComposeRoundtrip(inputMongoQuery, expectedParsedMongoQuery);
+        test(inputMongoQuery, expectedParsedMongoQuery);
 
     }
 
