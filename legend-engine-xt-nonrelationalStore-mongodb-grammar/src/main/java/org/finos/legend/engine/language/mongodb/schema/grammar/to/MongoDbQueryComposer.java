17224659e2e4c41d589a4f1b9841f0a526ace5b3
Theodosios Malatestas <TheodosisTheo.Malatestas@ln.email.gs.com>
2023-01-30 13:23:43 +0100

    composer using the visit pattern, removed stale classes from metamodel

--- legend-engine-xt-nonrelationalStore-mongodb-grammar/src/main/java/org/finos/legend/engine/language/mongodb/schema/grammar/to/MongoDbQueryComposer.java
+++ legend-engine-xt-nonrelationalStore-mongodb-grammar/src/main/java/org/finos/legend/engine/language/mongodb/schema/grammar/to/MongoDbQueryComposer.java
@@ -14,12 +14,12 @@
 
 package org.finos.legend.engine.language.mongodb.schema.grammar.to;
 
-import org.finos.legend.engine.language.mongodb.schema.grammar.from.ComparisonOperator;
-import org.finos.legend.engine.language.mongodb.schema.grammar.from.LogicalOperator;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.AndOperatorExpression;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.ArgumentExpression;
+import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.ArgumentExpressionVisitor;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.ArrayTypeValue;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.BaseTypeValue;
+import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.BaseTypeValueVisitor;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.BoolTypeValue;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.ComparisonOperatorExpression;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.ComputedFieldValue;
@@ -36,6 +36,7 @@ import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.LTE
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.LTOperatorExpression;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.LiteralValue;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.LogicalOperatorExpression;
+import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.LongTypeValue;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.MatchStage;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.NEOperatorExpression;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.NinOperatorExpression;
@@ -47,6 +48,7 @@ import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.Obj
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.OrOperatorExpression;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.ProjectStage;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.Stage;
+import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.StageVisitor;
 import org.finos.legend.engine.protocol.mongodb.schema.metamodel.aggregation.StringTypeValue;
 
 import java.util.List;
@@ -83,173 +85,232 @@ public class MongoDbQueryComposer
 
     private String visitPipelineStages(List<Stage> stages)
     {
-        List<String> strings = stages.stream().map(stage ->
-        {
-            if (stage instanceof MatchStage)
-            {
-                return "{ \"$match\" : " + visitExpression(((MatchStage) stage).expression) + " }";
-            }
-            else if (stage instanceof ProjectStage)
-            {
-                return "{ \"$project\" : " + visitExpression(((ProjectStage) stage).filters) + " }";
-            }
-            throw new RuntimeException("Unknown Stage at visitPipelineStages");
-
-        }).collect(Collectors.toList());
-
+        List<String> strings = stages.stream().map(this::visitPipelineStage).collect(Collectors.toList());
         return String.join(",", strings);
     }
 
-    private String visitExpression(ArgumentExpression expression)
+    private String visitPipelineStage(Stage stage)
+    {
+        return stage.accept(new StageVisitor<String>()
+        {
+
+            @Override
+            public String visit(MatchStage val)
+            {
+                return "{ \"$match\" : " + visitArgumentExpression(((MatchStage) stage).expression) + " }";
+            }
+
+            @Override
+            public String visit(ProjectStage val)
+            {
+                return "{ \"$project\" : " + visitArgumentExpression(((ProjectStage) stage).filters) + " }";
+            }
+        });
+    }
+
+    private String visitArgumentExpression(ArgumentExpression argumentExpression)
     {
 
-        if (expression instanceof LogicalOperatorExpression)
-        {
-
-            List<String> expressionsString = ((LogicalOperatorExpression) expression).expressions.stream()
-                    .map(this::visitExpression).collect(Collectors.toList());
-
-            LogicalOperator op;
-
-            if (expression instanceof OrOperatorExpression)
-            {
-                op = OR;
-            }
-            else if (expression instanceof AndOperatorExpression)
-            {
-                op = AND;
-            }
-            else if (expression instanceof NotOperatorExpression)
-            {
-                op = NOT;
-            }
-            else if (expression instanceof NorOperatorExpression)
-            {
-                op = NOR;
-            }
-            else
-            {
-                throw new RuntimeException("Unknown OperatorExpression expression");
-            }
-
-            return "{ \"" + op.label + "\" : [" + String.join(",", expressionsString) + "] }";
-
-        }
-        else if (expression instanceof ComparisonOperatorExpression)
-        {
-
-            String expString = visitExpression(((ComparisonOperatorExpression) expression).expression);
-            ComparisonOperator op;
-            if (expression instanceof EqOperatorExpression)
-            {
-                op = EQ;
-            }
-            else if (expression instanceof GTOperatorExpression)
-            {
-                op = GT;
-            }
-            else if (expression instanceof LTOperatorExpression)
-            {
-                op = LT;
-            }
-            else if (expression instanceof LTEOperatorExpression)
-            {
-                op = LTE;
-            }
-            else if (expression instanceof GreaterThanEqualsOperatorExpression)
-            {
-                op = GTE;
-            }
-            else if (expression instanceof NEOperatorExpression)
-            {
-                op = NE;
-            }
-            else if (expression instanceof NinOperatorExpression)
-            {
-                op = NIN;
-            }
-            else if (expression instanceof InOperatorExpression)
-            {
-                op = IN;
-            }
-            else
-            {
-                throw new RuntimeException("Unknown OperatorExpression expression");
-            }
-
-            return "{ \"" + op.label + "\" : " + expString + " }";
-        }
-        else if (expression instanceof ObjectExpression)
-        {
-
-            List<String> objPairString = ((ObjectExpression) expression).keyValues.stream()
-                    .map(this::visitKeyValueExpressionPair).collect(Collectors.toList());
-
-            return "{" + String.join(",", objPairString) + "}";
-        }
-        else if (expression instanceof ComputedFieldValue)
-        {
-            return ((ComputedFieldValue) expression).value;
-        }
-        else if (expression instanceof LiteralValue)
-        {
-            return visitBaseType(((LiteralValue) expression).value);
-        }
-        else
+        if (argumentExpression == null)
         {
             return "{}";
         }
+
+        return argumentExpression.accept(new ArgumentExpressionVisitor<String>()
+        {
+
+            @Override
+            public String visit(AndOperatorExpression val)
+            {
+                List<String> expressionsString = val.expressions.stream()
+                        .map(x -> visitArgumentExpression(x)).collect(Collectors.toList());
+                return "{ \"" + AND.label + "\" : [" + String.join(",", expressionsString) + "] }";
+            }
+
+            @Override
+            public String visit(ComparisonOperatorExpression val)
+            {
+                return null;
+            }
+
+            @Override
+            public String visit(ComputedFieldValue val)
+            {
+                return val.value;
+            }
+
+            @Override
+            public String visit(EqOperatorExpression val)
+            {
+                String expString = visit(val.expression);
+                return "{ \"" + EQ.label + "\" : " + expString + " }";
+            }
+
+            @Override
+            public String visit(GTOperatorExpression val)
+            {
+                String expString = visit(val.expression);
+                return "{ \"" + GT.label + "\" : " + expString + " }";
+            }
+
+            @Override
+            public String visit(GreaterThanEqualsOperatorExpression val)
+            {
+                String expString = visit(val.expression);
+                return "{ \"" + GTE.label + "\" : " + expString + " }";
+            }
+
+            @Override
+            public String visit(InOperatorExpression val)
+            {
+                String expString = visit(val.expression);
+                return "{ \"" + IN.label + "\" : " + expString + " }";
+            }
+
+            @Override
+            public String visit(LTEOperatorExpression val)
+            {
+                String expString = visit(val.expression);
+                return "{ \"" + LTE.label + "\" : " + expString + " }";
+            }
+
+            @Override
+            public String visit(LTOperatorExpression val)
+            {
+                String expString = visit(val.expression);
+                return "{ \"" + LT.label + "\" : " + expString + " }";
+            }
+
+            @Override
+            public String visit(LiteralValue val)
+            {
+                return visitBaseTypeValue(val.value);
+            }
+
+            @Override
+            public String visit(LogicalOperatorExpression val)
+            {
+                return null;
+            }
+
+            @Override
+            public String visit(NEOperatorExpression val)
+            {
+                String expString = visit(val.expression);
+                return "{ \"" + NE.label + "\" : " + expString + " }";
+            }
+
+            @Override
+            public String visit(NinOperatorExpression val)
+            {
+                String expString = visit(val.expression);
+                return "{ \"" + NIN.label + "\" : " + expString + " }";
+            }
+
+            @Override
+            public String visit(NorOperatorExpression val)
+            {
+                List<String> expressionsString = val.expressions.stream()
+                        .map(x -> visitArgumentExpression(x)).collect(Collectors.toList());
+                return "{ \"" + NOR.label + "\" : [" + String.join(",", expressionsString) + "] }";
+            }
+
+            @Override
+            public String visit(NotOperatorExpression val)
+            {
+                List<String> expressionsString = val.expressions.stream()
+                        .map(x -> visitArgumentExpression(x)).collect(Collectors.toList());
+                return "{ \"" + NOT.label + "\" : [" + String.join(",", expressionsString) + "] }";
+            }
+
+            @Override
+            public String visit(ObjectExpression val)
+            {
+                List<String> objPairString = val.keyValues.stream()
+                        .map(x -> visitKeyValueExpressionPair(x)).collect(Collectors.toList());
+                return "{" + String.join(",", objPairString) + "}";
+            }
+
+            @Override
+            public String visit(OrOperatorExpression val)
+            {
+                List<String> expressionsString = val.expressions.stream()
+                        .map(x -> visitArgumentExpression(x)).collect(Collectors.toList());
+                return "{ \"" + OR.label + "\" : [" + String.join(",", expressionsString) + "] }";
+            }
+        });
     }
 
-    private String visitBaseType(BaseTypeValue value)
+    public String visitBaseTypeValue(BaseTypeValue value)
     {
+        return value.accept(new BaseTypeValueVisitor<String>()
+        {
+            @Override
+            public String visit(ArrayTypeValue val)
+            {
+                List<String> arrayItemString = ((ArrayTypeValue) value).items.stream().map(x -> visitBaseTypeValue(x)).collect(Collectors.toList());
 
-        if (value instanceof StringTypeValue)
-        {
-            return ((StringTypeValue) value).value;
-        }
-        else if (value instanceof IntTypeValue)
-        {
-            return String.valueOf(((IntTypeValue) value).value);
-        }
-        else if (value instanceof DecimalTypeValue)
-        {
-            return String.valueOf(((DecimalTypeValue) value).value);
-        }
-        else if (value instanceof BoolTypeValue)
-        {
-            return String.valueOf(((BoolTypeValue) value).value);
-        }
-        else if (value instanceof NullTypeValue)
-        {
-            return null;
-        }
-        else if (value instanceof ObjectTypeValue)
-        {
-            List<String> objPairString = ((ObjectTypeValue) value).keyValues.stream()
-                    .map(this::visitKeyValuePair).collect(Collectors.toList());
-            return "{" + String.join(",", objPairString) + "}";
-        }
-        else if (value instanceof ArrayTypeValue)
-        {
-            List<String> arrayItemString = ((ArrayTypeValue) value).items.stream().map(this::visitBaseType).collect(Collectors.toList());
+                return "[" + String.join(",", arrayItemString) + "]";
+            }
 
-            return "[" + String.join(",", arrayItemString) + "]";
-        }
-        throw new RuntimeException("Unknown expression at visitLiteralValue");
+            @Override
+            public String visit(BoolTypeValue val)
+            {
+                return String.valueOf(val.value);
+            }
+
+            @Override
+            public String visit(DecimalTypeValue val)
+            {
+                return String.valueOf(val.value);
+            }
+
+            @Override
+            public String visit(IntTypeValue val)
+            {
+                return String.valueOf(val.value);
+            }
+
+            @Override
+            public String visit(LongTypeValue val)
+            {
+                // not used
+                return String.valueOf(val);
+            }
+
+            @Override
+            public String visit(NullTypeValue val)
+            {
+                return null;
+            }
+
+            @Override
+            public String visit(ObjectTypeValue val)
+            {
+                List<String> objPairString = val.keyValues.stream()
+                        .map(x -> visitKeyValuePair(x)).collect(Collectors.toList());
+                return "{" + String.join(",", objPairString) + "}";
+            }
+
+            @Override
+            public String visit(StringTypeValue val)
+            {
+                return String.valueOf(val.value);
+            }
+        });
     }
 
     private String visitKeyValuePair(KeyValuePair pair)
     {
         String field = pair.key;
-        String value = visitBaseType(pair.value);
+        String value = visitBaseTypeValue(pair.value);
         return field + " : " + value;
     }
 
     public String visitKeyValueExpressionPair(KeyValueExpressionPair pair)
     {
         String field = pair.field;
-        String value = visitExpression(pair.argument);
+        String value = visitArgumentExpression(pair.argument);
         return field + " : " + value;
     }
 
