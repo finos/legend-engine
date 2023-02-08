d9066e07fb561bfc1bc56a045b9f02ac5c286f7b
Leonid Shtivelman <leonid.shtivelman@gs.com>
2023-02-07 23:23:44 -0500

    Add copyright from CrateDB and cleanup packages

---
+++ legend-engine-xt-postgres-server/src/main/java/org/finos/legend/engine/postgres/handler/legend/LegendResultSetMetaData.java
@@ -0,0 +1,81 @@
+// Copyright 2023 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.finos.legend.engine.postgres.handler.legend;
+
+import org.finos.legend.engine.postgres.handler.PostgresResultSetMetaData;
+import java.sql.Types;
+import java.util.List;
+
+public class LegendResultSetMetaData implements PostgresResultSetMetaData
+{
+
+    private List<LegendColumn> legendColumns;
+
+    public LegendResultSetMetaData(List<LegendColumn> legendColumns)
+    {
+        this.legendColumns = legendColumns;
+    }
+
+    @Override
+    public int getColumnCount() throws Exception
+    {
+        return legendColumns.size();
+    }
+
+    @Override
+    public String getColumnName(int i) throws Exception
+    {
+        return getColumnPrivate(i).getName();
+    }
+
+    private LegendColumn getColumnPrivate(int i)
+    {
+        return legendColumns.get(i - 1);
+    }
+
+    @Override
+    public int getColumnType(int i) throws Exception
+    {
+        String legendType = getColumnPrivate(i).getType();
+        switch (legendType)
+        {
+
+            case "StrictDate":
+                return Types.DATE;
+            case "Date":
+                return Types.DATE;
+            case "DateTime":
+                return Types.TIMESTAMP;
+            case "Integer":
+                return Types.INTEGER;
+            case "Float":
+                return Types.FLOAT;
+            case "Number":
+                return Types.DOUBLE;
+            case "Boolean":
+                return Types.BOOLEAN;
+            default:
+                return Types.VARCHAR;
+
+        }
+
+    }
+
+    @Override
+    public int getScale(int i) throws Exception
+    {
+        return 0;
+    }
+}
---
+++ legend-engine-xt-postgres-server/src/main/java/org/finos/legend/engine/postgres/types/PGTypes.java
@@ -0,0 +1,216 @@
+/*
+ * Licensed to Crate.io GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package org.finos.legend.engine.postgres.types;
+
+import com.google.common.collect.ImmutableMap;
+import io.netty.util.collection.IntObjectHashMap;
+import io.netty.util.collection.IntObjectMap;
+import java.sql.Types;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import javax.annotation.Nullable;
+
+public class PGTypes
+{
+
+  private static final Map<Integer, PGType> SQL_TO_PG_TYPES = ImmutableMap.<Integer, PGType>builder()
+
+      //.put(Types.BYTE, CharType.INSTANCE)
+      .put(Types.VARCHAR, VarCharType.INSTANCE)
+      .put(Types.CHAR, CharType.INSTANCE)
+      .put(Types.BOOLEAN, BooleanType.INSTANCE)
+      .put(Types.BIT, BitType.INSTANCE)
+      //.put(DataTypes.UNTYPED_OBJECT, JsonType.INSTANCE)
+      //.put(RowType.EMPTY, RecordType.EMPTY_RECORD)
+      .put(Types.SMALLINT, SmallIntType.INSTANCE)
+      .put(Types.INTEGER, IntegerType.INSTANCE)
+      .put(Types.BIGINT, BigIntType.INSTANCE)
+      .put(Types.FLOAT, RealType.INSTANCE)
+      .put(Types.DOUBLE, DoubleType.INSTANCE)
+      .put(Types.NUMERIC, NumericType.INSTANCE)
+      //.put(Types.TIMESTAMP_WITH_TIMEZONE, TimeTZType.INSTANCE)
+      //.put(Types.TIMESTAMP, TimestampZType.INSTANCE)
+      .put(Types.TIMESTAMP, TimestampType.INSTANCE)
+      .put(Types.DATE, DateType.INSTANCE)
+
+      .build();
+     /*   .put(DataTypes.IP, VarCharType.INSTANCE) // postgres has no IP type, so map it to varchar - it matches the client representation
+        .put(DataTypes.UNDEFINED, VarCharType.INSTANCE)
+        .put(DataTypes.GEO_SHAPE, JsonType.INSTANCE)
+        .put(io.crate.types.JsonType.INSTANCE, JsonType.INSTANCE)
+        .put(DataTypes.GEO_POINT, PointType.INSTANCE)
+        .put(DataTypes.INTERVAL, IntervalType.INSTANCE)
+        .put(DataTypes.REGPROC, RegprocType.INSTANCE)
+        .put(DataTypes.REGCLASS, RegclassType.INSTANCE)
+        .put(BitStringType.INSTANCE_ONE, BitType.INSTANCE)
+        .put(new ArrayType<>(DataTypes.BYTE), PGArray.CHAR_ARRAY)
+        .put(new ArrayType<>(DataTypes.SHORT), PGArray.INT2_ARRAY)
+        .put(new ArrayType<>(DataTypes.INTEGER), PGArray.INT4_ARRAY)
+        .put(new ArrayType<>(DataTypes.LONG), PGArray.INT8_ARRAY)
+        .put(new ArrayType<>(DataTypes.FLOAT), PGArray.FLOAT4_ARRAY)
+        .put(new ArrayType<>(DataTypes.DOUBLE), PGArray.FLOAT8_ARRAY)
+        .put(new ArrayType<>(DataTypes.NUMERIC), PGArray.NUMERIC_ARRAY)
+        .put(new ArrayType<>(DataTypes.BOOLEAN), PGArray.BOOL_ARRAY)
+        .put(new ArrayType<>(DataTypes.TIMESTAMPZ), PGArray.TIMESTAMPZ_ARRAY)
+        .put(new ArrayType<>(DataTypes.TIMESTAMP), PGArray.TIMESTAMP_ARRAY)
+        .put(new ArrayType<>(DataTypes.DATE), PGArray.DATE_ARRAY)
+        .put(new ArrayType<>(DataTypes.TIMETZ), PGArray.TIMETZ_ARRAY)
+        .put(new ArrayType<>(DataTypes.STRING), PGArray.VARCHAR_ARRAY)
+        .put(new ArrayType<>(DataTypes.CHARACTER), PGArray.CHARACTER_ARRAY)
+        .put(new ArrayType<>(DataTypes.IP), PGArray.VARCHAR_ARRAY)
+        .put(new ArrayType<>(DataTypes.UNTYPED_OBJECT), PGArray.JSON_ARRAY)
+        .put(new ArrayType<>(DataTypes.GEO_POINT), PGArray.POINT_ARRAY)
+        .put(new ArrayType<>(DataTypes.GEO_SHAPE), PGArray.JSON_ARRAY)
+        .put(new ArrayType<>(DataTypes.INTERVAL), PGArray.INTERVAL_ARRAY)
+        .put(new ArrayType<>(RowType.EMPTY), PGArray.EMPTY_RECORD_ARRAY)
+        .put(new ArrayType<>(DataTypes.REGPROC), PGArray.REGPROC_ARRAY)
+        .put(new ArrayType<>(DataTypes.REGCLASS), PGArray.REGCLASS_ARRAY)
+        .put(new ArrayType<>(BitStringType.INSTANCE_ONE), PGArray.BIT_ARRAY)
+        .put(DataTypes.OIDVECTOR, PgOidVectorType.INSTANCE)*/
+
+
+  private static final IntObjectMap<Integer> PG_TYPES_TO_SQL_TYPE = new IntObjectHashMap<>();
+  private static final Set<PGType> TYPES;
+
+  static
+  {
+    for (Map.Entry<Integer, PGType> e : SQL_TO_PG_TYPES.entrySet())
+    {
+      int oid = e.getValue().oid();
+      // crate string and ip types both map to pg varchar, avoid overwriting the mapping that is first established.
+      if (!PG_TYPES_TO_SQL_TYPE.containsKey(oid))
+      {
+        PG_TYPES_TO_SQL_TYPE.put(oid, e.getKey());
+      }
+    }
+/*        PG_TYPES_TO_SQL_TYPE.put(0, DataTypes.UNDEFINED);
+        PG_TYPES_TO_SQL_TYPE.put(VarCharType.TextType.OID, DataTypes.STRING);
+        PG_TYPES_TO_SQL_TYPE.put(PGArray.TEXT_ARRAY.oid(), new ArrayType<>(DataTypes.STRING));
+        PG_TYPES_TO_SQL_TYPE.put(AnyType.OID, DataTypes.UNDEFINED);
+        PG_TYPES_TO_SQL_TYPE.put(PGArray.ANY_ARRAY.oid(), new ArrayType<>(DataTypes.UNDEFINED));
+        PG_TYPES_TO_SQL_TYPE.put(VarCharType.NameType.OID, DataTypes.STRING);
+        PG_TYPES_TO_SQL_TYPE.put(OidType.OID, DataTypes.INTEGER);*/
+    TYPES = new HashSet<>(
+        SQL_TO_PG_TYPES.values()); // some pgTypes are used multiple times, de-dup them
+
+    // There is no entry in `CRATE_TO_PG_TYPES` for these because we have no 1:1 mapping from dataType to pgType
+    //TYPES.add(AnyType.INSTANCE);
+    // TYPES.add(PGArray.ANY_ARRAY);
+    TYPES.add(VarCharType.NameType.INSTANCE);
+    TYPES.add(OidType.INSTANCE);
+
+    // We map DataTypes.STRING to varchar (for no good reason, other than history)
+    // But want to expose text additionally as well
+    TYPES.add(VarCharType.TextType.INSTANCE);
+    TYPES.add(PGArray.TEXT_ARRAY);
+  }
+
+  public static Iterable<PGType> pgTypes()
+  {
+    return TYPES;
+  }
+
+  @Nullable
+  public static int fromOID(int oid)
+  {
+    return PG_TYPES_TO_SQL_TYPE.get(oid);
+  }
+
+  public static PGType get(int columnType, int scale)
+  {
+
+    //TODO why do we need this block?
+    if (columnType == Types.CHAR && scale != 1)
+    {
+      return VarCharType.INSTANCE;
+    }
+
+    PGType pgType = SQL_TO_PG_TYPES.get(columnType);
+    if (pgType == null)
+    {
+      throw new IllegalArgumentException(String.format(Locale.ENGLISH,
+          "No type mapping from '%s' to pg_type'", columnType));
+    }
+    return pgType;
+  }
+
+    /*public static PGType get(DataType<?> type) {
+        switch (type.id()) {
+            case ArrayType.ID: {
+                DataType<?> innerType = ((ArrayType<?>) type).innerType();
+                if (innerType.id() == ArrayType.ID) {
+                    // if this is a nested collection stream it as JSON because
+                    // postgres binary format doesn't support multidimensional arrays
+                    // with sub-arrays of different length
+                    // (something like [ [1, 2], [3] ] is not supported)
+                    return JsonType.INSTANCE;
+                } else if (innerType.id() == ObjectType.ID) {
+                    return PGArray.JSON_ARRAY;
+                } else if (innerType.id() == RowType.ID) {
+                    return new PGArray(PGArray.EMPTY_RECORD_ARRAY.oid(), get(innerType));
+                }
+
+                PGType pgType = SQL_TO_PG_TYPES.get(type);
+                if (pgType == null) {
+                    PGType innerPGType = get(innerType);
+                    if (innerPGType == null) {
+                        throw new IllegalArgumentException(
+                                String.format(Locale.ENGLISH, "No type mapping from '%s' to pg_type", type.getName()));
+                    } else {
+                        return new PGArray(innerPGType.typArray(), innerPGType);
+                    }
+                }
+                return pgType;
+            }
+            case ObjectType.ID: {
+                return JsonType.INSTANCE;
+            }
+
+            case RowType.ID:
+                return new RecordType(Lists2.map(((RowType) type).fieldTypes(), PGTypes::get));
+
+            case StringType.ID:
+                return VarCharType.INSTANCE;
+
+            case io.crate.types.CharacterType.ID:
+                return CharacterType.INSTANCE;
+
+            case io.crate.types.NumericType.ID:
+                return NumericType.INSTANCE;
+
+            case BitStringType.ID:
+                return new BitType(((BitStringType) type).length());
+
+            default: {
+                PGType pgType = SQL_TO_PG_TYPES.get(type);
+                if (pgType == null) {
+                    throw new IllegalArgumentException(
+                            String.format(Locale.ENGLISH, "No type mapping from '%s' to pg_type", type.getName()));
+                }
+                return pgType;
+            }
+        }
+
+    }*/
+}
