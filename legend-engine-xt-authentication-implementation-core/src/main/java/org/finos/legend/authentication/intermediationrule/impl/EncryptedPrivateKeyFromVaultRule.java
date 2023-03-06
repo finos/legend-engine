2efc872e46277c4ffc4b2791a52a61e67c4b0d6e
jinanisha <anisha.jindal@ny.email.gs.com>
2023-03-06 10:37:46 +0530

    Revert "Refactor to use snowflake bouncycastle in EncryptedPrivateKeyFromVaultRule"
    This reverts commit 99e10ee81ddffca01d09efd397a7bf04d96cab10.

--- legend-engine-xt-authentication-implementation-core/src/main/java/org/finos/legend/authentication/intermediationrule/impl/EncryptedPrivateKeyFromVaultRule.java
+++ legend-engine-xt-authentication-implementation-core/src/main/java/org/finos/legend/authentication/intermediationrule/impl/EncryptedPrivateKeyFromVaultRule.java
@@ -14,14 +14,14 @@
 
 package org.finos.legend.authentication.intermediationrule.impl;
 
-import net.snowflake.client.jdbc.internal.org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import net.snowflake.client.jdbc.internal.org.bouncycastle.jce.provider.BouncyCastleProvider;
-import net.snowflake.client.jdbc.internal.org.bouncycastle.openssl.PEMParser;
-import net.snowflake.client.jdbc.internal.org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
-import net.snowflake.client.jdbc.internal.org.bouncycastle.openssl.jcajce.JceOpenSSLPKCS8DecryptorProviderBuilder;
-import net.snowflake.client.jdbc.internal.org.bouncycastle.operator.InputDecryptorProvider;
-import net.snowflake.client.jdbc.internal.org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
-import net.snowflake.client.jdbc.internal.apache.commons.codec.binary.Base64;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.jcajce.JceOpenSSLPKCS8DecryptorProviderBuilder;
+import org.bouncycastle.operator.InputDecryptorProvider;
+import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
+import org.bouncycastle.util.encoders.Base64;
 import org.eclipse.collections.api.RichIterable;
 import org.eclipse.collections.impl.factory.Strings;
 import org.finos.legend.authentication.intermediationrule.IntermediationRule;
@@ -75,7 +75,7 @@ public class EncryptedPrivateKeyFromVaultRule extends IntermediationRule<Encrypt
                     String encryptedPrivateKeyString = privateKey;
                     encryptedPrivateKeyString = encryptedPrivateKeyString.replace("-----BEGIN ENCRYPTED PRIVATE KEY-----", "");
                     encryptedPrivateKeyString = encryptedPrivateKeyString.replace("-----END ENCRYPTED PRIVATE KEY-----", "");
-                    EncryptedPrivateKeyInfo pki = new EncryptedPrivateKeyInfo(Base64.decodeBase64(encryptedPrivateKeyString));
+                    EncryptedPrivateKeyInfo pki = new EncryptedPrivateKeyInfo(Base64.decode(encryptedPrivateKeyString));
                     PBEKeySpec privateKeySpec = new PBEKeySpec(passPhrase.toCharArray());
                     SecretKeyFactory pbeKeyFactory = SecretKeyFactory.getInstance(pki.getAlgName());
                     PKCS8EncodedKeySpec encodedKeySpec = pki.getKeySpec(pbeKeyFactory.generateSecret(privateKeySpec));
