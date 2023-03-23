7fb946f41c948bccb4788edff387fd3e0f8fa26e
Hardik Maheshwari <19693874+hardikmaheshwari@users.noreply.github.com>
2023-03-23 21:09:57 +0530

    Revert "Revert "Delete Legacy Json Serialize/Deserialize nodes (#1397)" (#1564)" (#1565)
    This reverts commit b410d5a9e6d694c29653a6714ff5a896e5f9bc59.

--- legend-engine-extensions-collection-execution/src/test/java/org/finos/legend/engine/extensions/collection/execution/TestExtensions.java
+++ legend-engine-extensions-collection-execution/src/test/java/org/finos/legend/engine/extensions/collection/execution/TestExtensions.java
@@ -19,7 +19,6 @@ import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.api.set.MutableSet;
 import org.eclipse.collections.impl.factory.Sets;
 import org.finos.legend.engine.external.format.flatdata.FlatDataRuntimeExtension;
-import org.finos.legend.engine.external.format.json.JsonExecutionExtension;
 import org.finos.legend.engine.external.format.json.JsonSchemaRuntimeExtension;
 import org.finos.legend.engine.external.format.xml.XsdRuntimeExtension;
 import org.finos.legend.engine.external.shared.runtime.ExternalFormatExecutionExtension;
@@ -46,7 +45,6 @@ public class TestExtensions
         MutableList<Class<? extends ExecutionExtension>> expectedExtensions = Lists.mutable.<Class<? extends ExecutionExtension>>empty()
                 .with(RelationalExecutionExtension.class)
                 .with(ExternalFormatExecutionExtension.class)
-                .with(JsonExecutionExtension.class)
                 .with(ServiceStoreExecutionExtension.class);
         assertHasExtensions(expectedExtensions, ExecutionExtension.class);
     }
