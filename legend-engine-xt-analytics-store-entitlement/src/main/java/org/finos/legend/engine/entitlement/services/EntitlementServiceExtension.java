6e424575285ee85feb8e094687b15358d64af535
Yannan <Yannan.Gao@ny.email.gs.com>
2023-03-28 23:34:30 -0400

    support check extensible entitlements analytics

---
+++ legend-engine-xt-analytics-store-entitlement/src/main/java/org/finos/legend/engine/entitlement/services/EntitlementServiceExtension.java
@@ -0,0 +1,39 @@
+//  Copyright 2023 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.finos.legend.engine.entitlement.services;
+
+import org.eclipse.collections.api.list.MutableList;
+import org.finos.legend.engine.entitlement.model.entitlementReport.DatasetEntitlementReport;
+import org.finos.legend.engine.entitlement.model.specification.DatasetSpecification;
+import org.finos.legend.engine.language.pure.modelManager.ModelManager;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContext;
+import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.raw.Lambda;
+import org.pac4j.core.profile.CommonProfile;
+
+import java.util.Collections;
+import java.util.List;
+
+public interface EntitlementServiceExtension
+{
+   default List<DatasetSpecification> generateDatasetSpecifications(Lambda query, Runtime runtime, String mapping, PureModelContext model, ModelManager modelManager, MutableList<CommonProfile> profiles)
+   {
+      return Collections.emptyList();
+   }
+
+   default List<DatasetEntitlementReport> generateDatasetEntitlementReports(Lambda query, Runtime runtime, String mapping, PureModelContext model, ModelManager modelManager, MutableList<CommonProfile> profiles)
+   {
+      return Collections.emptyList();
+   }
+}
