c82d76a6dd7599a147411e263bcc190e897786c8
kumuwu <e0203066@u.nus.edu>
2023-05-16 14:29:43 +0800

    Complete composer for persistence relational + add tests

--- legend-engine-xt-persistence-target-relational-grammar/src/main/java/org/finos/legend/engine/language/pure/dsl/persistence/relational/grammar/to/HelperPersistenceRelationalComposer.java
+++ legend-engine-xt-persistence-target-relational-grammar/src/main/java/org/finos/legend/engine/language/pure/dsl/persistence/relational/grammar/to/HelperPersistenceRelationalComposer.java
@@ -21,6 +21,18 @@ import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persist
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.Temporality;
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.TemporalityVisitor;
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.Unitemporal;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.processing.BatchId;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.processing.BatchIdAndDateTime;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.processing.ProcessingDateTime;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.processing.ProcessingDimension;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.processing.ProcessingDimensionVisitor;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.sourcederived.SourceDerivedDimension;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.sourcederived.SourceDerivedDimensionVisitor;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.sourcederived.SourceDerivedTime;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.sourcederived.SourceTimeFields;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.sourcederived.SourceTimeFieldsVisitor;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.sourcederived.SourceTimeStart;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.persistence.relational.temporality.sourcederived.SourceTimeStartAndEnd;
 
 import static org.finos.legend.engine.language.pure.grammar.to.PureGrammarComposerUtility.getTabString;
 
@@ -67,8 +79,9 @@ public class HelperPersistenceRelationalComposer
         @Override
         public String visitUnitemporal(Unitemporal val)
         {
-            return "Unitemporal" +
+            return "Unitemporal\n" +
                 getTabString(indentLevel) + "{\n" +
+                renderProcessingDimension(val.processingDimension, indentLevel + 1) +
                 getTabString(indentLevel) + "}\n";
         }
 
@@ -77,6 +90,113 @@ public class HelperPersistenceRelationalComposer
         {
             return "Bitemporal\n" +
                 getTabString(indentLevel) + "{\n" +
+                renderProcessingDimension(val.processingDimension, indentLevel + 1) +
+                renderSourceDerivedDimension(val.sourceDerivedDimension, indentLevel + 1) +
+                getTabString(indentLevel) + "}\n";
+        }
+
+        public static String renderProcessingDimension(ProcessingDimension processingDimension, int indentLevel)
+        {
+            return getTabString(indentLevel) + "processingDimension: " + processingDimension.accept(new ProcessingDimensionComposer(indentLevel));
+        }
+
+        public static String renderSourceDerivedDimension(SourceDerivedDimension sourceDerivedDimension, int indentLevel)
+        {
+            return getTabString(indentLevel) + "sourceDerivedDimension: " + sourceDerivedDimension.accept(new SourceDerivedDimensionComposer(indentLevel));
+        }
+    }
+
+    private static class ProcessingDimensionComposer implements ProcessingDimensionVisitor<String>
+    {
+        private final int indentLevel;
+
+        private ProcessingDimensionComposer(int indentLevel)
+        {
+            this.indentLevel = indentLevel;
+        }
+
+        @Override
+        public String visitBatchId(BatchId val)
+        {
+            return "BatchId\n" +
+                getTabString(indentLevel) + "{\n" +
+                getTabString(indentLevel + 1) + "batchIdIn: " + val.batchIdIn + ";\n" +
+                getTabString(indentLevel + 1) + "batchIdOut: " + val.batchIdOut + ";\n" +
+                getTabString(indentLevel) + "}\n";
+        }
+
+        @Override
+        public String visitDateTime(ProcessingDateTime val)
+        {
+            return "DateTime\n" +
+                getTabString(indentLevel) + "{\n" +
+                getTabString(indentLevel + 1) + "dateTimeIn: " + val.timeIn + ";\n" +
+                getTabString(indentLevel + 1) + "dateTimeOut: " + val.timeOut + ";\n" +
+                getTabString(indentLevel) + "}\n";
+        }
+
+        @Override
+        public String visitBatchIdAndDateTime(BatchIdAndDateTime val)
+        {
+            return "BatchIdAndDateTime\n" +
+                getTabString(indentLevel) + "{\n" +
+                getTabString(indentLevel + 1) + "batchIdIn: " + val.batchIdIn + ";\n" +
+                getTabString(indentLevel + 1) + "batchIdOut: " + val.batchIdOut + ";\n" +
+                getTabString(indentLevel + 1) + "dateTimeIn: " + val.timeIn + ";\n" +
+                getTabString(indentLevel + 1) + "dateTimeOut: " + val.timeOut + ";\n" +
+                getTabString(indentLevel) + "}\n";
+        }
+    }
+
+    private static class SourceDerivedDimensionComposer implements SourceDerivedDimensionVisitor<String>
+    {
+        private final int indentLevel;
+
+        private SourceDerivedDimensionComposer(int indentLevel)
+        {
+            this.indentLevel = indentLevel;
+        }
+
+        @Override
+        public String visitSourceDerivedTime(SourceDerivedTime val)
+        {
+            return "DateTime\n" +
+                getTabString(indentLevel) + "{\n" +
+                getTabString(indentLevel + 1) + "dateTimeStart: " + val.timeStart + ";\n" +
+                getTabString(indentLevel + 1) + "dateTimeEnd: " + val.timeEnd + ";\n" +
+                renderSourceTimeFields(val.sourceTimeFields, indentLevel + 1) +
+                getTabString(indentLevel) + "}\n";
+        }
+
+        public static String renderSourceTimeFields(SourceTimeFields sourceTimeFields, int indentLevel)
+        {
+            return getTabString(indentLevel) + "sourceFields: " + sourceTimeFields.accept(new SourceTimeFieldsComposer(indentLevel));
+        }
+    }
+
+    private static class SourceTimeFieldsComposer implements SourceTimeFieldsVisitor<String>
+    {
+        private int indentLevel;
+
+        private SourceTimeFieldsComposer(int indentLevel)
+        {
+            this.indentLevel = indentLevel;
+        }
+
+        @Override
+        public String visit(SourceTimeStart val) {
+            return "Start\n" +
+                getTabString(indentLevel) + "{\n" +
+                getTabString(indentLevel + 1) + "startField: " + val.startField + ";\n" +
+                getTabString(indentLevel) + "}\n";
+        }
+
+        @Override
+        public String visit(SourceTimeStartAndEnd val) {
+            return "StartAndEnd\n" +
+                getTabString(indentLevel) + "{\n" +
+                getTabString(indentLevel + 1) + "startField: " + val.startField + ";\n" +
+                getTabString(indentLevel + 1) + "endField: " + val.endField + ";\n" +
                 getTabString(indentLevel) + "}\n";
         }
     }
--- legend-engine-xt-persistence-target-relational-grammar/src/test/java/org/finos/legend/engine/language/pure/dsl/persistence/relational/grammar/test/TestPersistenceRelationalGrammarRoundTrip.java
+++ legend-engine-xt-persistence-target-relational-grammar/src/test/java/org/finos/legend/engine/language/pure/dsl/persistence/relational/grammar/test/TestPersistenceRelationalGrammarRoundTrip.java
@@ -20,119 +20,271 @@ import org.junit.Test;
 public class TestPersistenceRelationalGrammarRoundTrip extends TestGrammarRoundtrip.TestGrammarRoundtripTestSuite
 {
     @Test
+    public void persistenceTargetNontemporal()
+    {
+        test("###Persistence\n" +
+            "Persistence test::TestPersistence\n" +
+            "{\n" +
+            "  doc: 'This is test documentation.';\n" +
+            "  trigger: Manual;\n" +
+            "  service: test::Service;\n" +
+            "  serviceOutputTargets:\n" +
+            "  [\n" +
+            "    ROOT\n" +
+            "    {\n" +
+            "      keys:\n" +
+            "      [\n" +
+            "        foo, bar\n" +
+            "      ]\n" +
+            "      datasetType: Delta\n" +
+            "      {\n" +
+            "        actionIndicator: DeleteIndicator\n" +
+            "        {\n" +
+            "          deleteField: isDeleted;\n" +
+            "          deleteValues: ['Y', '1', 'true'];\n" +
+            "        }\n" +
+            "      }\n" +
+            "      deduplication: AnyVersion;\n" +
+            "    }\n" +
+            "    ->\n" +
+            "    Relational\n" +
+            "    #{\n" +
+            "      table: TableA;\n" +
+            "      temporality: None;\n" +
+            "    }#,\n" +
+            "    #/test::example::MyType/prop#\n" +
+            "    {\n" +
+            "      keys:\n" +
+            "      [\n" +
+            "        #/test::example::MyType/prop/foo#, #/test::example::MyType/prop/bar#\n" +
+            "      ]\n" +
+            "      datasetType: Snapshot\n" +
+            "      {\n" +
+            "        partitioning: None\n" +
+            "        {\n" +
+            "          emptyDatasetHandling: NoOp;\n" +
+            "        }\n" +
+            "      }\n" +
+            "      deduplication: MaxVersion\n" +
+            "      {\n" +
+            "        versionField: #/test::example::MyType/prop/version#;\n" +
+            "      }\n" +
+            "    }\n" +
+            "    ->\n" +
+            "    Relational\n" +
+            "    #{\n" +
+            "      table: TableB;\n" +
+            "      temporality: None;\n" +
+            "    }#\n" +
+            "  ];\n" +
+            "}\n");
+    }
+
+    @Test
+    public void persistenceTargetUnitemporal()
+    {
+        test("###Persistence\n" +
+            "Persistence test::TestPersistence\n" +
+            "{\n" +
+            "  doc: 'This is test documentation.';\n" +
+            "  trigger: Manual;\n" +
+            "  service: test::Service;\n" +
+            "  serviceOutputTargets:\n" +
+            "  [\n" +
+            "    ROOT\n" +
+            "    {\n" +
+            "      keys:\n" +
+            "      [\n" +
+            "        foo, bar\n" +
+            "      ]\n" +
+            "      datasetType: Delta\n" +
+            "      {\n" +
+            "        actionIndicator: DeleteIndicator\n" +
+            "        {\n" +
+            "          deleteField: isDeleted;\n" +
+            "          deleteValues: ['Y', '1', 'true'];\n" +
+            "        }\n" +
+            "      }\n" +
+            "      deduplication: AnyVersion;\n" +
+            "    }\n" +
+            "    ->\n" +
+            "    Relational\n" +
+            "    #{\n" +
+            "      table: TableA;\n" +
+            "      temporality: Unitemporal\n" +
+            "      {\n" +
+            "        processingDimension: DateTime\n" +
+            "        {\n" +
+            "          dateTimeIn: y;\n" +
+            "          dateTimeOut: y;\n" +
+            "        }\n" +
+            "      }\n" +
+            "    }#,\n" +
+            "    #/test::example::MyType/prop#\n" +
+            "    {\n" +
+            "      keys:\n" +
+            "      [\n" +
+            "        #/test::example::MyType/prop/foo#, #/test::example::MyType/prop/bar#\n" +
+            "      ]\n" +
+            "      datasetType: Snapshot\n" +
+            "      {\n" +
+            "        partitioning: None\n" +
+            "        {\n" +
+            "          emptyDatasetHandling: NoOp;\n" +
+            "        }\n" +
+            "      }\n" +
+            "      deduplication: MaxVersion\n" +
+            "      {\n" +
+            "        versionField: #/test::example::MyType/prop/version#;\n" +
+            "      }\n" +
+            "    }\n" +
+            "    ->\n" +
+            "    Relational\n" +
+            "    #{\n" +
+            "      table: TableB;\n" +
+            "      temporality: Unitemporal\n" +
+            "      {\n" +
+            "        processingDimension: BatchId\n" +
+            "        {\n" +
+            "          batchIdIn: batchIdIn;\n" +
+            "          batchIdOut: batchIdOut;\n" +
+            "        }\n" +
+            "      }\n" +
+            "    }#\n" +
+            "  ];\n" +
+            "}\n");
+    }
+
+    @Test
     public void persistenceTargetBitemporal()
     {
         test("###Persistence\n" +
-                "Persistence test::TestPersistence\n" +
-                "{\n" +
-                "  doc: 'This is test documentation.';\n" +
-                "  trigger: Manual;\n" +
-                "  service: test::Service;\n" +
-                "  serviceOutputTargets:\n" +
-                "  [\n" +
-                "    ROOT\n" +
-                "    {\n" +
-                "      keys:\n" +
-                "      [\n" +
-                "        foo, bar\n" +
-                "      ]\n" +
-                "      datasetType: Delta\n" +
-                "      {\n" +
-                "        actionIndicator: DeleteIndicator\n" +
-                "        {\n" +
-                "          deleteField: isDeleted;\n" +
-                "          deleteValues: ['Y', '1', 'true'];\n" +
-                "        }\n" +
-                "      }\n" +
-                "      deduplication: AnyVersion;\n" +
-                "    }\n" +
-                "    ->\n" +
-                "    Relational\n" +
-                "    #{\n" +
-                "      table: TableA;\n" +
-                "      temporality: Bitemporal\n" +
-                "      {\n" +
-                "        processingDimension: BatchIdAndDateTime\n" +
-                "        {\n" +
-                "          batchIdIn: x;\n" +
-                "          batchIdOut: y;\n" +
-                "          dateTimeIn: y;\n" +
-                "          dateTimeOut: y;\n" +
-                "        }\n" +
-                "        sourceDerivedDimension: DateTime\n" +
-                "        {\n" +
-                "          dateTimeStart: x;\n" +
-                "          dateTimeEnd: y;\n" +
-                "          sourceFields: StartAndEnd\n" +
-                "          {\n" +
-                "            startField: timeIn;\n" +
-                "            endField: timeOut;\n" +
-                "          }\n" +
-                "        }\n" +
-                "      }\n" +
-                "    }#,\n" +
-                "    #/test::example::MyType/prop#\n" +
-                "    {\n" +
-                "      keys:\n" +
-                "      [\n" +
-                "        #/test::example::MyType/prop/foo#, #/test::example::MyType/prop/bar#\n" +
-                "      ]\n" +
-                "      datasetType: Snapshot\n" +
-                "      {\n" +
-                "        partitioning: None\n" +
-                "        {\n" +
-                "          emptyDatasetHandling: NoOp;\n" +
-                "        }\n" +
-                "      }\n" +
-                "      deduplication: MaxVersion\n" +
-                "      {\n" +
-                "        versionField: #/test::example::MyType/prop/version#;\n" +
-                "      }\n" +
-                "    }\n" +
-                "    ->\n" +
-                "    Relational\n" +
-                "    #{\n" +
-                "      table: TableB;\n" +
-                "      temporality: None;\n" +
-                "    }#\n" +
-                "  ];\n" +
-                "  tests:\n" +
-                "  [\n" +
-                "    test1:\n" +
-                "    {\n" +
-                "      testBatches:\n" +
-                "      [\n" +
-                "        testBatch1:\n" +
-                "        {\n" +
-                "          data:\n" +
-                "          {\n" +
-                "            connection:\n" +
-                "            {\n" +
-                "              ExternalFormat\n" +
-                "              #{\n" +
-                "                contentType: 'application/x.flatdata';\n" +
-                "                data: 'FIRST_NAME,LAST_NAME\\nFred,Bloggs\\nJane,Doe';\n" +
-                "              }#\n" +
-                "            }\n" +
-                "          }\n" +
-                "          asserts:\n" +
-                "          [\n" +
-                "            assert1:\n" +
-                "              EqualToJson\n" +
-                "              #{\n" +
-                "                expected : \n" +
-                "                  ExternalFormat\n" +
-                "                  #{\n" +
-                "                    contentType: 'application/json';\n" +
-                "                    data: '{\"Age\":12, \"Name\":\"dummy\"}';\n" +
-                "                  }#;\n" +
-                "              }#\n" +
-                "          ]\n" +
-                "        }\n" +
-                "      ]\n" +
-                "      isTestDataFromServiceOutput: false;\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}\n");
+            "Persistence test::TestPersistence\n" +
+            "{\n" +
+            "  doc: 'This is test documentation.';\n" +
+            "  trigger: Manual;\n" +
+            "  service: test::Service;\n" +
+            "  serviceOutputTargets:\n" +
+            "  [\n" +
+            "    ROOT\n" +
+            "    {\n" +
+            "      keys:\n" +
+            "      [\n" +
+            "        foo, bar\n" +
+            "      ]\n" +
+            "      datasetType: Delta\n" +
+            "      {\n" +
+            "        actionIndicator: DeleteIndicator\n" +
+            "        {\n" +
+            "          deleteField: isDeleted;\n" +
+            "          deleteValues: ['Y', '1', 'true'];\n" +
+            "        }\n" +
+            "      }\n" +
+            "      deduplication: AnyVersion;\n" +
+            "    }\n" +
+            "    ->\n" +
+            "    Relational\n" +
+            "    #{\n" +
+            "      table: TableA;\n" +
+            "      temporality: Bitemporal\n" +
+            "      {\n" +
+            "        processingDimension: BatchIdAndDateTime\n" +
+            "        {\n" +
+            "          batchIdIn: x;\n" +
+            "          batchIdOut: y;\n" +
+            "          dateTimeIn: y;\n" +
+            "          dateTimeOut: y;\n" +
+            "        }\n" +
+            "        sourceDerivedDimension: DateTime\n" +
+            "        {\n" +
+            "          dateTimeStart: x;\n" +
+            "          dateTimeEnd: y;\n" +
+            "          sourceFields: StartAndEnd\n" +
+            "          {\n" +
+            "            startField: timeIn;\n" +
+            "            endField: timeOut;\n" +
+            "          }\n" +
+            "        }\n" +
+            "      }\n" +
+            "    }#,\n" +
+            "    #/test::example::MyType/prop#\n" +
+            "    {\n" +
+            "      keys:\n" +
+            "      [\n" +
+            "        #/test::example::MyType/prop/foo#, #/test::example::MyType/prop/bar#\n" +
+            "      ]\n" +
+            "      datasetType: Snapshot\n" +
+            "      {\n" +
+            "        partitioning: None\n" +
+            "        {\n" +
+            "          emptyDatasetHandling: NoOp;\n" +
+            "        }\n" +
+            "      }\n" +
+            "      deduplication: MaxVersion\n" +
+            "      {\n" +
+            "        versionField: #/test::example::MyType/prop/version#;\n" +
+            "      }\n" +
+            "    }\n" +
+            "    ->\n" +
+            "    Relational\n" +
+            "    #{\n" +
+            "      table: TableB;\n" +
+            "      temporality: Bitemporal\n" +
+            "      {\n" +
+            "        processingDimension: BatchId\n" +
+            "        {\n" +
+            "          batchIdIn: batchIdIn;\n" +
+            "          batchIdOut: batchIdOut;\n" +
+            "        }\n" +
+            "        sourceDerivedDimension: DateTime\n" +
+            "        {\n" +
+            "          dateTimeStart: startTime;\n" +
+            "          dateTimeEnd: endTime;\n" +
+            "          sourceFields: Start\n" +
+            "          {\n" +
+            "            startField: someTime;\n" +
+            "          }\n" +
+            "        }\n" +
+            "      }\n" +
+            "    }#\n" +
+            "  ];\n" +
+            "  tests:\n" +
+            "  [\n" +
+            "    test1:\n" +
+            "    {\n" +
+            "      testBatches:\n" +
+            "      [\n" +
+            "        testBatch1:\n" +
+            "        {\n" +
+            "          data:\n" +
+            "          {\n" +
+            "            connection:\n" +
+            "            {\n" +
+            "              ExternalFormat\n" +
+            "              #{\n" +
+            "                contentType: 'application/x.flatdata';\n" +
+            "                data: 'FIRST_NAME,LAST_NAME\\nFred,Bloggs\\nJane,Doe';\n" +
+            "              }#\n" +
+            "            }\n" +
+            "          }\n" +
+            "          asserts:\n" +
+            "          [\n" +
+            "            assert1:\n" +
+            "              EqualToJson\n" +
+            "              #{\n" +
+            "                expected : \n" +
+            "                  ExternalFormat\n" +
+            "                  #{\n" +
+            "                    contentType: 'application/json';\n" +
+            "                    data: '{\"Age\":12, \"Name\":\"dummy\"}';\n" +
+            "                  }#;\n" +
+            "              }#\n" +
+            "          ]\n" +
+            "        }\n" +
+            "      ]\n" +
+            "      isTestDataFromServiceOutput: false;\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}\n");
     }
 }
