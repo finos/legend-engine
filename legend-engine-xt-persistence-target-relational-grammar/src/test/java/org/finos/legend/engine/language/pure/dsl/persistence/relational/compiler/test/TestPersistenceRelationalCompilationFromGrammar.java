5ec7950765349da13bcd91243cd348bf14427289
kumuwu <e0203066@u.nus.edu>
2023-05-24 17:15:21 +0800

    Add persistence relational compiler test skeleton

--- legend-engine-xt-persistence-target-relational-grammar/src/test/java/org/finos/legend/engine/language/pure/dsl/persistence/relational/compiler/test/TestPersistenceRelationalCompilationFromGrammar.java
+++ legend-engine-xt-persistence-target-relational-grammar/src/test/java/org/finos/legend/engine/language/pure/dsl/persistence/relational/compiler/test/TestPersistenceRelationalCompilationFromGrammar.java
@@ -191,4 +191,357 @@ public class TestPersistenceRelationalCompilationFromGrammar extends TestCompila
 
         PureModel model = result.getTwo();
     }
+
+    @Test
+    public void unitemporal()
+    {
+        Pair<PureModelContextData, PureModel> result = test(
+            "Class test::Person\n" +
+                "{\n" +
+                "  name: String[1];\n" +
+                "}\n" +
+                "\n" +
+                "###Mapping\n" +
+                "Mapping test::Mapping ()\n" +
+                "\n" +
+                "###Service\n" +
+                "Service test::Service \n" +
+                "{\n" +
+                "  pattern : 'test';\n" +
+                "  documentation : 'test';\n" +
+                "  autoActivateUpdates: true;\n" +
+                "  execution: Single\n" +
+                "  {\n" +
+                "    query: |test::Person.all()->project([p|$p.name],['name']);\n" +
+                "    mapping: test::Mapping;\n" +
+                "    runtime:\n" +
+                "    #{\n" +
+                "      connections: [];\n" +
+                "    }#;\n" +
+                "  }\n" +
+                "  test: Single\n" +
+                "  {\n" +
+                "    data: 'test';\n" +
+                "    asserts: [];\n" +
+                "  }\n" +
+                "}\n" +
+                "\n" +
+                "###Relational\n" +
+                "Database test::Database\n" +
+                "(\n" +
+                "  Table personTable\n" +
+                "  (\n" +
+                "    ID INTEGER PRIMARY KEY,\n" +
+                "    NAME VARCHAR(100)," +
+                "    time_in TIMESTAMP," +
+                "    time_out TIMESTAMP\n" +
+                "  )\n" +
+                ")" +
+                "\n" +
+                "###Persistence\n" +
+                "Persistence test::TestPersistence\n" +
+                "{\n" +
+                "  doc: 'This is test documentation.';\n" +
+                "  trigger: Manual;\n" +
+                "  service: test::Service;\n" +
+                "  serviceOutputTargets:\n" +
+                "  [\n" +
+                "    ROOT\n" +
+                "    {\n" +
+                "      keys:\n" +
+                "      [\n" +
+                "        foo, bar\n" +
+                "      ]\n" +
+                "      datasetType: Snapshot\n" +
+                "      {\n" +
+                "        partitioning: FieldBased\n" +
+                "        {\n" +
+                "          partitionFields:\n" +
+                "          [\n" +
+                "            foo1, bar2\n" +
+                "          ];\n" +
+                "        }\n" +
+                "      }\n" +
+                "      deduplication: MaxVersion\n" +
+                "      {\n" +
+                "        versionField: version;\n" +
+                "      }\n" +
+                "    }\n" +
+                "    ->\n" +
+                "    Relational\n" +
+                "    #{\n" +
+                "      database: test::Database;\n" +
+                "      table: personTable;\n" +
+                "      temporality: Unitemporal\n" +
+                "      {\n" +
+                "        processingDimension: DateTime\n" +
+                "        {\n" +
+                "          dateTimeIn: time_in;\n" +
+                "          dateTimeOut: time_out;\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }#\n" +
+                "  ];\n" +
+                "  tests:\n" +
+                "  [\n" +
+                "    test1:\n" +
+                "    {\n" +
+                "      testBatches:\n" +
+                "      [\n" +
+                "        testBatch1:\n" +
+                "        {\n" +
+                "         data:\n" +
+                "         {\n" +
+                "           connection:\n" +
+                "           {\n" +
+                "              ExternalFormat\n" +
+                "              #{\n" +
+                "                contentType: 'application/x.flatdata';\n" +
+                "                data: 'FIRST_NAME,LAST_NAME\\nFred,Bloggs\\nJane,Doe';\n" +
+                "              }#\n" +
+                "           }\n" +
+                "         }\n" +
+                "         asserts:\n" +
+                "         [\n" +
+                "           assert1:\n" +
+                "             EqualToJson\n" +
+                "             #{\n" +
+                "               expected: \n" +
+                "                 ExternalFormat\n" +
+                "                 #{\n" +
+                "                   contentType: 'application/json';\n" +
+                "                   data: '{\"Age\":12, \"Name\":\"dummy\"}';\n" +
+                "                 }#;\n" +
+                "             }#\n" +
+                "          ]\n" +
+                "        }\n" +
+                "      ]\n" +
+                "      isTestDataFromServiceOutput: false;\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}\n");
+
+        PureModel model = result.getTwo();
+    }
+
+    @Test
+    public void bitemporal()
+    {
+        Pair<PureModelContextData, PureModel> result = test(
+            "Class test::model::Firm\n" +
+                "{\n" +
+                "  employees: test::model::Person[1..*];\n" +
+                "  legalName: String[1];\n" +
+                "}\n" +
+                "\n" +
+                "Class test::model::Person\n" +
+                "{\n" +
+                "  firstName: String[1];\n" +
+                "  lastName: String[1];\n" +
+                "  isDeleted: String[1];\n" +
+                "}\n" +
+                "\n" +
+                "Class test::store::S_Firm\n" +
+                "{\n" +
+                "  employees: test::store::S_Person[1..*];\n" +
+                "  legalName: String[1];\n" +
+                "}\n" +
+                "\n" +
+                "Class test::store::S_Person\n" +
+                "{\n" +
+                "  firstName: String[1];\n" +
+                "  lastName: String[1];\n" +
+                "}\n" +
+                "\n" +
+                "\n" +
+                "###Mapping\n" +
+                "Mapping test::mapping::FirmMapping\n" +
+                "(\n" +
+                "  *test::model::Firm: Pure\n" +
+                "  {\n" +
+                "    ~src test::store::S_Firm\n" +
+                "    legalName: $src.legalName,\n" +
+                "    employees[test_model_Person]: $src.employees\n" +
+                "  }\n" +
+                "  *test::model::Person: Pure\n" +
+                "  {\n" +
+                "    ~src test::store::S_Person\n" +
+                "    firstName: $src.firstName,\n" +
+                "    lastName: $src.lastName\n" +
+                "  }\n" +
+                ")\n" +
+                "\n" +
+                "\n" +
+                "###Connection\n" +
+                "JsonModelConnection test::runtime::SFirmConnection\n" +
+                "{\n" +
+                "  class: test::store::S_Firm;\n" +
+                "  url: 'executor:default';\n" +
+                "}\n" +
+                "JsonModelConnection test::runtime::SPersonConnection\n" +
+                "{\n" +
+                "  class: test::store::S_Person;\n" +
+                "  url: 'executor:default';\n" +
+                "}\n" +
+                "\n" +
+                "\n" +
+                "###Runtime\n" +
+                "Runtime test::runtime::SFirmRuntime\n" +
+                "{\n" +
+                "  mappings:\n" +
+                "  [\n" +
+                "    test::mapping::FirmMapping\n" +
+                "  ];\n" +
+                "  connections:\n" +
+                "  [\n" +
+                "    ModelStore:\n" +
+                "    [\n" +
+                "      connection1: test::runtime::SFirmConnection\n" +
+                "    ]\n" +
+                "  ];\n" +
+                "}\n" +
+                "Runtime test::runtime::SFirmAndSPersonRuntime\n" +
+                "{\n" +
+                "  mappings:\n" +
+                "  [\n" +
+                "    test::mapping::FirmMapping\n" +
+                "  ];\n" +
+                "  connections:\n" +
+                "  [\n" +
+                "    ModelStore:\n" +
+                "    [\n" +
+                "      connection1: test::runtime::SFirmConnection,\n" +
+                "      connection2: test::runtime::SPersonConnection\n" +
+                "    ]\n" +
+                "  ];\n" +
+                "}\n" +
+                "\n" +
+                "\n" +
+                "###Service\n" +
+                "Service test::service::FirmService\n" +
+                "{\n" +
+                "  pattern: '/testFirmService';\n" +
+                "  owners:\n" +
+                "  [\n" +
+                "    'owner1',\n" +
+                "    'owner2'\n" +
+                "  ];\n" +
+                "  documentation: '';\n" +
+                "  autoActivateUpdates: true;\n" +
+                "  execution: Single\n" +
+                "  {\n" +
+                "    query: name:String[1]|test::model::Firm.all()->filter(f | $f.legalName == $name )->graphFetch(#{test::model::Firm{employees{firstName,lastName},legalName}}#)->serialize(#{test::model::Firm{employees{firstName,lastName},legalName}}#);\n" +
+                "    mapping: test::mapping::FirmMapping;\n" +
+                "    runtime: test::runtime::SFirmRuntime;\n" +
+                "  }\n" +
+                "  testSuites:\n" +
+                "  [\n" +
+                "  ]\n" +
+                "}\n" +
+                "\n" +
+                "\n" +
+                "###Relational\n" +
+                "Database test::Database\n" +
+                "(\n" +
+                "  Table personTable\n" +
+                "  (\n" +
+                "    ID INTEGER PRIMARY KEY,\n" +
+                "    NAME VARCHAR(100)," +
+                "    startTime TIMESTAMP," +
+                "    endTime TIMESTAMP," +
+                "    batchIdIn INTEGER," +
+                "    batchIdOut INTEGER\n" +
+                "  )\n" +
+                ")" +
+                "\n" +
+                "###Persistence\n" +
+                "Persistence test::TestPersistence\n" +
+                "{\n" +
+                "  doc: 'This is test documentation.';\n" +
+                "  trigger: Manual;\n" +
+                "  service: test::service::FirmService;\n" +
+                "  serviceOutputTargets:\n" +
+                "  [\n" +
+                "    #/test::model::Firm/employees#\n" +
+                "    {\n" +
+                "      keys:\n" +
+                "      [\n" +
+                "        #/test::model::Firm/employees/lastName#\n" +
+                "      ]\n" +
+                "      datasetType: Delta\n" +
+                "      {\n" +
+                "        actionIndicator: DeleteIndicator\n" +
+                "        {\n" +
+                "          deleteField: #/test::model::Firm/employees/isDeleted#;\n" +
+                "          deleteValues: ['Yes', 'true', '1'];\n" +
+                "        }\n" +
+                "      }\n" +
+                "      deduplication: None;\n" +
+                "    }\n" +
+                "    ->\n" +
+                "    Relational\n" +
+                "    #{\n" +
+                "      table: personTable;\n" +
+                "      database: test::Database;\n" +
+                "      temporality: Bitemporal\n" +
+                "      {\n" +
+                "        processingDimension: BatchId\n" +
+                "        {\n" +
+                "          batchIdIn: batchIdIn;\n" +
+                "          batchIdOut: batchIdOut;\n" +
+                "        }\n" +
+                "        sourceDerivedDimension: DateTime\n" +
+                "        {\n" +
+                "          dateTimeStart: startTime;\n" +
+                "          dateTimeEnd: endTime;\n" +
+                "          sourceFields: StartAndEnd\n" +
+                "          {\n" +
+                "            startField: timeFrom;\n" +
+                "            endField: timeThru;\n" +
+                "          }\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }#\n" +
+                "  ];\n" +
+                "  tests:\n" +
+                "  [\n" +
+                "    test1:\n" +
+                "    {\n" +
+                "      testBatches:\n" +
+                "      [\n" +
+                "        testBatch1:\n" +
+                "        {\n" +
+                "         data:\n" +
+                "         {\n" +
+                "           connection:\n" +
+                "           {\n" +
+                "              ExternalFormat\n" +
+                "              #{\n" +
+                "                contentType: 'application/x.flatdata';\n" +
+                "                data: 'FIRST_NAME,LAST_NAME\\nFred,Bloggs\\nJane,Doe';\n" +
+                "              }#\n" +
+                "           }\n" +
+                "         }\n" +
+                "         asserts:\n" +
+                "         [\n" +
+                "           assert1:\n" +
+                "             EqualToJson\n" +
+                "             #{\n" +
+                "               expected: \n" +
+                "                 ExternalFormat\n" +
+                "                 #{\n" +
+                "                   contentType: 'application/json';\n" +
+                "                   data: '{\"Age\":12, \"Name\":\"dummy\"}';\n" +
+                "                 }#;\n" +
+                "             }#\n" +
+                "          ]\n" +
+                "        }\n" +
+                "      ]\n" +
+                "      isTestDataFromServiceOutput: false;\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}\n");
+
+        PureModel model = result.getTwo();
+    }
 }
