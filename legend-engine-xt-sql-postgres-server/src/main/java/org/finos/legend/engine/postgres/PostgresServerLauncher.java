0d08047f01945c642b00e505c984890b07776077
Leonid Shtivelman <leonid.shtivelman@gs.com>
2023-05-31 01:10:54 -0400

    changed to use logback logback file can be specified in config and specified at run time of docker image

---
+++ legend-engine-xt-sql-postgres-server/src/main/java/org/finos/legend/engine/postgres/PostgresServerLauncher.java
@@ -0,0 +1,59 @@
+// Copyright 2023 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package org.finos.legend.engine.postgres;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import org.finos.legend.engine.postgres.auth.AuthenticationMethod;
+import org.finos.legend.engine.postgres.config.Builder;
+import org.finos.legend.engine.postgres.config.ServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PostgresServerLauncher
+{
+
+
+    public static void main(String[] args) throws Exception
+    {
+        //TODO ADD CLI
+        String configPath = args[0];
+        InputStream configStream = new FileInputStream(new File(configPath));
+        ObjectMapper objectMapper = new ObjectMapper();
+        ServerConfig serverConfig = objectMapper.readValue(configStream, ServerConfig.class);
+        /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+          System property must be set before any logger instance is created.
+          Do NOT create static logger in this class.
+        */
+        if (serverConfig.getLogConfigFile() != null)
+        {
+            System.setProperty("logback.configurationFile", serverConfig.getLogConfigFile());
+        }
+        if (serverConfig.getGss() != null)
+        {
+            System.setProperty("java.security.krb5.conf", serverConfig.getGss().getKerberosConfigFile());
+        }
+
+        //Log config has been initiated. We can create a logger now.
+        Logger logger = LoggerFactory.getLogger(PostgresServerLauncher.class);
+        SessionsFactory sessionFactory = Builder.buildSessionFactory(serverConfig);
+        AuthenticationMethod authenticationMethod = Builder.buildAuthenticationMethod(serverConfig);
+
+        logger.info("Starting server in port: " + serverConfig.getPort());
+        new PostgresServer(serverConfig, sessionFactory, (user, connectionProperties) -> authenticationMethod).run();
+    }
+}
