6c5f6cfc115e6121e1cd7f6fdc0edfea696584cf
horbe <hudson.orbe@ny.email.gs.com>
2023-06-30 11:44:08 -0400

    Boilerplate for BigQuery function activator

---
+++ legend-engine-xt-bigqueryFunc-api/src/main/java/org/finos/legend/engine/language/bigqueryFunc/api/BigQueryFunctionDeployableArtifact.java
@@ -0,0 +1,126 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.language.bigqueryFunc.api;
+
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.block.function.Function;
+import org.eclipse.collections.impl.factory.Lists;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.plan.execution.stores.relational.connection.manager.ConnectionManagerSelector;
+import org.finos.legend.engine.plan.generation.PlanGenerator;
+import org.finos.legend.engine.plan.platform.PlanPlatform;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.DatabaseType;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.RelationalDatabaseConnection;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.authentication.GCPApplicationDefaultCredentialsAuthenticationStrategy;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.authentication.SnowflakePublicAuthenticationStrategy;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.BigQueryDatasourceSpecification;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.specification.SnowflakeDatasourceSpecification;
+import org.finos.legend.engine.shared.core.identity.Identity;
+import org.finos.legend.pure.generated.*;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.FunctionDefinition;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+final class BigQueryFunctionDeployableArtifact
+{
+    private final String functionName;
+    private final String functionAsSQLStatement;
+    private final Root_meta_pure_alloy_connections_alloy_specification_BigQueryDatasourceSpecification datasourceSpecification;
+    private final Root_meta_pure_alloy_connections_alloy_authentication_GCPApplicationDefaultCredentialsAuthenticationStrategy authenticationStrategy;
+
+    private BigQueryFunctionDeployableArtifact(
+            String functionName,
+            String functionAsSQLStatement,
+            Root_meta_pure_alloy_connections_alloy_specification_BigQueryDatasourceSpecification datasourceSpecification,
+            Root_meta_pure_alloy_connections_alloy_authentication_GCPApplicationDefaultCredentialsAuthenticationStrategy authenticationStrategy)
+    {
+        this.functionName = functionName;
+        this.functionAsSQLStatement = functionAsSQLStatement;
+        this.datasourceSpecification = datasourceSpecification;
+        this.authenticationStrategy = authenticationStrategy;
+    }
+
+    public static BigQueryFunctionDeployableArtifact buildArtifact(
+            PureModel pureModel,
+            Root_meta_external_functionActivator_bigQueryFunc_BigQueryFunction bigQueryFunction,
+            Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensions) throws BigQueryFunctionArtifactValidationException
+    {
+        Root_meta_pure_executionPlan_ExecutionPlan executionPlan =
+                PlanGenerator.generateExecutionPlanAsPure((FunctionDefinition<?>) bigQueryFunction._function(), null, null, null, pureModel, PlanPlatform.JAVA, null, routerExtensions.apply(pureModel));
+        RichIterable<Root_meta_relational_mapping_SQLExecutionNode> sqlExecutionNodes =
+                BigQueryFunctionDeployableArtifact.collectAllNodes(executionPlan._rootExecutionNode()).selectInstancesOf(Root_meta_relational_mapping_SQLExecutionNode.class);
+
+        RichIterable<String> sqlExpressions =
+                sqlExecutionNodes
+                        .collect(Root_meta_relational_mapping_SQLExecutionNode::_sqlQuery)
+                        .select(x -> !x.toLowerCase().startsWith("alter"));
+
+        if (sqlExpressions.size() != 1)
+        {
+            throw new BigQueryFunctionArtifactValidationException("BigQuery Function can't be used with a plan containing '" + sqlExpressions.size() + "' SQL expressions", sqlExpressions);
+        }
+
+        Root_meta_pure_alloy_connections_RelationalDatabaseConnection relationalDatabaseConnection = (Root_meta_pure_alloy_connections_RelationalDatabaseConnection) sqlExecutionNodes.getAny()._connection();
+        return new BigQueryFunctionDeployableArtifact(
+                bigQueryFunction._functionName(),
+                sqlExpressions.getOnly(),
+                (Root_meta_pure_alloy_connections_alloy_specification_BigQueryDatasourceSpecification) relationalDatabaseConnection._datasourceSpecification(),
+                (Root_meta_pure_alloy_connections_alloy_authentication_GCPApplicationDefaultCredentialsAuthenticationStrategy) relationalDatabaseConnection._authenticationStrategy());
+    }
+
+    private static RichIterable<Root_meta_pure_executionPlan_ExecutionNode> collectAllNodes(Root_meta_pure_executionPlan_ExecutionNode node)
+    {
+        return Lists.mutable.with(node).withAll(node._executionNodes().flatCollect(BigQueryFunctionDeployableArtifact::collectAllNodes));
+    }
+
+    public BigQueryFunctionArtifact toArtifact()
+    {
+        return new BigQueryFunctionArtifact(this.functionAsSQLStatement);
+    }
+
+    // TODO HSO: Handle parameterized SQL statements
+    public void deploy(ConnectionManagerSelector connectionManagerSelector) throws SQLException
+    {
+        BigQueryDatasourceSpecification datasourceSpecification = new BigQueryDatasourceSpecification();
+        datasourceSpecification.defaultDataset = this.datasourceSpecification._defaultDataset();
+        datasourceSpecification.projectId = this.datasourceSpecification._projectId();
+        datasourceSpecification.proxyHost = this.datasourceSpecification._proxyHost();
+        datasourceSpecification.proxyPort = this.datasourceSpecification._proxyPort();
+
+        GCPApplicationDefaultCredentialsAuthenticationStrategy authenticationStrategy = new GCPApplicationDefaultCredentialsAuthenticationStrategy();
+
+        RelationalDatabaseConnection connection = new RelationalDatabaseConnection(
+                datasourceSpecification,
+                authenticationStrategy,
+                DatabaseType.BigQuery);
+
+        try (Connection jdbcConnection = connectionManagerSelector.getDatabaseConnection(new Identity("anonymous"), connection))
+        {
+            jdbcConnection.setAutoCommit(false);
+            Statement statement = jdbcConnection.createStatement();
+            String createTableFunctionExpression =
+                    String.format("CREATE OR REPLACE TABLE FUNCTION %s.%s() AS (%s)", this.datasourceSpecification._projectId(), this.functionName, this.functionAsSQLStatement);
+            statement.execute(createTableFunctionExpression);
+            jdbcConnection.commit();
+        }
+    }
+
+    public String getFunctionName()
+    {
+        return this.functionName;
+    }
+}
---
+++ legend-engine-xt-bigqueryFunc-api/src/main/java/org/finos/legend/engine/language/bigqueryFunc/api/BigQueryFunctionService.java
@@ -0,0 +1,108 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.language.bigqueryFunc.api;
+
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.block.function.Function;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.impl.factory.Lists;
+import org.finos.legend.engine.functionActivator.api.output.FunctionActivatorInfo;
+import org.finos.legend.engine.functionActivator.service.FunctionActivatorArtifact;
+import org.finos.legend.engine.functionActivator.service.FunctionActivatorError;
+import org.finos.legend.engine.functionActivator.service.FunctionActivatorService;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.plan.execution.stores.relational.config.TemporaryTestDbConfiguration;
+import org.finos.legend.engine.plan.execution.stores.relational.connection.manager.ConnectionManagerSelector;
+import org.finos.legend.engine.protocol.bigqueryFunc.metamodel.BigQueryFunctionProtocolExtension;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContext;
+import org.finos.legend.pure.generated.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.SQLException;
+
+public class BigQueryFunctionService implements FunctionActivatorService<Root_meta_external_functionActivator_bigQueryFunc_BigQueryFunction>
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(BigQueryFunctionService.class);
+
+    @Override
+    public FunctionActivatorInfo info(PureModel pureModel, String version)
+    {
+        return new FunctionActivatorInfo(
+                "BigQuery Function",
+                "Create a BigQuery Function that can activate in BigQuery.",
+                "meta::protocols::pure::" + version + "::metamodel::functionActivator::bigQueryFunc::BigQueryFunction",
+                BigQueryFunctionProtocolExtension.packageJSONType,
+                pureModel);
+    }
+
+    @Override
+    public boolean supports(Root_meta_external_functionActivator_FunctionActivator functionActivator)
+    {
+        return functionActivator instanceof Root_meta_external_functionActivator_bigQueryFunc_BigQueryFunction;
+    }
+
+    @Override
+    public MutableList<? extends FunctionActivatorError> validate(PureModel pureModel, Root_meta_external_functionActivator_bigQueryFunc_BigQueryFunction functionActivator, PureModelContext inputModel, Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensions)
+    {
+        try
+        {
+            BigQueryFunctionDeployableArtifact.buildArtifact(pureModel, functionActivator, routerExtensions);
+            return Lists.mutable.empty();
+
+        }
+        catch (BigQueryFunctionArtifactValidationException e)
+        {
+            return Lists.mutable.with(e.toError());
+        }
+    }
+
+    @Override
+    public MutableList<? extends FunctionActivatorError> publishToSandbox(PureModel pureModel, Root_meta_external_functionActivator_bigQueryFunc_BigQueryFunction functionActivator, PureModelContext inputModel, Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensions)
+    {
+        ConnectionManagerSelector connectionManagerSelector = new ConnectionManagerSelector(new TemporaryTestDbConfiguration(), Lists.fixedSize.empty());
+        try
+        {
+            BigQueryFunctionDeployableArtifact deployableArtifact = BigQueryFunctionDeployableArtifact.buildArtifact(pureModel, functionActivator, routerExtensions);
+            LOGGER.info("Starting deployment of {}!", functionActivator._functionName());
+            deployableArtifact.deploy(connectionManagerSelector);
+            LOGGER.info("Completed deployment of {} successfully!", functionActivator._functionName());
+            return Lists.mutable.empty();
+        }
+        catch (BigQueryFunctionArtifactValidationException e)
+        {
+            return Lists.mutable.with(e.toError());
+        }
+        catch (SQLException e)
+        {
+            LOGGER.info("Deployment of {} failed!", functionActivator._functionName());
+            return Lists.mutable.with(new FunctionActivatorError(e.getMessage()));
+        }
+    }
+
+    @Override
+    public FunctionActivatorArtifact renderArtifact(PureModel pureModel, Root_meta_external_functionActivator_bigQueryFunc_BigQueryFunction functionActivator, PureModelContext inputModel, Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensions)
+    {
+        try
+        {
+            return BigQueryFunctionDeployableArtifact.buildArtifact(pureModel, functionActivator, routerExtensions).toArtifact();
+        }
+        catch (BigQueryFunctionArtifactValidationException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
