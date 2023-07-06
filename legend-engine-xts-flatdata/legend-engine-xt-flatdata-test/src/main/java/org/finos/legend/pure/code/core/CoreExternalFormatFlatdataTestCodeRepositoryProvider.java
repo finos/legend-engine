9601633b83bf6084cb8f7bf4ab6e57187a3a8b86
Maheshwari, Hardik <Hardik.Maheshwari@ny.email.gs.com>
2023-07-06 05:55:13 -0400

    Move flatdata TestPlanBinder into flatdata test module

--- legend-engine-xts-flatdata/legend-engine-xt-flatdata-test/src/main/java/org/finos/legend/pure/code/core/CoreExternalFormatFlatdataTestCodeRepositoryProvider.java
+++ legend-engine-xts-flatdata/legend-engine-xt-flatdata-test/src/main/java/org/finos/legend/pure/code/core/CoreExternalFormatFlatdataTestCodeRepositoryProvider.java
@@ -12,6 +12,17 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-public class JavaPlaceHolder
+package org.finos.legend.pure.code.core;
+
+import org.finos.legend.pure.m3.serialization.filesystem.repository.CodeRepository;
+import org.finos.legend.pure.m3.serialization.filesystem.repository.CodeRepositoryProvider;
+import org.finos.legend.pure.m3.serialization.filesystem.repository.GenericCodeRepository;
+
+public class CoreExternalFormatFlatdataTestCodeRepositoryProvider implements CodeRepositoryProvider
 {
+    @Override
+    public CodeRepository repository()
+    {
+        return GenericCodeRepository.build("core_external_format_flatdata_test.definition.json");
+    }
 }
