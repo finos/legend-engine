46129111ed451d4a0a1e41734889bb21f0f5f052
Jake Kim <60619826+jake-kim1@users.noreply.github.com>
2023-07-19 14:40:25 -0400

    Add API endpoint for autogenerating model from a given database (#2049)

---
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-generation/legend-engine-xt-relationalStore-api/src/main/java/org/finos/legend/engine/language/pure/relational/api/relationalElement/RelationalElementAPI.java
@@ -0,0 +1,91 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.language.pure.relational.api.relationalElement;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.block.function.Function;
+import org.eclipse.collections.api.list.MutableList;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.language.pure.relational.api.relationalElement.input.DatabaseToModelGenerationInput;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
+import org.finos.legend.engine.shared.core.deployment.DeploymentMode;
+import org.finos.legend.engine.shared.core.kerberos.ProfileManagerHelper;
+import org.finos.legend.engine.shared.core.operational.errorManagement.ExceptionTool;
+import org.finos.legend.engine.shared.core.operational.logs.LoggingEventType;
+import org.finos.legend.pure.generated.Root_meta_pure_extension_Extension;
+import org.finos.legend.pure.generated.core_relational_relational_autogeneration_relationalToPure;
+import org.finos.legend.pure.m3.coreinstance.meta.relational.metamodel.Database;
+import org.finos.legend.pure.m3.coreinstance.meta.relational.metamodel.Schema;
+import org.finos.legend.pure.m3.execution.ExecutionSupport;
+import org.pac4j.core.profile.CommonProfile;
+import org.pac4j.core.profile.ProfileManager;
+import org.pac4j.jax.rs.annotations.Pac4JProfileManager;
+
+@Api(tags = "Pure - Relational")
+@Path("pure/v1/relational")
+public class RelationalElementAPI
+{
+    private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(RelationalElementAPI.class);
+    private final Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensionsFunc;
+    private final DeploymentMode deploymentMode;
+
+    public RelationalElementAPI(Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensionsFunc, DeploymentMode deploymentMode)
+    {
+        this.routerExtensionsFunc = routerExtensionsFunc;
+        this.deploymentMode = deploymentMode;
+    }
+
+    @POST
+    @Path("generateModelFromDatabase")
+    @ApiOperation(value = "Autogenerate model JSON from database")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response generateModelFromDatabase(DatabaseToModelGenerationInput input, @ApiParam(hidden = true) @Pac4JProfileManager ProfileManager<CommonProfile> pm)
+    {
+        MutableList<CommonProfile> profiles = ProfileManagerHelper.extractProfiles(pm);
+        try
+        {
+            PureModelContextData modelData = input.getModelData();
+            String databasePath = input.getDatabasePath();
+            PureModel model = new PureModel(modelData, profiles, this.deploymentMode);
+            Database database = (Database) model.getStore(databasePath);
+            RichIterable<? extends Schema> schemas = database._schemas();
+            Schema schema = schemas.getFirst(); // TODO: Handle multiple schemas within a database
+            String targetPackage = getTargetPackageFromDatabasePath(databasePath);
+            RichIterable<? extends Root_meta_pure_extension_Extension> extensions = this.routerExtensionsFunc.apply(model);
+            ExecutionSupport executionSupport = model.getExecutionSupport();
+            String result = core_relational_relational_autogeneration_relationalToPure.Root_meta_relational_transform_autogen_classesAssociationsAndMappingFromSchema_Schema_1__String_1__Extension_MANY__String_1_(schema, targetPackage, extensions, executionSupport);
+            return Response.ok(result).type(MediaType.APPLICATION_JSON_TYPE).build();
+        }
+        catch (Exception ex)
+        {
+            LOGGER.error("Failed to generate model from database", ex);
+            return ExceptionTool.exceptionManager(ex, LoggingEventType.CATCH_ALL, profiles);
+        }
+    }
+
+    private String getTargetPackageFromDatabasePath(String databasePath)
+    {
+        int lastColonIndex = databasePath.lastIndexOf(":");
+        return databasePath.substring(0, lastColonIndex + 1);
+    }
+}
---
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-generation/legend-engine-xt-relationalStore-api/src/test/java/org/finos/legend/engine/language/pure/relational/api/relationalElement/test/TestRelationalElementApi.java
@@ -0,0 +1,79 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.language.pure.relational.api.relationalElement.test;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.block.function.Function;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.language.pure.relational.api.relationalElement.RelationalElementAPI;
+import org.finos.legend.engine.language.pure.relational.api.relationalElement.input.DatabaseToModelGenerationInput;
+import org.finos.legend.engine.language.pure.grammar.from.PureGrammarParser;
+import org.finos.legend.engine.plan.generation.extension.PlanGeneratorExtension;
+import org.finos.legend.engine.protocol.Protocol;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextPointer;
+import org.finos.legend.engine.shared.core.deployment.DeploymentMode;
+import org.finos.legend.pure.generated.Root_meta_pure_extension_Extension;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.ServiceLoader;
+
+public class TestRelationalElementApi
+{
+    private static PureModelContextData compilePmcd(String model)
+    {
+        return PureModelContextData.newBuilder()
+                .withPureModelContextData(PureGrammarParser.newInstance().parseModel(model))
+                .withSerializer(new Protocol("pure", "vX_X_X"))
+                .withOrigin(new PureModelContextPointer())
+                .build();
+    }
+
+    private static String loadFromFile(String filename) throws IOException
+    {
+        return IOUtils.toString(Objects.requireNonNull(TestRelationalElementApi.class
+                        .getClassLoader()
+                        .getResource(filename)),
+                StandardCharsets.UTF_8
+        );
+    }
+
+    @Test
+    public void shouldGenerateModelFromDatabase() throws IOException
+    {
+        String expectedJson = loadFromFile("expectedJson.json");
+        String inputGrammar = loadFromFile("inputGrammar.pure");
+        Assert.assertNotNull(expectedJson);
+        Assert.assertNotNull(inputGrammar);
+        PureModelContextData inputPmcd = compilePmcd(inputGrammar);
+        String databasePath = "meta::relational::transform::autogen::tests::testDB";
+        DatabaseToModelGenerationInput inputJson = new DatabaseToModelGenerationInput(databasePath, inputPmcd);
+        MutableList<PlanGeneratorExtension> generatorExtensions = Lists.mutable.withAll(ServiceLoader.load(PlanGeneratorExtension.class));
+        Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensions = (PureModel pureModel) -> generatorExtensions.flatCollect(e -> e.getExtraExtensions(pureModel));
+        RelationalElementAPI relationalElementAPI = new RelationalElementAPI(routerExtensions, DeploymentMode.PROD);
+        Response response = relationalElementAPI.generateModelFromDatabase(inputJson, null);
+        Assert.assertNotNull(response);
+        String actualJson = response.getEntity().toString();
+        Assert.assertEquals(expectedJson, actualJson);
+    }
+}
