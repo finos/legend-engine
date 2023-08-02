4802d5139ee7969cf1bfa1945fb4df8e90c7aa96
Mythreyi <Mythreyi.Rengarajan@ny.email.gs.com>
2023-08-03 07:17:38 +0800

    Fixing the composer for graphFetchPath attribute

--- legend-engine-xts-persistence/legend-engine-xt-persistence-grammar/src/main/java/org/finos/legend/engine/language/pure/dsl/persistence/grammar/to/HelperPersistenceComposer.java
+++ legend-engine-xts-persistence/legend-engine-xt-persistence-grammar/src/main/java/org/finos/legend/engine/language/pure/dsl/persistence/grammar/to/HelperPersistenceComposer.java
@@ -271,7 +271,7 @@ public class HelperPersistenceComposer
         // graphFetchPath
         if (persistenceTest.graphFetchPath != null)
         {
-            getTabString(indentLevel + 1);
+            str.append(getTabString(indentLevel + 1));
             str.append("graphFetchPath")
                     .append(": ")
                     .append(ServiceOutputComposer.renderPath(persistenceTest.graphFetchPath))
