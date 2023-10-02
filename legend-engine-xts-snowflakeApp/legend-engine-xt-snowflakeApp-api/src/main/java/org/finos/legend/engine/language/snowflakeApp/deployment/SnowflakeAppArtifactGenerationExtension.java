04064cb79a44e1a2630917a263b76a16564ecc06
sprisha <61725373+sprisha@users.noreply.github.com>
2023-10-02 09:32:47 -0500

    Add missing return statement

--- legend-engine-xts-snowflakeApp/legend-engine-xt-snowflakeApp-api/src/main/java/org/finos/legend/engine/language/snowflakeApp/deployment/SnowflakeAppArtifactGenerationExtension.java
+++ legend-engine-xts-snowflakeApp/legend-engine-xt-snowflakeApp-api/src/main/java/org/finos/legend/engine/language/snowflakeApp/deployment/SnowflakeAppArtifactGenerationExtension.java
@@ -56,6 +56,8 @@ public class SnowflakeAppArtifactGenerationExtension implements ArtifactGenerati
         {
             LOGGER.warn("Error generating openapi specification", ex);
         }
+
+        return Collections.emptyList();
         
     }
 
