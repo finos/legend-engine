aef23ef93e0cc6aefa8fd7edeb0652ebd66b2ba5
sprisha <61725373+sprisha@users.noreply.github.com>
2023-10-02 10:25:14 -0500

    rename root path

--- legend-engine-xts-snowflakeApp/legend-engine-xt-snowflakeApp-api/src/main/java/org/finos/legend/engine/language/snowflakeApp/deployment/SnowflakeAppArtifactGenerationExtension.java
+++ legend-engine-xts-snowflakeApp/legend-engine-xt-snowflakeApp-api/src/main/java/org/finos/legend/engine/language/snowflakeApp/deployment/SnowflakeAppArtifactGenerationExtension.java
@@ -27,7 +27,7 @@ import java.util.List;
 
 public class SnowflakeAppArtifactGenerationExtension implements ArtifactGenerationExtension
 {
-    private static final String ROOT_PATH = "Snowflake App ";
+    private static final String ROOT_PATH = "snowflakeApp";
     private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(SnowflakeAppArtifactGenerationExtension.class);
 
     @Override
