6cbbcf44e7a07d05ec30eee283f05d4c5944543d
gs-gunjan <72594207+gs-gunjan@users.noreply.github.com>
2023-10-10 19:37:43 +0530

    fix bug while creating temporary table in snowflake for bit type

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-snowflake/legend-engine-xt-relationalStore-snowflake-execution/src/main/java/org/finos/legend/engine/plan/execution/stores/relational/connection/driver/vendors/snowflake/SnowflakeCommands.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-snowflake/legend-engine-xt-relationalStore-snowflake-execution/src/main/java/org/finos/legend/engine/plan/execution/stores/relational/connection/driver/vendors/snowflake/SnowflakeCommands.java
@@ -15,6 +15,7 @@
 package org.finos.legend.engine.plan.execution.stores.relational.connection.driver.vendors.snowflake;
 
 import org.eclipse.collections.api.factory.Maps;
+import org.eclipse.collections.api.map.ImmutableMap;
 import org.finos.legend.engine.plan.execution.stores.relational.connection.driver.commands.Column;
 import org.finos.legend.engine.plan.execution.stores.relational.connection.driver.commands.IngestionMethod;
 import org.finos.legend.engine.plan.execution.stores.relational.connection.driver.commands.RelationalDatabaseCommands;
@@ -22,12 +23,11 @@ import org.finos.legend.engine.plan.execution.stores.relational.connection.drive
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class SnowflakeCommands extends RelationalDatabaseCommands
 {
-    private static final Map<String, String> columnTypeToSqlTextMap = Maps.mutable.of("BIT", "BOOLEAN");
+    private static final ImmutableMap<String, String> columnTypeToSqlTextMap = Maps.immutable.of("BIT", "BOOLEAN");
 
     @Override
     public String processTempTableName(String tempTableName)
@@ -54,7 +54,7 @@ public class SnowflakeCommands extends RelationalDatabaseCommands
             optionalCSVFileLocation = optionalCSVFileLocation.substring(1);
         }
         List<String> strings = Arrays.asList(
-                "CREATE TEMPORARY TABLE " + tableName + " " + columns.stream().map(c -> c.name + " " + columnTypeToSqlTextMap.getOrDefault(c.type, c.type)).collect(Collectors.joining(",", "(", ")")),
+                "CREATE TEMPORARY TABLE " + tableName + " " + columns.stream().map(c -> c.name + " " + columnTypeToSqlTextMap.getIfAbsentValue(c.type, c.type)).collect(Collectors.joining(",", "(", ")")),
                 "CREATE OR REPLACE TEMPORARY STAGE " + tempStageName(),
                 "PUT file:///" + optionalCSVFileLocation + " @" + tempStageName() + "/" + optionalCSVFileLocation + " PARALLEL = 16 AUTO_COMPRESS = TRUE",
                 "COPY INTO " + tableName + " FROM @" + tempStageName() + "/" + optionalCSVFileLocation + " file_format = (type = CSV field_optionally_enclosed_by= '\"')",
