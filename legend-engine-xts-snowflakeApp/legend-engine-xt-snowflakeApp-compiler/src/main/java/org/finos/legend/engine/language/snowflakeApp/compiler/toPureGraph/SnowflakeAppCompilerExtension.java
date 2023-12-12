b12e0e2fd620b28816e84f39b0e4210f6109c131
Mohammed Ibrahim <mibrahim1771@gmail.com>
2023-12-12 14:56:25 -0500

    Remove Type From native App metamodel (#2504)
    * rename method
    
    * Function activation cleanup
    
    * rollback file changes
    
    * Change spark keywords
    
    * Change spark keywords
    
    * Fix test
    
    * Add Primitive Type to Open APi Generator
    
    * Add byte
    
    * BigQuery Activator: Sandbox
    
    * refactor modules
    
    * refactor modules
    
    * Add exclusions
    
    * Move protocols
    
    * update dependencies
    
    * update dependencies
    
    * Uplift Activation
    
    * fix test
    
    * Add deploymentLocation
    
    * fix checkstyle errors
    
    * fix checkstyle errors
    
    * Cange location url
    
    * Remove Application type
    
    * Remove Application type
    
    * fix dependency
    
    * further cleanups
    
    * clean up dependencies
    
    * fix test
    
    * fix test

--- legend-engine-xts-snowflakeApp/legend-engine-xt-snowflakeApp-compiler/src/main/java/org/finos/legend/engine/language/snowflakeApp/compiler/toPureGraph/SnowflakeAppCompilerExtension.java
+++ legend-engine-xts-snowflakeApp/legend-engine-xt-snowflakeApp-compiler/src/main/java/org/finos/legend/engine/language/snowflakeApp/compiler/toPureGraph/SnowflakeAppCompilerExtension.java
@@ -63,7 +63,6 @@ public class SnowflakeAppCompilerExtension implements CompilerExtension
                         ._function(func)
                         ._description(app.description)
                         ._owner(app.owner)
-                        ._type(app.type != null ? context.pureModel.getEnumValue("meta::external::function::activator::snowflakeApp::SnowflakeDeploymentType", app.type.toString()) : context.pureModel.getEnumValue("meta::external::function::activator::snowflakeApp::SnowflakeDeploymentType", "FULL"))
                         ._activationConfiguration(app.activationConfiguration != null ? buildDeploymentConfig((SnowflakeAppDeploymentConfiguration) app.activationConfiguration, context) : null);
         }
         catch (Exception e)
--- legend-engine-xts-snowflakeApp/legend-engine-xt-snowflakeApp-grammar/src/main/antlr4/org/finos/legend/engine/language/pure/grammar/from/antlr4/SnowflakeAppLexerGrammar.g4
+++ legend-engine-xts-snowflakeApp/legend-engine-xt-snowflakeApp-grammar/src/main/antlr4/org/finos/legend/engine/language/pure/grammar/from/antlr4/SnowflakeAppLexerGrammar.g4
@@ -7,7 +7,6 @@ SNOWFLAKE_APP__APPLICATION_NAME:                'applicationName';
 SNOWFLAKE_APP__DESCRIPTION:                     'description';
 SNOWFLAKE_APP__FUNCTION:                        'function';
 SNOWFLAKE_APP__OWNER:                           'owner';
-SNOWFLAKE_APP__TYPE:                           'type';
 SNOWFLAKE_APP__ACTIVATION:                      'activationConfiguration';
 
 // ------------------------------------- CONFIGURATION -------------------------------
