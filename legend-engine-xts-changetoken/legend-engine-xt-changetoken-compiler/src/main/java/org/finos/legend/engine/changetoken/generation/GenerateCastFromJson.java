ea40870cbf6acf0d67faf9ce610ed8585a97cdd7
Mateusz Klatt <mateusz@klatt.ie>
2024-02-14 20:10:45 +0100

    make cast and diff generators configurable (#2623)

--- legend-engine-xts-changetoken/legend-engine-xt-changetoken-compiler/src/main/java/org/finos/legend/engine/changetoken/generation/GenerateCastFromJson.java
+++ legend-engine-xts-changetoken/legend-engine-xt-changetoken-compiler/src/main/java/org/finos/legend/engine/changetoken/generation/GenerateCastFromJson.java
@@ -1,4 +1,4 @@
-//  Copyright 2023 Goldman Sachs
+//  Copyright 2024 Goldman Sachs
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -14,26 +14,23 @@
 
 package org.finos.legend.engine.changetoken.generation;
 
-import org.finos.legend.engine.external.language.java.generation.GenerateJavaProject;
 import org.finos.legend.pure.generated.Root_meta_external_language_java_metamodel_project_Project;
 import org.finos.legend.pure.generated.core_pure_changetoken_cast_generation;
 import org.finos.legend.pure.runtime.java.compiled.execution.CompiledExecutionSupport;
 
-public class GenerateCastFromJson extends GenerateJavaProject
+public class GenerateCastFromJson extends GenerateCastFromVersions
 {
     private final String jsonString;
-    private final String outputClassName;
 
     public GenerateCastFromJson(String outputDirectory, String jsonString, String outputClassName)
     {
-        super(outputDirectory);
+        super(outputDirectory, null, outputClassName);
         this.jsonString = jsonString;
-        this.outputClassName = outputClassName;
     }
 
     @Override
     protected Root_meta_external_language_java_metamodel_project_Project doExecute(CompiledExecutionSupport executionSupport)
     {
-        return core_pure_changetoken_cast_generation.Root_meta_pure_changetoken_cast_generation_generateCastFromJson_String_1__String_1__Project_1_(jsonString, outputClassName, executionSupport);
+        return super.doExecute(core_pure_changetoken_cast_generation.Root_meta_pure_changetoken_cast_generation_jsonToVersions_String_1__String_1__Versions_1_(jsonString, this.getTypeKeyName(), executionSupport), executionSupport);
     }
 }
