af2f478e7083fbdb6f39956486a47e44a7c702a4
Rafael Bey <rafael.bey@gs.com>
2024-04-05 18:32:46 -0400

    Let OS pick the port for H2 server

--- legend-engine-config/legend-engine-repl/legend-engine-repl-relational/src/main/java/org/finos/legend/engine/repl/relational/RelationalReplExtension.java
+++ legend-engine-config/legend-engine-repl/legend-engine-repl-relational/src/main/java/org/finos/legend/engine/repl/relational/RelationalReplExtension.java
@@ -45,11 +45,10 @@ public class RelationalReplExtension implements ReplExtension
 
     static
     {
-        int port = 1024 + (int)(Math.random() * 10000);
-        System.setProperty("legend.test.h2.port", String.valueOf(port));
         try
         {
-            AlloyH2Server.startServer(port);
+            int port = AlloyH2Server.startServer(0).getPort();
+            System.setProperty("legend.test.h2.port", String.valueOf(port));
         }
         catch (SQLException e)
         {
