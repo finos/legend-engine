547c6c045b7b8f504c3e82a4eb5991ee9f41291d
Mauricio Uyaguari <lmauricio12@gmail.com>
2024-04-17 09:50:58 -0400

    add execution context to query (#2787)

---
+++ legend-engine-application-query/src/main/java/org/finos/legend/engine/application/query/model/QueryExecutionContext.java
@@ -0,0 +1,28 @@
+// Copyright 2024 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.application.query.model;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "_type", defaultImpl = QueryExplicitExecutionContext.class)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = QueryExplicitExecutionContext.class, name = "explicitExecutionContext"),
+        @JsonSubTypes.Type(value = QueryDataSpaceExecutionContext.class, name = "dataSpaceExecutionContext"),
+})
+public abstract class QueryExecutionContext
+{
+
+}
