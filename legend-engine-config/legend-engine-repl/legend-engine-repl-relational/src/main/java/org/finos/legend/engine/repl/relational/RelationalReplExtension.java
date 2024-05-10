17f37fc0f07b8dd3675664d03bc84de21da4a112
Rafael Bey <24432403+rafaelbey@users.noreply.github.com>
2024-05-10 18:09:05 -0400

    Ensure REPL grid server set content type header (#2841)

--- legend-engine-config/legend-engine-repl/legend-engine-repl-relational/src/main/java/org/finos/legend/engine/repl/relational/RelationalReplExtension.java
+++ legend-engine-config/legend-engine-repl/legend-engine-repl-relational/src/main/java/org/finos/legend/engine/repl/relational/RelationalReplExtension.java
@@ -81,7 +81,7 @@ public class RelationalReplExtension implements ReplExtension
         }
         catch (Exception e)
         {
-            this.client.getTerminal().writer().println(e.getMessage());
+           throw new RuntimeException(e);
         }
     }
 
