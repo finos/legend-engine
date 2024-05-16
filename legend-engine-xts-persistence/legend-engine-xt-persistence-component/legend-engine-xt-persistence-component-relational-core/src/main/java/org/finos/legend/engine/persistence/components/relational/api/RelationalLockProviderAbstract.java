88b647553b3ff9f9146146825bf2086c56019f8a
prasar-ashutosh <ashutosh.prasar@hk.email.gs.com>
2024-05-16 19:25:14 +0530

    Expose Lock provider as a service

---
+++ legend-engine-xts-persistence/legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-core/src/main/java/org/finos/legend/engine/persistence/components/relational/api/RelationalLockProviderAbstract.java
@@ -0,0 +1,128 @@
+// Copyright 2024 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.persistence.components.relational.api;
+
+import org.finos.legend.engine.persistence.components.executor.Executor;
+import org.finos.legend.engine.persistence.components.logicalplan.LogicalPlan;
+import org.finos.legend.engine.persistence.components.logicalplan.operations.Create;
+import org.finos.legend.engine.persistence.components.logicalplan.operations.Operation;
+import org.finos.legend.engine.persistence.components.logicalplan.values.BatchStartTimestampAbstract;
+import org.finos.legend.engine.persistence.components.relational.CaseConversion;
+import org.finos.legend.engine.persistence.components.relational.RelationalSink;
+import org.finos.legend.engine.persistence.components.relational.SqlPlan;
+import org.finos.legend.engine.persistence.components.relational.sql.TabularData;
+import org.finos.legend.engine.persistence.components.relational.sqldom.SqlGen;
+import org.finos.legend.engine.persistence.components.relational.transformer.RelationalTransformer;
+import org.finos.legend.engine.persistence.components.transformer.TransformOptions;
+import org.finos.legend.engine.persistence.components.transformer.Transformer;
+import org.finos.legend.engine.persistence.components.util.LockInfoDataset;
+import org.finos.legend.engine.persistence.components.util.LockInfoUtils;
+import org.finos.legend.engine.persistence.components.util.PlaceholderValue;
+import org.finos.legend.engine.persistence.components.util.SqlLogging;
+import org.immutables.value.Value;
+import org.immutables.value.Value.Default;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.finos.legend.engine.persistence.components.relational.api.RelationalIngestorAbstract.BATCH_START_TS_PATTERN;
+import static org.finos.legend.engine.persistence.components.transformer.Transformer.TransformOptionsAbstract.DATE_TIME_FORMATTER;
+
+@Value.Immutable
+@Value.Style(
+        typeAbstract = "*Abstract",
+        typeImmutable = "*",
+        jdkOnly = true,
+        optionalAcceptNullable = true,
+        strictBuilder = true
+)
+public abstract class RelationalLockProviderAbstract
+{
+    //---------- FIELDS ----------
+    public abstract RelationalSink relationalSink();
+
+    @Default
+    public CaseConversion caseConversion()
+    {
+        return CaseConversion.NONE;
+    }
+
+    @Default
+    public SqlLogging sqlLogging()
+    {
+        return SqlLogging.DISABLED;
+    }
+
+    @Default
+    public Clock executionTimestampClock()
+    {
+        return Clock.systemUTC();
+    }
+
+    @Value.Derived
+    protected Transformer<SqlGen, SqlPlan> transformer()
+    {
+        TransformOptions.Builder builder = TransformOptions.builder().executionTimestampClock(executionTimestampClock());
+        relationalSink().optimizerForCaseConversion(caseConversion()).ifPresent(builder::addOptimizers);
+        TransformOptions transformOptions = builder.build();
+        return new RelationalTransformer(relationalSink(), transformOptions);
+    }
+
+    // ---------- Private Fields ----------
+    private static final Logger LOGGER = LoggerFactory.getLogger(RelationalLockProvider.class);
+
+    // ------API-----
+    public void createAndInitialize(Executor<SqlGen, TabularData, SqlPlan> executor, LockInfoDataset lockDataset, String tableName)
+    {
+        executor.setSqlLogging(sqlLogging());
+        LOGGER.info("Creating and initializing Lock dataset: " + lockDataset.name());
+        Map<String, PlaceholderValue> placeHolderKeyValues = new HashMap<>();
+        placeHolderKeyValues.put(BATCH_START_TS_PATTERN, PlaceholderValue.of(LocalDateTime.now(executionTimestampClock()).format(DATE_TIME_FORMATTER), false));
+        executor.executePhysicalPlan(createAndInitializeLockSqlPlan(lockDataset, tableName), placeHolderKeyValues);
+    }
+
+    public void lock(Executor<SqlGen, TabularData, SqlPlan> executor, LockInfoDataset lockDataset)
+    {
+        executor.setSqlLogging(sqlLogging());
+        LOGGER.info("Acquiring lock using LockInfoDataset: " + lockDataset.name());
+        Map<String, PlaceholderValue> placeHolderKeyValues = new HashMap<>();
+        placeHolderKeyValues.put(BATCH_START_TS_PATTERN, PlaceholderValue.of(LocalDateTime.now(executionTimestampClock()).format(DATE_TIME_FORMATTER), false));
+        executor.executePhysicalPlan(acquireLockSqlPlan(lockDataset), placeHolderKeyValues);
+    }
+
+    // ------ Utility methods -----
+    public SqlPlan createAndInitializeLockSqlPlan(LockInfoDataset lockInfoDataset, String tableName)
+    {
+        LockInfoDataset enrichedLockInfoDataset = ApiUtils.applyCase(lockInfoDataset, caseConversion());
+        List<Operation> operations = new ArrayList<>();
+        LockInfoUtils lockInfoUtils = new LockInfoUtils(enrichedLockInfoDataset);
+        operations.add(Create.of(true, enrichedLockInfoDataset.get()));
+        operations.add(lockInfoUtils.initializeLockInfo(tableName, BatchStartTimestampAbstract.INSTANCE));
+        LogicalPlan initializeLockLogicalPlan = LogicalPlan.of(operations);
+        return transformer().generatePhysicalPlan(initializeLockLogicalPlan);
+    }
+
+    public SqlPlan acquireLockSqlPlan(LockInfoDataset lockInfoDataset)
+    {
+        LockInfoDataset enrichedLockInfoDataset = ApiUtils.applyCase(lockInfoDataset, caseConversion());
+        LockInfoUtils lockInfoUtils = new LockInfoUtils(enrichedLockInfoDataset);
+        LogicalPlan acquireLockLogicalPlan = LogicalPlan.of(Collections.singleton(lockInfoUtils.updateLockInfo(BatchStartTimestampAbstract.INSTANCE)));
+        return transformer().generatePhysicalPlan(acquireLockLogicalPlan);
+    }
+
+}
---
+++ legend-engine-xts-persistence/legend-engine-xt-persistence-component/legend-engine-xt-persistence-component-relational-h2/src/test/java/org/finos/legend/engine/persistence/components/ingestmode/mixed/RelationalLockProviderMultiThreadTest.java
@@ -0,0 +1,60 @@
+// Copyright 2024 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.persistence.components.ingestmode.mixed;
+
+import org.finos.legend.engine.persistence.components.BaseTest;
+import org.finos.legend.engine.persistence.components.RelationalSinkCleanerTest;
+import org.finos.legend.engine.persistence.components.executor.Executor;
+import org.finos.legend.engine.persistence.components.relational.api.RelationalIngestor;
+import org.finos.legend.engine.persistence.components.relational.api.RelationalLockProvider;
+import org.finos.legend.engine.persistence.components.relational.h2.H2Sink;
+import org.finos.legend.engine.persistence.components.relational.jdbc.JdbcConnection;
+import org.finos.legend.engine.persistence.components.util.LockInfoDataset;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RelationalLockProviderMultiThreadTest extends BaseTest
+{
+
+    /*
+    initial value of batch_id = 1
+    Two threads are trying to read and increment the value of this batch_id
+    without lock , there will be race condition and the final value could be 2
+    With lock , the final value should be 3
+     */
+    @Test
+    public void testMultiThreads() throws InterruptedException
+    {
+        LockInfoDataset lockInfoDataset = LockInfoDataset.builder().name("main_legend_persistence_lock").build();
+        RelationalLockProvider lockProvider = RelationalLockProvider.builder()
+                .relationalSink(H2Sink.get())
+                .executionTimestampClock(fixedClock_2000_01_01)
+                .build();
+        Executor executor = RelationalIngestor.getExecutor(H2Sink.get(), JdbcConnection.of(h2Sink.connection()));
+        RelationalSinkCleanerTest.createBatchMetadataTableWithData("batch_metadata", "main");
+        lockProvider.createAndInitialize(executor, lockInfoDataset, "main");
+
+        Thread t1 = new Thread(new TestRunner(lockProvider, H2_USER_NAME, H2_PASSWORD, H2_JDBC_URL));
+        Thread t2 = new Thread(new TestRunner(lockProvider, H2_USER_NAME, H2_PASSWORD, H2_JDBC_URL));
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        String maxBatchIdSql = "select max(table_batch_id) as batch_id from batch_metadata";
+        int maxBatchId = (int) h2Sink.executeQuery(maxBatchIdSql).get(0).get("batch_id");
+        Assertions.assertEquals(3, maxBatchId);
+    }
+
+}
