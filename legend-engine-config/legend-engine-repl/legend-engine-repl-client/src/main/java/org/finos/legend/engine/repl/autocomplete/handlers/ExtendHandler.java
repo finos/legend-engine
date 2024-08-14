c553ca2293f95270ae532aaf065e395a9d3fa7e4
Pierre De Belen <pierre.debelen@gmail.com>
2024-08-14 13:41:34 -0400

    Add support for autocomplete for OLAP queries (#3015)

--- legend-engine-config/legend-engine-repl/legend-engine-repl-client/src/main/java/org/finos/legend/engine/repl/autocomplete/handlers/ExtendHandler.java
+++ legend-engine-config/legend-engine-repl/legend-engine-repl-client/src/main/java/org/finos/legend/engine/repl/autocomplete/handlers/ExtendHandler.java
@@ -14,17 +14,27 @@
 
 package org.finos.legend.engine.repl.autocomplete.handlers;
 
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.impl.utility.ListIterate;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.ProcessingContext;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.ValueSpecification;
 import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.Variable;
 import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.application.AppliedFunction;
 import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.raw.ClassInstance;
+import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.raw.Collection;
 import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.raw.Lambda;
 import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.raw.classInstance.relation.ColSpec;
 import org.finos.legend.engine.protocol.pure.v1.model.valueSpecification.raw.classInstance.relation.ColSpecArray;
+import org.finos.legend.engine.repl.autocomplete.Completer;
+import org.finos.legend.engine.repl.autocomplete.CompletionItem;
 import org.finos.legend.engine.repl.autocomplete.FunctionHandler;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.type.generics.GenericType;
+import org.finos.legend.pure.m3.navigation.M3Paths;
+
+import static org.finos.legend.engine.repl.autocomplete.Completer.proposeColumnNamesForEditColSpec;
+import static org.finos.legend.engine.repl.autocomplete.handlers.GroupByHandler.updateColSpecSecondFunction;
 
 public class ExtendHandler extends FunctionHandler
 {
@@ -35,34 +45,69 @@ public class ExtendHandler extends FunctionHandler
     }
 
     @Override
+    public MutableList<CompletionItem> proposedParameters(AppliedFunction currentFunc, GenericType leftType, PureModel pureModel, Completer completer, ProcessingContext processingContext, ValueSpecification currentVS)
+    {
+        if (currentFunc.parameters.size() == 2 && currentFunc.parameters.get(1) instanceof AppliedFunction && "over".equals(((AppliedFunction) currentFunc.parameters.get(1)).function))
+        {
+            AppliedFunction over = (AppliedFunction) currentFunc.parameters.get(1);
+            if (!over.parameters.isEmpty())
+            {
+                Object parameter = over.parameters.get(over.parameters.size() - 1);
+                if (parameter instanceof ClassInstance)
+                {
+                    return proposeColumnNamesForEditColSpec(((ClassInstance) parameter).value, leftType);
+                }
+                else if (parameter instanceof Collection)
+                {
+                    Object lastValue = ((Collection) parameter).values.get(((Collection) parameter).values.size() - 1);
+                    if (lastValue instanceof ClassInstance)
+                    {
+                        return proposeColumnNamesForEditColSpec(((ClassInstance) lastValue).value, leftType);
+                    }
+                }
+            }
+        }
+        return Lists.mutable.empty();
+    }
+
+    @Override
     public void handleFunctionAppliedParameters(AppliedFunction currentFunc, GenericType leftType, ProcessingContext processingContext, PureModel pureModel)
     {
-        if (currentFunc.parameters.size() > 1 && currentFunc.parameters.get(1) instanceof ClassInstance)
+        if (currentFunc.parameters.size() > 1 && currentFunc.parameters.get(currentFunc.parameters.size() - 1) instanceof ClassInstance)
         {
-            updateColSpecs(((ClassInstance) currentFunc.parameters.get(1)).value, leftType, processingContext, pureModel);
+            Object val = ((ClassInstance) currentFunc.parameters.get(currentFunc.parameters.size() - 1)).value;
+            updateColSpecFirstFunction(val, leftType, processingContext, pureModel);
+            updateColSpecSecondFunction(processingContext, pureModel, val);
         }
     }
 
-    public static void updateColSpecs(Object o, GenericType leftType, ProcessingContext processingContext, PureModel pureModel)
+    public static void updateColSpecFirstFunction(Object o, GenericType leftType, ProcessingContext processingContext, PureModel pureModel)
     {
         GenericType propertyType = leftType._typeArguments().getFirst();
 
         if (o instanceof ColSpec)
         {
-            processColSpec((ColSpec) o, propertyType, processingContext, pureModel);
+            processColSpecFirstFunction((ColSpec) o, propertyType, processingContext, pureModel);
         }
         if (o instanceof ColSpecArray)
         {
-            ListIterate.forEach(((ColSpecArray) o).colSpecs, x -> processColSpec(x, propertyType, processingContext, pureModel));
+            ListIterate.forEach(((ColSpecArray) o).colSpecs, x -> processColSpecFirstFunction(x, propertyType, processingContext, pureModel));
         }
     }
 
-    private static void processColSpec(ColSpec o, GenericType propertyType, ProcessingContext processingContext, PureModel pureModel)
+    private static void processColSpecFirstFunction(ColSpec o, GenericType propertyType, ProcessingContext processingContext, PureModel pureModel)
     {
         Lambda lambda = o.function1;
         if (lambda != null)
         {
-            Variable variable = lambda.parameters.get(0);
+            if (lambda.parameters.size() > 1)
+            {
+                Variable p = lambda.parameters.get(0);
+                processingContext.addInferredVariables(p.name, buildTypedVariable(p, pureModel.getGenericType(M3Paths.Relation), pureModel.getMultiplicity("one"), pureModel));
+                Variable f = lambda.parameters.get(1);
+                processingContext.addInferredVariables(f.name, buildTypedVariable(f, pureModel.getGenericType("meta::pure::functions::relation::Frame"), pureModel.getMultiplicity("one"), pureModel));
+            }
+            Variable variable = lambda.parameters.get(lambda.parameters.size() - 1);
             processingContext.addInferredVariables(variable.name, buildTypedVariable(variable, propertyType, pureModel.getMultiplicity("one"), pureModel));
         }
     }
