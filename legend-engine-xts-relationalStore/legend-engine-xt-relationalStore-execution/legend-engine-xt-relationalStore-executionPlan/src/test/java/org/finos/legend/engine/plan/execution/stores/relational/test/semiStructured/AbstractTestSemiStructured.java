8e739dfe56a1c63a08cb94d2c8eea126f93ef713
Mohammed Ibrahim <mohammed.ibrahim@gs.com>
2024-08-20 09:26:59 -0400

    Relax semi structured column exclusion

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-execution/legend-engine-xt-relationalStore-executionPlan/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/semiStructured/AbstractTestSemiStructured.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-execution/legend-engine-xt-relationalStore-executionPlan/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/semiStructured/AbstractTestSemiStructured.java
@@ -79,8 +79,7 @@ public abstract class AbstractTestSemiStructured
 
         return core_pure_executionPlan_executionPlan_print.Root_meta_pure_executionPlan_toString_planToString_ExecutionPlan_1__Boolean_1__Extension_MANY__String_1_(executionPlan, true, extensions, pureModel.getExecutionSupport());
     }
-
-    protected String executeFunction(String function, String mapping, String runtime)
+    protected SingleExecutionPlan generatePlan(String function, String mapping, String runtime)
     {
         Function functionObject = Objects.requireNonNull(contextData.getElementsOfType(Function.class).stream().filter(x -> function.equals(x._package + "::" + x.name)).findFirst().orElse(null));
 
@@ -96,6 +95,12 @@ public abstract class AbstractTestSemiStructured
                 getExtensions(),
                 LegendPlanTransformers.transformers
         );
+        return executionPlan;
+    }
+
+    protected String executeFunction(String function, String mapping, String runtime)
+    {
+        SingleExecutionPlan executionPlan = generatePlan(function,mapping,runtime);
 
         RelationalResult result = (RelationalResult) this.planExecutor.execute(executionPlan);
         return new String(new RelationalResultToCSVSerializer(result).flush().toByteArray(), StandardCharsets.UTF_8);
