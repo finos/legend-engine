0ad0fbdcc037a44ccd607bd2513c33ce23554fc1
jackie-h <10471950+jackie-h@users.noreply.github.com>
2024-11-21 18:15:59 +0000

    Implement strategic database connection

---
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-oracle/legend-engine-xt-relationalStore-oracle-execution/src/main/java/org/finos/legend/engine/plan/execution/stores/relational/connection/ds/specifications/OracleDataSourceSpecification.java
@@ -0,0 +1,77 @@
+// Copyright 2021 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.plan.execution.stores.relational.connection.ds.specifications;
+
+import org.finos.legend.engine.plan.execution.stores.relational.connection.authentication.AuthenticationStrategy;
+import org.finos.legend.engine.plan.execution.stores.relational.connection.driver.DatabaseManager;
+import org.finos.legend.engine.plan.execution.stores.relational.connection.ds.DataSourceSpecification;
+import org.finos.legend.engine.plan.execution.stores.relational.connection.ds.specifications.keys.OracleDatasourceSpecificationKey;
+
+import java.util.Optional;
+import java.util.Properties;
+
+import static java.util.Optional.ofNullable;
+
+public class OracleDataSourceSpecification extends DataSourceSpecification
+{
+    public static String ORACLE_HOST = "host";
+    public static String ORACLE_PORT = "port";
+    public static String ORACLE_DATABASE_NAME = "databaseName";
+
+    private final OracleDatasourceSpecificationKey key;
+
+    public OracleDataSourceSpecification(OracleDatasourceSpecificationKey key, DatabaseManager databaseManager, AuthenticationStrategy authenticationStrategy, Properties extraUserProperties)
+    {
+        super(key, databaseManager, authenticationStrategy, extraUserProperties);
+        this.key = key;
+        this.extraDatasourceProperties.putAll(getProperties());
+    }
+
+    public OracleDataSourceSpecification(OracleDatasourceSpecificationKey key, DatabaseManager databaseManager, AuthenticationStrategy authenticationStrategy, Properties extraUserProperties, int maxPoolSize, int minPoolSize)
+    {
+        super(key, databaseManager, authenticationStrategy, extraUserProperties, maxPoolSize, minPoolSize);
+        this.key = key;
+        this.extraDatasourceProperties.putAll(getProperties());
+    }
+
+    private Properties getProperties()
+    {
+        Properties properties = new Properties();
+
+        ofNullable(key.getDatabaseName()).ifPresent(x -> properties.setProperty(ORACLE_DATABASE_NAME, x));
+
+        return properties;
+    }
+
+    private void putIfNotEmpty(Properties connectionProperties, String propName, String propValue)
+    {
+        Optional.ofNullable(propValue).ifPresent(x -> connectionProperties.put(propName, propValue));
+    }
+
+    public OracleDataSourceSpecification(OracleDatasourceSpecificationKey key, DatabaseManager databaseManager, AuthenticationStrategy authenticationStrategy)
+    {
+        this(key, databaseManager, authenticationStrategy, new Properties());
+    }
+
+    @Override
+    protected String getJdbcUrl(String host, int port, String databaseName, Properties properties)
+    {
+        return super.getJdbcUrl(
+                ((OracleDatasourceSpecificationKey) this.datasourceKey).getHost(),
+                ((OracleDatasourceSpecificationKey) this.datasourceKey).getPort(),
+                ((OracleDatasourceSpecificationKey) this.datasourceKey).getDatabaseName(),
+                properties);
+    }
+}
--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-oracle/legend-engine-xt-relationalStore-oracle-protocol/src/main/java/org/finos/legend/engine/protocol/pure/v1/model/packageableElement/store/relational/connection/specification/OracleDatasourceSpecification.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-oracle/legend-engine-xt-relationalStore-oracle-protocol/src/main/java/org/finos/legend/engine/protocol/pure/v1/model/packageableElement/store/relational/connection/specification/OracleDatasourceSpecification.java
@@ -18,7 +18,7 @@ public class OracleDatasourceSpecification extends DatasourceSpecification
 {
     public String host;
     public int port;
-    public String schema;
+    public String databaseName;
 
 
     @Override
