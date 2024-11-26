e03815fecc4f7b6f1a8058fa36fc3f61843ecdd8
Stebbins, Travis [Engineering] <travis.stebbins@ny.email.gs.com>
2024-11-26 15:54:52 -0500

    feat: Move light graph building into its own class

--- legend-engine-xts-analytics/legend-engine-xts-analytics-mapping/legend-engine-xt-analytics-mapping-http-api/src/main/java/org/finos/legend/engine/api/analytics/MappingAnalytics.java
+++ legend-engine-xts-analytics/legend-engine-xts-analytics-mapping/legend-engine-xt-analytics-mapping-http-api/src/main/java/org/finos/legend/engine/api/analytics/MappingAnalytics.java
@@ -20,22 +20,18 @@ import io.opentracing.util.GlobalTracer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.eclipse.collections.api.RichIterable;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.impl.utility.ListIterate;
 import org.finos.legend.engine.api.analytics.model.MappingModelCoverageAnalysisInput;
 import org.finos.legend.engine.api.analytics.model.MappingRuntimeCompatibilityAnalysisInput;
 import org.finos.legend.engine.api.analytics.model.MappingRuntimeCompatibilityAnalysisResult;
-import org.finos.legend.engine.language.pure.compiler.fromPureGraph.PureModelContextDataGenerator;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.HelperModelBuilder;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.HelperRuntimeBuilder;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
-import org.finos.legend.engine.language.pure.grammar.to.HelperValueSpecificationGrammarComposer;
 import org.finos.legend.engine.language.pure.modelManager.ModelManager;
+import org.finos.legend.engine.protocol.analytics.model.MappingModelCoverageAnalysisLightGraphBuilder;
 import org.finos.legend.engine.protocol.analytics.model.MappingModelCoverageAnalysisResult;
 import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
-import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.domain.Function;
-import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.externalFormat.Binding;
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.runtime.PackageableRuntime;
 import org.finos.legend.engine.shared.core.ObjectMapperFactory;
 import org.finos.legend.engine.shared.core.api.result.ManageConstantResult;
@@ -44,20 +40,10 @@ import org.finos.legend.engine.shared.core.kerberos.ProfileManagerHelper;
 import org.finos.legend.engine.shared.core.operational.errorManagement.ExceptionTool;
 import org.finos.legend.engine.shared.core.operational.http.InflateInterceptor;
 import org.finos.legend.engine.shared.core.operational.logs.LoggingEventType;
-import org.finos.legend.pure.generated.Root_meta_analytics_binding_modelCoverage_BindingModelCoverageAnalysisResult;
-import org.finos.legend.pure.generated.Root_meta_analytics_function_modelCoverage_FunctionModelCoverageAnalysisResult;
 import org.finos.legend.pure.generated.Root_meta_analytics_mapping_modelCoverage_MappingModelCoverageAnalysisResult;
-import org.finos.legend.pure.generated.Root_meta_external_format_shared_binding_Binding;
-import org.finos.legend.pure.generated.core_analytics_binding_modelCoverage_analytics;
-import org.finos.legend.pure.generated.core_analytics_function_modelCoverage_analytics;
 import org.finos.legend.pure.generated.core_analytics_mapping_modelCoverage_analytics;
 import org.finos.legend.pure.generated.core_analytics_mapping_modelCoverage_serializer;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.mapping.Mapping;
-import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.extension.Profile;
-import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.ConcreteFunctionDefinition;
-import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.type.Class;
-import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.type.Enum;
-import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.type.Enumeration;
 import org.pac4j.core.profile.CommonProfile;
 import org.pac4j.core.profile.ProfileManager;
 import org.pac4j.jax.rs.annotations.Pac4JProfileManager;
@@ -70,9 +56,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Api(tags = "Analytics - Model")
 @Path("pure/v1/analytics/mapping")
@@ -108,74 +91,9 @@ public class MappingAnalytics
             {
                 Root_meta_analytics_mapping_modelCoverage_MappingModelCoverageAnalysisResult analysisResult = core_analytics_mapping_modelCoverage_analytics.Root_meta_analytics_mapping_modelCoverage_analyze_Mapping_1__Boolean_1__Boolean_1__Boolean_1__MappingModelCoverageAnalysisResult_1_(mapping, returnMappedEntityInfo, returnMappedPropertyInfo, returnLightGraph, pureModel.getExecutionSupport());
                 MappingModelCoverageAnalysisResult result = this.objectMapper.readValue(core_analytics_mapping_modelCoverage_serializer.Root_meta_analytics_mapping_modelCoverage_serialization_json_getSerializedMappingModelCoverageAnalysisResult_MappingModelCoverageAnalysisResult_1__String_1_(analysisResult, pureModel.getExecutionSupport()), MappingModelCoverageAnalysisResult.class);
-                PureModelContextData.Builder builder = PureModelContextData.newBuilder();
                if (returnLightGraph)
                {
-                   // Here we prune the bindings to have just packageableIncludes part of ModelUnit
-                   // because we only need that as a part of analytics.
-                   List<String> bindingPaths = pureModelContextData.getElements().stream().filter(el -> el instanceof Binding).map(b ->
-                   {
-                       Binding _binding = new Binding();
-                       _binding.name = b.name;
-                       _binding.contentType = ((Binding) b).contentType;
-                       _binding._package = b._package;
-                       _binding.modelUnit = ((Binding) b).modelUnit;
-                       _binding.modelUnit.packageableElementExcludes = org.eclipse.collections.api.factory.Lists.mutable.empty();
-                       builder.addElement(_binding);
-                       return b.getPath();
-                   }).collect(Collectors.toList());
-                   RichIterable<? extends Root_meta_external_format_shared_binding_Binding> bindings = org.eclipse.collections.api.factory.Lists.mutable.ofAll(bindingPaths.stream().map(path ->
-                   {
-                       Root_meta_external_format_shared_binding_Binding binding;
-                       try
-                       {
-                           binding = (Root_meta_external_format_shared_binding_Binding) pureModel.getPackageableElement(path);
-                           return binding;
-                       }
-                       catch (Exception ignored)
-                       {
-
-                       }
-                       return null;
-                   }).filter(c -> c != null).collect(Collectors.toList()));
-                   Root_meta_analytics_binding_modelCoverage_BindingModelCoverageAnalysisResult bindingAnalysisResult = core_analytics_binding_modelCoverage_analytics.Root_meta_analytics_binding_modelCoverage_getBindingModelCoverage_Binding_MANY__BindingModelCoverageAnalysisResult_1_(bindings, pureModel.getExecutionSupport());
-                   List<String> functionPaths = pureModelContextData.getElements().stream().filter(el -> el instanceof Function).map(e -> e.getPath()).collect(Collectors.toList());
-                   List<String> allExtraElements = functionPaths;
-                   pureModelContextData.getElements().stream().filter(el -> allExtraElements.contains(el.getPath())).forEach(builder::addElement);
-                   List<org.finos.legend.engine.protocol.pure.v1.model.packageableElement.PackageableElement> elements = builder.build().getElements();
-                   RichIterable<? extends ConcreteFunctionDefinition<? extends  Object>> functions = org.eclipse.collections.api.factory.Lists.mutable.ofAll(functionPaths.stream().map(path ->
-                   {
-                       ConcreteFunctionDefinition<? extends Object> function = null;
-                       try
-                       {
-                           function = pureModel.getConcreteFunctionDefinition_safe(path);
-                           if (function == null)
-                           {
-                               Function _function = (Function) elements.stream().filter(e -> e.getPath().equals(path)).findFirst().get();
-                               function = pureModel.getConcreteFunctionDefinition_safe(path + HelperValueSpecificationGrammarComposer.getFunctionSignature(_function));
-
-                           }
-                           return function;
-                       }
-                       catch (Exception ignored)
-                       {
-
-                       }
-                       return null;
-                   }).filter(c -> c != null).collect(Collectors.toList()));
-                   Root_meta_analytics_function_modelCoverage_FunctionModelCoverageAnalysisResult functionCoverageAnalysisResult = core_analytics_function_modelCoverage_analytics.Root_meta_analytics_function_modelCoverage_getFunctionModelCoverage_ConcreteFunctionDefinition_MANY__FunctionModelCoverageAnalysisResult_1_(org.eclipse.collections.impl.factory.Lists.mutable.ofAll(functions), pureModel.getExecutionSupport());
-                   MutableList<? extends Class<? extends Object>> coveredClasses = analysisResult._classes().toList();
-                   List<String> coveredClassesPaths = coveredClasses.stream().map(c -> HelperModelBuilder.getElementFullPath(c, pureModel.getExecutionSupport())).collect(Collectors.toList());
-                   coveredClasses = org.eclipse.collections.impl.factory.Lists.mutable.ofAll(Stream.concat(Stream.concat(functionCoverageAnalysisResult._classes().toList().stream().filter(c -> !coveredClassesPaths.contains(HelperModelBuilder.getElementFullPath(c, pureModel.getExecutionSupport()))),
-                           bindingAnalysisResult._classes().toList().stream().filter(c -> !coveredClassesPaths.contains(HelperModelBuilder.getElementFullPath(c, pureModel.getExecutionSupport())))).distinct(),
-                           analysisResult._classes().toList().stream()).collect(Collectors.toList()));
-                   MutableList<Enumeration<? extends Enum>> coveredEnumerations = org.eclipse.collections.impl.factory.Lists.mutable.ofAll(Stream.concat(analysisResult._enumerations().toList().stream(), functionCoverageAnalysisResult._enumerations().toList().stream()).distinct().collect(Collectors.toList()));
-                   PureModelContextData classes = PureModelContextDataGenerator.generatePureModelContextDataFromClasses(coveredClasses, input.clientVersion, pureModel.getExecutionSupport());
-                   PureModelContextData enums = PureModelContextDataGenerator.generatePureModelContextDataFromEnumerations(coveredEnumerations, input.clientVersion, pureModel.getExecutionSupport());
-                   PureModelContextData _profiles = PureModelContextDataGenerator.generatePureModelContextDataFromProfile((RichIterable<Profile>) analysisResult._profiles(), input.clientVersion, pureModel.getExecutionSupport());
-                   PureModelContextData associations = PureModelContextDataGenerator.generatePureModelContextDataFromAssociations(analysisResult._associations(), input.clientVersion, pureModel.getExecutionSupport());
-
-                   result.model = builder.build().combine(classes).combine(enums).combine(_profiles).combine(associations);
+                   result.model = MappingModelCoverageAnalysisLightGraphBuilder.buildLightGraph(analysisResult, pureModel, pureModelContextData, input.clientVersion);
                }
                 return ManageConstantResult.manageResult(identity.getName(), result);
             }
