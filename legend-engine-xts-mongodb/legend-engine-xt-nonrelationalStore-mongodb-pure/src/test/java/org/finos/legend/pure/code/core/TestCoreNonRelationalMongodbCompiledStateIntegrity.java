1df271df80e7b6da7ace370879aa565a7b88eb1e
Aziem Chawdhary <aziem.chawdhary@gs.com>
2025-01-14 19:27:54 +0000

    Revert "Add batch 2 of compiled state integrity tests (#3347)"
    This reverts commit adc50cfdeb8b65175a255d52eacea2d6461fd910.

--- legend-engine-xts-mongodb/legend-engine-xt-nonrelationalStore-mongodb-pure/src/test/java/org/finos/legend/pure/code/core/TestCoreNonRelationalMongodbCompiledStateIntegrity.java
+++ legend-engine-xts-mongodb/legend-engine-xt-nonrelationalStore-mongodb-pure/src/test/java/org/finos/legend/pure/code/core/TestCoreNonRelationalMongodbCompiledStateIntegrity.java
@@ -16,6 +16,7 @@ package org.finos.legend.pure.code.core;
 
 import org.finos.legend.pure.m3.tests.AbstractCompiledStateIntegrityTest;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestCoreNonRelationalMongodbCompiledStateIntegrity extends AbstractCompiledStateIntegrityTest
