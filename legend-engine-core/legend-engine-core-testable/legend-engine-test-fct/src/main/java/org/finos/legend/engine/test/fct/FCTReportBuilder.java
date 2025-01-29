e66875aa560bcbaddeaa7ece6b4dbef92e754122
AFine-gs <69924417+AFine-gs@users.noreply.github.com>
2025-01-29 13:45:36 -0500

    Add fct matrix (#3364)
    * [maven-release-plugin] prepare release legend-engine-4.69.0
    
    * [maven-release-plugin] prepare for next development iteration
    
    * add FCT test runner
    
    * add FCT test runner
    
    * add FCT test runner
    
    * add fct matrix
    
    * add fct matrix
    
    * add fct matrix
    
    * fct feature matrix
    
    * update fct report generation
    
    * update relational POM
    
    * revert pure core skip
    
    * clean up new repo
    
    * exclude FCT Tests
    
    * update pom
    
    * Revert "update pom"
    
    This reverts commit f5588878ff40aa83416e3b4ec1b0d92d27a5c0a2.
    
    * Revert "[maven-release-plugin] prepare for next development iteration"
    
    This reverts commit 5bec6683889f68b5f2c56f594d34a0fcc6300f8f.
    
    * Revert "[maven-release-plugin] prepare release legend-engine-4.69.0"
    
    This reverts commit 608cc8eabd310cb015151020be3cf20520c43b6e.
    
    * add test extensions
    
    * Ignoring long running Integrity tests (#3376)
    
    * Reuse qualifiers in XStore expressions (#3374)
    
    * Revert "Revert "update pom""
    
    This reverts commit b19c7def94e4033004a9e41151e1bcc4a61a0723.
    
    * update fct matrix
    
    * Ignoring long running Integrity tests (#3376)
    
    * Reuse qualifiers in XStore expressions (#3374)
    
    * update pom
    
    ---------
    
    Co-authored-by: FINOS Administrator <37706051+finos-admin@users.noreply.github.com>
    Co-authored-by: Sean Borland <sean.borland@ny.email.gs.com>
    Co-authored-by: vijaylather <63856272+vijaylather@users.noreply.github.com>

---
+++ legend-engine-core/legend-engine-core-testable/legend-engine-test-fct/src/main/java/org/finos/legend/engine/test/fct/FCTReportBuilder.java
@@ -0,0 +1,100 @@
+// Copyright 2022 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package org.finos.legend.engine.test.fct;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.map.MutableMap;
+import org.eclipse.collections.impl.lazy.parallel.set.sorted.SelectSortedSetBatch;
+import org.eclipse.collections.impl.list.mutable.FastList;
+import org.finos.legend.engine.test.fct.model.FCTTestReport;
+import org.finos.legend.engine.test.fct.model.FCTTestResult;
+import org.finos.legend.engine.test.fct.model.FeatureTest;
+import org.finos.legend.pure.generated.Root_meta_pure_extension_Extension;
+
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.ConcreteFunctionDefinition;
+import org.finos.legend.pure.m3.pct.reports.config.PCTReportConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+//import static org.finos.legend.pure.generated.core_analytics_test_coverage_modelCoverage_analytics;
+import static org.finos.legend.pure.generated.core_analytics_test_coverage_modelCoverage_analytics.Root_meta_analytics_testCoverage_featureMatrix_buildStoreReportJSON_ConcreteFunctionDefinition_MANY__String_1__String_1__String_1_;
+import static org.finos.legend.pure.runtime.java.compiled.testHelper.PureTestBuilderCompiled.getClassLoaderExecutionSupport;
+
+public class FCTReportBuilder
+{
+
+
+    public static List<FCTTestReport> generateReport(ImmutableList<FCTReport> reports, MutableList<? extends Root_meta_pure_extension_Extension> extensions)
+    {
+       ObjectMapper mapper = new ObjectMapper();
+       FastList<FCTTestReport> results = FastList.newList();
+       reports.forEach(report ->
+               {
+
+                   MutableMap<String, String> explodedExpectedFailures = PCTReportConfiguration.explodeExpectedFailures(report.expectedFailures(), getClassLoaderExecutionSupport().getProcessorSupport());
+
+                   //list of mappingtests
+                   RichIterable<? extends ConcreteFunctionDefinition<? extends Object>> testfunctions = (RichIterable<? extends ConcreteFunctionDefinition<? extends Object>>) (RichIterable<?>) Lists.mutable.withAll(report.getTestCollection().stream()
+                           .flatMap(c -> c.getAllTestFunctions().stream()).map(c -> (ConcreteFunctionDefinition<? extends Object>) c).collect(Collectors.toList()));
+                   String json = Root_meta_analytics_testCoverage_featureMatrix_buildStoreReportJSON_ConcreteFunctionDefinition_MANY__String_1__String_1__String_1_(testfunctions, report.getreportID(), report.getStoreID(), getClassLoaderExecutionSupport());
+                   try
+                   {
+                       FCTTestResult[] fctTestResults = mapper.readValue(json, FCTTestResult[].class);
+                       for (FCTTestResult testResult : fctTestResults)
+                       {
+                           FCTTestReport fctTestReport = new FCTTestReport(testResult);
+                           if (testResult.featureTests != null)
+                           {
+                               for (FeatureTest featureTest : testResult.featureTests)
+                               {
+                                   fctTestReport.functionName = featureTest.functionName;
+                                   String error = explodedExpectedFailures.get(featureTest.functionName);
+                                   fctTestReport.success = (error == null  && featureTest.expectedError == null   && Objects.equals(featureTest.assertionType, "assertion"));
+                                   fctTestReport.errorMessage = featureTest.expectedError != null ? featureTest.expectedError : error;
+                                   if (Objects.equals(featureTest.assertionType, "assertion"))
+                                   {
+                                       fctTestReport.assertionType = Objects.equals(testResult.testType, "LineageFCT") ? "Lineage" : "Execution";
+                                   }
+                                   else
+                                   {
+                                       fctTestReport.assertionType = featureTest.assertionType;
+                                   }
+                               }
+                           }
+                           results.add(fctTestReport);
+
+                       }
+
+                   }
+                   catch (JsonProcessingException e)
+                   {
+                       throw new RuntimeException(e);
+                   }
+               }
+       );
+        return results;
+    }
+
+}
+
