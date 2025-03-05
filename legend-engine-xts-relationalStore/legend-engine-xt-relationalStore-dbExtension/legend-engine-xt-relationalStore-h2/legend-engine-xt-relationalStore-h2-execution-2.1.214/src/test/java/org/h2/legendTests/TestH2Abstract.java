37646dfe2b89f175ac527c50f9ca47ca16134037
Aziem Chawdhary <aziem.chawdhary@gs.com>
2025-03-05 12:48:40 +0000

    Fix checkstyle warnings

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-h2/legend-engine-xt-relationalStore-h2-execution-2.1.214/src/test/java/org/h2/legendTests/TestH2Abstract.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-h2/legend-engine-xt-relationalStore-h2-execution-2.1.214/src/test/java/org/h2/legendTests/TestH2Abstract.java
@@ -1,3 +1,17 @@
+//  Copyright 2025 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package org.h2.legendTests;
 
 import org.junit.AfterClass;
@@ -17,7 +31,7 @@ public class TestH2Abstract
     {
         Class.forName("org.h2.Driver"); // Driver name
         String defaultH2Properties = ";NON_KEYWORDS=ANY,ASYMMETRIC,AUTHORIZATION,CAST,CURRENT_PATH,CURRENT_ROLE,DAY,DEFAULT,ELSE,END,HOUR,KEY,MINUTE,MONTH,SECOND,SESSION_USER,SET,SOME,SYMMETRIC,SYSTEM_USER,TO,UESCAPE,USER,VALUE,WHEN,YEAR;MODE=LEGACY";
-        String url = "jdbc:h2:~/test"+ defaultH2Properties;
+        String url = "jdbc:h2:~/test" + defaultH2Properties;
         h2Conn = DriverManager.getConnection(url);
         System.out.println("H2 Connection Acquired....");
         h2ConnStatement = h2Conn.createStatement();
