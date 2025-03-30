b959e29c16d1d914d583027fb22341a5311c789c
Yasirmod17 <Mohammed.Ibrahim@gs.com>
2025-03-29 23:22:21 -0400

    Fix style

--- legend-engine-config/legend-engine-server/legend-engine-server-http-server/src/test/java/org/finos/legend/engine/server/TestSemiStructuredWrite.java
+++ legend-engine-config/legend-engine-server/legend-engine-server-http-server/src/test/java/org/finos/legend/engine/server/TestSemiStructuredWrite.java
@@ -1,3 +1,17 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.finos.legend.engine.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -173,7 +187,7 @@ public class TestSemiStructuredWrite
         try (Statement statement = testDBConnection.createStatement())
         {
            ResultSet r =  statement.executeQuery(s);
-           while( r.next())
+           while (r.next())
            {
 //               r.next();
                System.out.println(r.getString(1) + " " + r.getString(2) + " "  + r.getString(3) + " "  + r.getString(4));
