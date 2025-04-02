abae95dd5ae3c0ff132217809bd5cb71ca57f09a
janeenyamak1 <janeen.yamak@gs.com>
2025-04-02 12:07:55 -0500

    Support generation of Tabular Functions when building databases in studio

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-execution/legend-engine-xt-relationalStore-executionPlan/src/main/java/org/finos/legend/engine/plan/execution/stores/relational/exploration/model/DatabaseBuilderConfig.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-execution/legend-engine-xt-relationalStore-executionPlan/src/main/java/org/finos/legend/engine/plan/execution/stores/relational/exploration/model/DatabaseBuilderConfig.java
@@ -28,6 +28,8 @@ public class DatabaseBuilderConfig
     public boolean enrichPrimaryKeys;
 
     public boolean enrichColumns;
+    
+    public boolean enrichTableFunctions = false;
 
     public List<DatabasePattern> patterns = Lists.mutable.empty();
 
