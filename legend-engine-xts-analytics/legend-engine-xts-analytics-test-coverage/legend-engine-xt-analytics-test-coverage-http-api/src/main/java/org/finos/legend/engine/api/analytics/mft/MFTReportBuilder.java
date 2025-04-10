73aa5960b317c064bd30a47ffc4991148e9c2a49
afine-gs <adam.fine@ny.email.gs.com>
2025-04-10 08:22:14 -0400

    update mappingtestgen

--- legend-engine-xts-analytics/legend-engine-xts-analytics-test-coverage/legend-engine-xt-analytics-test-coverage-http-api/src/main/java/org/finos/legend/engine/api/analytics/mft/MFTReportBuilder.java
+++ legend-engine-xts-analytics/legend-engine-xts-analytics-test-coverage/legend-engine-xt-analytics-test-coverage-http-api/src/main/java/org/finos/legend/engine/api/analytics/mft/MFTReportBuilder.java
@@ -97,8 +97,8 @@ public class MFTReportBuilder
                                }
                                else
                                {
-                                   mftTestReport.assertionType = "UnTested";
-                                   mftTestReport.status = "UnTested";
+                                   mftTestReport.assertionType = "Untested";
+                                   mftTestReport.status = "Untested";
 
                                }
 
