953e7f61f9bf7c5e6e8e163771e3eea232300fde
Aziem Chawdhary <aziem.chawdhary@gs.com>
2025-04-15 15:28:53 +0100

    IDE: Build repositories based on configuration

--- legend-engine-core/legend-engine-core-pure/legend-engine-pure-ide/legend-engine-pure-ide-light-http-server/src/main/java/org/finos/legend/engine/ide/PureIDEServer.java
+++ legend-engine-core/legend-engine-core-pure/legend-engine-pure-ide/legend-engine-pure-ide-light-http-server/src/main/java/org/finos/legend/engine/ide/PureIDEServer.java
@@ -21,12 +21,19 @@ import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 import io.federecio.dropwizard.swagger.SwaggerResource;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.TimeUnit;
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.factory.Maps;
 import org.eclipse.collections.api.factory.Sets;
 import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.map.MutableMap;
 import org.eclipse.collections.api.set.MutableSet;
+import org.eclipse.collections.impl.collector.Collectors2;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.finos.legend.engine.ide.api.*;
 import org.finos.legend.engine.ide.api.concept.Concept;
@@ -44,8 +51,10 @@ import org.finos.legend.engine.ide.session.PureSession;
 import org.finos.legend.pure.m3.serialization.filesystem.repository.CodeRepository;
 import org.finos.legend.pure.m3.serialization.filesystem.repository.CodeRepositoryProviderHelper;
 import org.finos.legend.pure.m3.serialization.filesystem.repository.CodeRepositorySet;
+import org.finos.legend.pure.m3.serialization.filesystem.repository.GenericCodeRepository;
 import org.finos.legend.pure.m3.serialization.filesystem.usercodestorage.RepositoryCodeStorage;
 import org.finos.legend.pure.m3.serialization.filesystem.usercodestorage.classpath.ClassLoaderCodeStorage;
+import org.finos.legend.pure.m3.serialization.filesystem.usercodestorage.fs.MutableFSCodeStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -152,17 +161,29 @@ public abstract class PureIDEServer extends Application<ServerConfiguration>
         corsFilter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "*");
     }
 
-    private MutableList<RepositoryCodeStorage> getRepositories(org.finos.legend.engine.ide.SourceLocationConfiguration sourceLocationConfiguration, List<String> requiredRepositories)
+    private MutableList<RepositoryCodeStorage> getRepositories(ServerConfiguration configuration)
     {
         Map<CodeRepository, RepositoryCodeStorage> repoToCodeStorageMap = Maps.mutable.empty();
         repoToCodeStorageMap.putAll(CodeRepositoryProviderHelper.findCodeRepositories().toMap(r -> r, ClassLoaderCodeStorage::new));
-        repoToCodeStorageMap.putAll(this.buildRepositories(sourceLocationConfiguration).toMap(cs -> cs.getAllRepositories().getOnly(), cs -> cs));
+        repoToCodeStorageMap.putAll(this.buildRepositories(configuration.sourceLocationConfiguration).toMap(cs -> cs.getAllRepositories().getOnly(), cs -> cs));
 
         CodeRepositorySet codeRepositorySet = CodeRepositorySet.newBuilder().withCodeRepositories(repoToCodeStorageMap.keySet()).build();
 
-        if (requiredRepositories != null)
+        if (configuration.excludeRepositories != null && !configuration.excludeRepositories.isEmpty())
         {
-            MutableSet<String> requiredSet = Sets.mutable.withAll(requiredRepositories);
+            MutableList<Pattern> repositoryExclusionMatchers = configuration.excludeRepositories.stream().map(exclusion -> Pattern.compile(exclusion)).collect(Collectors2.toList());
+            repoToCodeStorageMap.forEach((k,v) ->
+            {
+                if (repositoryExclusionMatchers.anySatisfy(m -> m.matcher(k.getName()).matches()))
+                {
+                    repoToCodeStorageMap.remove(k);
+                }
+            });
+        }
+
+        if (configuration.requiredRepositories != null && !configuration.requiredRepositories.isEmpty())
+        {
+            MutableSet<String> requiredSet = Sets.mutable.withAll(configuration.requiredRepositories);
             if (codeRepositorySet.hasRepository("pure_ide"))
             {
                 requiredSet.add("pure_ide");
@@ -178,5 +199,66 @@ public abstract class PureIDEServer extends Application<ServerConfiguration>
         return pureSession;
     }
 
-    protected abstract MutableList<RepositoryCodeStorage> buildRepositories(SourceLocationConfiguration sourceLocationConfiguration);
+    protected MutableList<RepositoryCodeStorage> buildRepositories(SourceLocationConfiguration sourceLocationConfiguration)
+    {
+            MutableMap<CodeRepository, Path> codeRepositoryPathMap = Maps.mutable.empty();
+            List<String> directoriesToSearch = sourceLocationConfiguration.directories != null && !sourceLocationConfiguration.directories.isEmpty()
+                    ? sourceLocationConfiguration.directories
+                    : Lists.mutable.of(".");
+
+            for (String path : directoriesToSearch)
+            {
+                Map<CodeRepository, Path> p = findCodeRepositoriesAndMapToPath(path, sourceLocationConfiguration.pathPatternsToExclude);
+                codeRepositoryPathMap.putAll(p);
+            }
+
+            MutableList<RepositoryCodeStorage> result = Lists.mutable.empty();
+            codeRepositoryPathMap.forEachKeyValue((r, repoDefFilePath) ->
+            {
+                if (r instanceof GenericCodeRepository) //TODO is this correct/needed?
+                {
+                    String dirName = r.getName();
+                    Path parentDirectory = repoDefFilePath.getParent();
+                    result.add(new MutableFSCodeStorage(r, parentDirectory.resolve(dirName)));
+                }
+            });
+
+            return result;
+    }
+
+        //TODO: This should probably be moved to CodeRepositoryProviderHelper in legend-pure
+        protected Map<CodeRepository, Path> findCodeRepositoriesAndMapToPath(String path, List<String> patternsToExclude)
+        {
+            Map<CodeRepository, Path> result = Maps.mutable.empty();
+            FileSystem f = FileSystems.getDefault();
+            MutableList<PathMatcher> excludeMatchers = patternsToExclude.stream().map(p -> f.getPathMatcher("glob:" + p)).collect(Collectors2.toList());
+
+            try
+            {
+                Files.walkFileTree(Paths.get(path), new SimpleFileVisitor<Path>()
+                {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir, java.nio.file.attribute.BasicFileAttributes attrs)
+                    {
+                        return excludeMatchers.anySatisfy(m -> m.matches(dir)) ? FileVisitResult.SKIP_SUBTREE : FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                    {
+                        if (file.toString().endsWith(".definition.json"))
+                        {
+                            result.put(GenericCodeRepository.build(file), file);
+                        }
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            }
+            catch (IOException e)
+            {
+                throw new UncheckedIOException(e);
+            }
+            return result;
+        }
+    }
 }
