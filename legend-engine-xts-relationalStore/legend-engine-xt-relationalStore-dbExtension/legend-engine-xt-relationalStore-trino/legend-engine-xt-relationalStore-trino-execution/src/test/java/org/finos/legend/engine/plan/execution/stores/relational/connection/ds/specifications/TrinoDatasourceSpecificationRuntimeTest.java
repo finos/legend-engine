c4fcff9e1faf5c3452764fc8d7bcd83c7d028ed3
Mohammed Ibrahim <mibrahim1771@gmail.com>
2025-04-23 17:43:59 -0400

    Trino G2 support

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-trino/legend-engine-xt-relationalStore-trino-execution/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/connection/ds/specifications/TrinoDatasourceSpecificationRuntimeTest.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-trino/legend-engine-xt-relationalStore-trino-execution/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/connection/ds/specifications/TrinoDatasourceSpecificationRuntimeTest.java
@@ -1,3 +1,17 @@
+//  Copyright 2022 Goldman Sachs
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package org.finos.legend.engine.plan.execution.stores.relational.connection.ds.specifications;
 
 import org.finos.legend.engine.authentication.vaults.InMemoryVaultForTesting;
@@ -37,22 +51,6 @@ public class TrinoDatasourceSpecificationRuntimeTest extends TrinoDatasourceSpec
     }
 
     @Test
-    public void testTrinoDatasourceSpecificationProperties_WithDefaultTrustStore()
-    {
-        TrinoSSLSpecification trinoSSLSpecification = buildSSLSpecWith(true, null, null);
-        TrinoDatasourceSpecificationRuntime ds = buildDatasourceSpecificationRuntime(trinoSSLSpecification);
-
-        Properties properties = ds.getExtraDatasourceProperties();
-        assertEquals("catalog", properties.getProperty(CATALOG));
-        assertEquals("schema", properties.getProperty(SCHEMA));
-        assertEquals("cg:test", properties.getProperty(CLIENT_TAGS));
-        assertEquals("true", properties.getProperty(SSL));
-        assertEquals("test_user", properties.getProperty(USER));
-        assertEquals(DEFAULT_TRUST_STORE_PATH, properties.getProperty(SSL_TRUST_STORE_PATH));
-        assertNull(properties.getProperty(SSL_TRUST_STORE_PASSWORD));
-    }
-
-    @Test
     public void testTrinoDatasourceSpecificationProperties_WithValidCustomTrustStore()
     {
         TrinoSSLSpecification trinoSSLSpecification = buildSSLSpecWith(true, "testPathRef", "testPwdRef");
@@ -69,6 +67,22 @@ public class TrinoDatasourceSpecificationRuntimeTest extends TrinoDatasourceSpec
     }
 
     @Test
+    public void testTrinoDatasourceSpecificationProperties_WithEmptyTrustStoreAndPwd_AddNothing()
+    {
+        TrinoSSLSpecification trinoSSLSpecification = buildSSLSpecWith(true, null, null);
+        TrinoDatasourceSpecificationRuntime ds = buildDatasourceSpecificationRuntime(trinoSSLSpecification);
+
+        Properties properties = ds.getExtraDatasourceProperties();
+        assertEquals("catalog", properties.getProperty(CATALOG));
+        assertEquals("schema", properties.getProperty(SCHEMA));
+        assertEquals("cg:test", properties.getProperty(CLIENT_TAGS));
+        assertEquals("true", properties.getProperty(SSL));
+        assertEquals("test_user", properties.getProperty(USER));
+        assertNull(properties.getProperty(SSL_TRUST_STORE_PATH));
+        assertNull(properties.getProperty(SSL_TRUST_STORE_PASSWORD));
+    }
+
+    @Test
     public void testTrinoDatasourceSpecificationProperties_WithInvalidTrustStorePwdRef_ThrowException()
     {
 
@@ -82,6 +96,7 @@ public class TrinoDatasourceSpecificationRuntimeTest extends TrinoDatasourceSpec
             assertEquals("No valid SSL trust store password value found for vault reference", re.getMessage());
         }
     }
+
     @Test
     public void testTrinoDatasourceSpecificationProperties_WithInvalidTrustStoreRef_ThrowException()
     {
@@ -94,6 +109,33 @@ public class TrinoDatasourceSpecificationRuntimeTest extends TrinoDatasourceSpec
         {
             assertEquals("No valid SSL trust store value found for vault reference", re.getMessage());
         }
+    }
+
+    @Test
+    public void test_getJdbcUrl_givenCatalog_noSchema()
+    {
+        TrinoSSLSpecification trinoSSLSpecification = buildSSLSpecWith(true, null, null);
+        TrinoDatasourceSpecificationRuntime ds = new TrinoDatasourceSpecificationRuntime(
+                new TrinoDatasourceSpecificationKey("host", 8000, "catalog", null, "cg:test", trinoSSLSpecification),
+                new TrinoManager(),
+                new TrinoDelegatedKerberosAuthenticationStrategyRuntime("test", "HTTP", false),
+                new Properties()
+        );
+
+        Properties properties = ds.getExtraDatasourceProperties();
+        String jdbcUrl = ds.getJdbcUrl("unused_host", 9090, null, properties);
+        assertEquals("jdbc:trino://host:8000/catalog", jdbcUrl);
+    }
+
+    @Test
+    public void test_getJdbcUrl_givenCatalogAndSchema()
+    {
+        TrinoSSLSpecification trinoSSLSpecification = buildSSLSpecWith(true, null, null);
+        TrinoDatasourceSpecificationRuntime ds = buildDatasourceSpecificationRuntime(trinoSSLSpecification);
+
+        Properties properties = ds.getExtraDatasourceProperties();
+        String jdbcUrl = ds.getJdbcUrl("unused_host", 9090, null, properties);
+        assertEquals("jdbc:trino://host:8000/catalog/schema", jdbcUrl);
     }
 
     private static TrinoSSLSpecification buildSSLSpecWith(boolean ssl, String trustStorePathVaultRef, String trustStorePwdVaultRef)
