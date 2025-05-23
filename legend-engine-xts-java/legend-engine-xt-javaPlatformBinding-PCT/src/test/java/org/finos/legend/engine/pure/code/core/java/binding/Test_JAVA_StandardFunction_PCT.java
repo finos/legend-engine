d78c52c16b130efc3f7a5365936507c528df2438
tamimi <mimi.x.tam@ny.email.gs.com>
2025-05-23 18:43:29 -0400

    adding bitwise functions and, or, xor, not, shiftleft, shiftright

--- legend-engine-xts-java/legend-engine-xt-javaPlatformBinding-PCT/src/test/java/org/finos/legend/engine/pure/code/core/java/binding/Test_JAVA_StandardFunction_PCT.java
+++ legend-engine-xts-java/legend-engine-xt-javaPlatformBinding-PCT/src/test/java/org/finos/legend/engine/pure/code/core/java/binding/Test_JAVA_StandardFunction_PCT.java
@@ -143,8 +143,18 @@ public class Test_JAVA_StandardFunction_PCT extends PCTReportConfiguration
 
             // Least
             one("meta::pure::functions::collection::tests::least::testLeast_Empty_Function_1__Boolean_1_", "\"Cast exception: JSONNull cannot be cast to JSONObject\""),
-            one("meta::pure::functions::collection::tests::least::testLeast_Number_Function_1__Boolean_1_", "\"\nexpected: 1.0D\nactual:   1.0\"")
-        );
+            one("meta::pure::functions::collection::tests::least::testLeast_Number_Function_1__Boolean_1_", "\"\nexpected: 1.0D\nactual:   1.0\""),
+
+            // Bitwise
+            pack("meta::pure::functions::math::tests::tbd::bitAnd", "Error in 'test::testFunction': Error in function 'test::testFunction__Integer_1_' - Type error: 'Number' is not a subtype of 'Integer'"),
+            pack("meta::pure::functions::math::tests::tbd::bitNot", "Error in 'test::testFunction': Error in function 'test::testFunction__Integer_1_' - Type error: 'Number' is not a subtype of 'Integer'"),
+            pack("meta::pure::functions::math::tests::tbd::bitOr", "Error in 'test::testFunction': Error in function 'test::testFunction__Integer_1_' - Type error: 'Number' is not a subtype of 'Integer'"),
+            pack("meta::pure::functions::math::tests::tbd::bitXor", "Error in 'test::testFunction': Error in function 'test::testFunction__Integer_1_' - Type error: 'Float' is not a subtype of 'Integer'"),
+            pack("meta::pure::functions::math::tests::tbd::bitShiftLeft", "Error in 'test::testFunction': Error in function 'test::testFunction__Integer_1_' - Type error: 'Number' is not a subtype of 'Integer'"),
+            one("meta::pure::functions::math::tests::tbd::bitShiftRight::testBitShiftRight_UpTo62Bits_Function_1__Boolean_1_", "Error in 'test::testFunction': Error in function 'test::testFunction__Integer_1_' - Type error: 'Number' is not a subtype of 'Integer'"),
+            one("meta::pure::functions::math::tests::tbd::bitShiftLeft::testBitShiftLeft_MoreThan62Bits_Function_1__Boolean_1_", "\"Execution error message mismatch.\nThe actual message was \"Unexpected error executing function with params [Anonymous_Lambda]\"\nwhere the expected message was:\"Unsupported number of bits to shift - max bits allowed is 62\"\""),
+            one("meta::pure::functions::math::tests::tbd::bitShiftRight::testBitShiftRight_MoreThan62Bits_Function_1__Boolean_1_", "\"Execution error message mismatch.\nThe actual message was \"Unexpected error executing function with params [Anonymous_Lambda]\"\nwhere the expected message was:\"Unsupported number of bits to shift - max bits allowed is 62\"\"")
+    );
 
     public static Test suite()
     {
