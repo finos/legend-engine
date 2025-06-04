9bc72588dd35cda46cb1138adc2ab531112fe061
ADV-718 <Amandeep.Verma@ny.email.gs.com>
2025-06-04 17:25:12 -0400

    Removed unnecessay function from generator file

--- legend-engine-xts-memsqlFunction/legend-engine-xt-memsqlFunction-generator/src/main/java/org/finos/legend/engine/language/memsqlFunction/generator/MemSqlFunctionGenerator.java
+++ legend-engine-xts-memsqlFunction/legend-engine-xt-memsqlFunction-generator/src/main/java/org/finos/legend/engine/language/memsqlFunction/generator/MemSqlFunctionGenerator.java
@@ -17,12 +17,8 @@ package org.finos.legend.engine.language.memsqlFunction.generator;
 
 import org.eclipse.collections.api.RichIterable;
 import org.eclipse.collections.api.block.function.Function;
-import org.eclipse.collections.api.tuple.Pair;
 import org.eclipse.collections.impl.factory.Lists;
-import org.eclipse.collections.impl.tuple.Tuples;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
-import org.finos.legend.engine.plan.generation.PlanGenerator;
-import org.finos.legend.engine.plan.platform.PlanPlatform;
 import org.finos.legend.engine.protocol.memsqlFunction.deployment.MemSqlFunctionArtifact;
 import org.finos.legend.engine.protocol.memsqlFunction.deployment.MemSqlFunctionDeploymentConfiguration;
 import org.finos.legend.engine.protocol.memsqlFunction.metamodel.MemSqlFunction;
@@ -32,16 +28,12 @@ import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.connect
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.store.relational.connection.RelationalDatabaseConnection;
 
 import org.finos.legend.pure.generated.*;
-import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.FunctionDefinition;
-import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.PackageableFunction;
 
 public class MemSqlFunctionGenerator
 {
 
     public static MemSqlFunctionArtifact generateArtifact(PureModel pureModel, Root_meta_external_function_activator_memSqlFunction_MemSqlFunction activator, PureModelContext inputModel, Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensions)
     {
-        Pair<Root_meta_pure_alloy_connections_alloy_specification_StaticDatasourceSpecification_Impl, RichIterable<String>> artifactDetails = extractArtifactDetails(pureModel, activator, routerExtensions);
-
         String sqlExpressions = core_memsqlfunction_generation_generation.Root_meta_external_function_activator_memSqlFunction_generation_generateArtifact_MemSqlFunction_1__Extension_MANY__String_1_(activator, routerExtensions.apply(pureModel), pureModel.getExecutionSupport());
 
         RelationalDatabaseConnection connection;
@@ -62,27 +54,5 @@ public class MemSqlFunctionGenerator
     public static String generateLineage(PureModel pureModel, Root_meta_external_function_activator_memSqlFunction_MemSqlFunction activator, PureModelContext inputModel, Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensions)
     {
         return core_memsqlfunction_generation_generation.Root_meta_external_function_activator_memSqlFunction_generation_computeLineage_MemSqlFunction_1__Extension_MANY__String_1_(activator, routerExtensions.apply(pureModel), pureModel.getExecutionSupport());
-    }
-
-    private static Pair<Root_meta_pure_alloy_connections_alloy_specification_StaticDatasourceSpecification_Impl, RichIterable<String>> extractArtifactDetails(PureModel pureModel, Root_meta_external_function_activator_memSqlFunction_MemSqlFunction activator, Function<PureModel, RichIterable<? extends Root_meta_pure_extension_Extension>> routerExtensions)
-    {
-        PackageableFunction<?> function = activator._function();
-        Root_meta_pure_executionPlan_ExecutionPlan executionPlan = PlanGenerator.generateExecutionPlanAsPure((FunctionDefinition<?>) function, null, null, null, pureModel, PlanPlatform.JAVA, null, routerExtensions.apply(pureModel));
-        RichIterable<Root_meta_relational_mapping_SQLExecutionNode> sqlExecutionNodes =
-                collectAllNodes(executionPlan._rootExecutionNode()).selectInstancesOf(Root_meta_relational_mapping_SQLExecutionNode.class);
-
-        Root_meta_external_store_relational_runtime_RelationalDatabaseConnection relationalDatabaseConnection = (Root_meta_external_store_relational_runtime_RelationalDatabaseConnection) sqlExecutionNodes.getAny()._connection();
-        Root_meta_pure_alloy_connections_alloy_specification_StaticDatasourceSpecification_Impl memSqlDatasourceSpecification = ((Root_meta_pure_alloy_connections_alloy_specification_StaticDatasourceSpecification_Impl) relationalDatabaseConnection._datasourceSpecification());
-
-        return Tuples.pair(
-                memSqlDatasourceSpecification,
-                sqlExecutionNodes
-                        .collect(Root_meta_relational_mapping_SQLExecutionNode::_sqlQuery)
-                        .select(x -> !x.toLowerCase().startsWith("alter")));
-    }
-
-    private static RichIterable<Root_meta_pure_executionPlan_ExecutionNode> collectAllNodes(Root_meta_pure_executionPlan_ExecutionNode node)
-    {
-        return Lists.mutable.with(node).withAll(node._executionNodes().flatCollect(MemSqlFunctionGenerator::collectAllNodes));
     }
 }
