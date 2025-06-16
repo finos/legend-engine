fa97afe10357517ea0b7104ba1be7869576743c6
Rafael Bey <rafael.bey@gs.com>
2025-06-16 17:37:46 -0400

    Simple form for PCT report

--- legend-engine-config/legend-engine-server/legend-engine-server-http-server/src/main/java/org/finos/legend/engine/server/core/pct/PCT.java
+++ legend-engine-config/legend-engine-server/legend-engine-server-http-server/src/main/java/org/finos/legend/engine/server/core/pct/PCT.java
@@ -14,17 +14,19 @@
 
 package org.finos.legend.engine.server.core.pct;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.net.URI;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import org.finos.legend.pure.m3.pct.aggregate.generation.DocumentationGeneration;
 import org.finos.legend.pure.m3.pct.aggregate.model.Documentation;
 
@@ -33,10 +35,140 @@ import org.finos.legend.pure.m3.pct.aggregate.model.Documentation;
 public class PCT
 {
     @GET
+    @Path("form")
+    @ApiOperation(value = "PCT report form")
+    @Produces(MediaType.TEXT_HTML)
+    public Response formPCT(@Context UriInfo urlInfo)
+    {
+        String path = URI.create(urlInfo.getAbsolutePath().toString().replace("pct/form", "pct/html")).getPath();
+
+        Documentation doc = DocumentationGeneration.buildDocumentation();
+        String adapters = doc.adapters.stream().map(x -> x.adapter.name)
+                .distinct()
+                .sorted()
+                .map(x -> String.format("      <option value='%s'>%s</option>\n", x, x))
+                .collect(Collectors.joining());
+
+        String qualifiers = doc.functionsDocumentation.stream()
+                .flatMap(x -> x.functionTestResults.values().stream())
+                .flatMap(x -> x.tests.stream())
+                .flatMap(x -> x.qualifiers.stream())
+                .distinct()
+                .sorted()
+                .map(x -> String.format("      <option value='%s'>%s</option>\n", x, x))
+                .collect(Collectors.joining());
+
+        String form = "<!DOCTYPE html>\n" +
+                "<html lang='en'>\n" +
+                "<head>\n" +
+                "  <meta charset='UTF-8' />\n" +
+                "  <title>PCT Report Form</title>\n" +
+                "  <style>\n" +
+                "    body {\n" +
+                "      font-family: sans-serif;\n" +
+                "      padding: 2em;\n" +
+                "      background: #f9f9f9;\n" +
+                "    }\n" +
+                "    #formContainer {\n" +
+                "      position: sticky;\n" +
+                "      top: 0;\n" +
+                "      background: #fff;\n" +
+                "      padding: 1em 2em;\n" +
+                "      box-shadow: 0 2px 4px rgba(0,0,0,0.1);\n" +
+                "      z-index: 100;\n" +
+                "    }\n" +
+                "    form {\n" +
+                "      display: flex;\n" +
+                "      flex-direction: row;\n" +
+                "      gap: 2em;\n" +
+                "      align-items: flex-start;\n" +
+                "    }\n" +
+                "    label {\n" +
+                "      display: block;\n" +
+                "      margin-top: 1em;\n" +
+                "      font-weight: bold;\n" +
+                "    }\n" +
+                "    select {\n" +
+                "      width: 200px;\n" +
+                "      height: 120px;\n" +
+                "    }\n" +
+                "    #iframeContainer {\n" +
+                "      position: fixed;\n" +
+//                "      top: calc(1em + 2em + 120px); /* padding + label + select height */\n" +
+                "      left: 0;\n" +
+                "      right: 0;\n" +
+//                "      bottom: 0;\n" +
+                "      width: 100%;\n" +
+                "      height: 80%;\n" +
+                "    }\n" +
+                "    #resultFrame {\n" +
+                "      width: 100%;\n" +
+                "      height: 100%;\n" +
+                "      border: none;\n" +
+                "      display: block;\n" +
+                "    }\n" +
+                "  </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                " <div id='formContainer'>\n" +
+                "  <form id='adapterForm'>\n" +
+                "    <label for='adapter'>Adapter:</label>\n" +
+                "    <select id='adapter' name='adapter' multiple>\n" +
+                adapters +
+                "    </select>\n" +
+                "\n" +
+                "    <label for='qualifier'>Qualifier:</label>\n" +
+                "    <select id='qualifier' name='qualifier' multiple>\n" +
+                qualifiers +
+                "    </select>\n" +
+                "  </form>\n" +
+                " </div>\n" +
+                "\n" +
+                "  <div id='iframeContainer'>\n" +
+                "    <iframe id='resultFrame' title='Query Results'></iframe>\n" +
+                "  </div>\n" +
+                "\n" +
+                "  <script>\n" +
+                "    const adapterSelect = document.getElementById('adapter');\n" +
+                "    const qualifierSelect = document.getElementById('qualifier');\n" +
+                "    const resultFrame = document.getElementById('resultFrame');\n" +
+                "\n" +
+                "    function getSelectedValues(selectElement) {\n" +
+                "      return Array.from(selectElement.selectedOptions).map(opt => opt.value);\n" +
+                "    }\n" +
+                "\n" +
+                "    async function fetchAndUpdate() {\n" +
+                "      const adapters = getSelectedValues(adapterSelect);\n" +
+                "      const qualifiers = getSelectedValues(qualifierSelect);\n" +
+                "\n" +
+                "      const params = new URLSearchParams();\n" +
+                "      adapters.forEach(g => params.append('adapter', g));\n" +
+                "      qualifiers.forEach(q => params.append('qualifier', q));\n" +
+                "\n" +
+                "      const query = params.toString();\n" +
+                String.format("      const url = query ? `%s?${query}` : `%s`;\n", path, path) +
+                "\n" +
+                "      resultFrame.src = url;\n" +
+                "    }\n" +
+                "\n" +
+                "    // Auto-refresh on change\n" +
+                "    adapterSelect.addEventListener('change', fetchAndUpdate);\n" +
+                "    qualifierSelect.addEventListener('change', fetchAndUpdate);\n" +
+                "\n" +
+                "    // Initial fetch on page load\n" +
+                "    fetchAndUpdate();\n" +
+                "  </script>\n" +
+                "</body>\n" +
+                "</html>\n";
+
+        return Response.status(200).type(MediaType.TEXT_HTML).entity(form).build();
+    }
+
+    @GET
     @Path("html")
     @ApiOperation(value = "PCT report in HTML")
     @Produces(MediaType.TEXT_HTML)
-    public Response htmlPCT(@QueryParam("adapter") Set<String> adapterKeys, @QueryParam("qualifiers") Set<String> adapterQualifiers)    // adapterQualifiers - if empty will default to greener report, all will give all adapters, can have additional qualifiers for specific adapters to be included
+    public Response htmlPCT(@QueryParam("adapter") Set<String> adapterKeys, @QueryParam("qualifier") Set<String> adapterQualifiers)    // adapterQualifiers - if empty will default to greener report, all will give all adapters, can have additional qualifiers for specific adapters to be included
     {
         return Response.status(200).type(MediaType.TEXT_HTML).entity(PCT_to_SimpleHTML.buildHTML(adapterKeys, adapterQualifiers)).build();
     }
@@ -47,14 +179,7 @@ public class PCT
     @Produces(MediaType.APPLICATION_JSON)
     public Response jsonPCT()
     {
-        try
-        {
-            Documentation doc = DocumentationGeneration.buildDocumentation();
-            return Response.status(200).type(MediaType.APPLICATION_JSON).entity(new ObjectMapper().writeValueAsString(doc)).build();
-        }
-        catch (JsonProcessingException e)
-        {
-            throw new RuntimeException(e);
-        }
+        Documentation doc = DocumentationGeneration.buildDocumentation();
+        return Response.status(200).type(MediaType.APPLICATION_JSON).entity(doc).build();
     }
 }
