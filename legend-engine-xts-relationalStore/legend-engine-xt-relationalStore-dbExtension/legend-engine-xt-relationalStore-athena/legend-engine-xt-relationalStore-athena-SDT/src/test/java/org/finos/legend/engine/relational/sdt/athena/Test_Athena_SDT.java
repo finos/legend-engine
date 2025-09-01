d7a94bb5ee6b3e142599b12c898422372a523787
Paarth002 <parth.bhore@ny.email.gs.com>
2025-09-01 21:57:43 +0000

    Athena jdbc jar direct installation to .m2

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-athena/legend-engine-xt-relationalStore-athena-SDT/src/test/java/org/finos/legend/engine/relational/sdt/athena/Test_Athena_SDT.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-athena/legend-engine-xt-relationalStore-athena-SDT/src/test/java/org/finos/legend/engine/relational/sdt/athena/Test_Athena_SDT.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package athena;
+package org.finos.legend.engine.relational.sdt.athena;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
