2fafdc0151a060f33be48cdb92fd3c96f74e4031
gs-ssh16 <SaiSriharsha.Annepu@ny.email.gs.com>
2025-09-15 22:09:27 +0530

    Python execution native function

---
+++ legend-engine-xts-python/legend-engine-xt-python-functions-execution/legend-engine-xt-python-functions-execution-pure/src/test/java/org/finos/legend/engine/pure/code/core/python/execution/ExecutePythonScriptTest.java
@@ -0,0 +1,61 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.pure.code.core.python.execution;
+
+import org.finos.legend.pure.m3.tests.AbstractPureTestWithCoreCompiled;
+import org.junit.Test;
+
+public abstract class ExecutePythonScriptTest extends AbstractPureTestWithCoreCompiled
+{
+    @Test
+    public void testSuccessfulExecution()
+    {
+        test(
+                "let x = meta::python::execution::executePythonScript('print(\"Hello, World!\")');\n" +
+                        "assertEquals(0, $x.exitCode);\n" +
+                        "assertEquals('Hello, World!\\n', $x.output);\n" +
+                        "assertEquals('', $x.error);\n"
+        );
+    }
+
+    @Test
+    public void testFailedExecution()
+    {
+        test(
+                "let x = meta::python::execution::executePythonScript('import unknown_lib');\n" +
+                        "assertEquals(1, $x.exitCode);\n" +
+                        "assertEquals('', $x.output);\n" +
+                        "assert($x.error->indexOf('ModuleNotFoundError: No module named \\'unknown_lib\\'') >= 1);\n"
+        );
+    }
+
+    private void test(String code)
+    {
+        try
+        {
+            compileTestSource(
+                    "testSource.pure",
+                    "function test():Any[*] \n" +
+                            "{" + code + "}\n"
+            );
+            this.execute("test():Any[*]");
+        }
+        finally
+        {
+            runtime.delete("testSource.pure");
+            runtime.compile();
+        }
+    }
+}
