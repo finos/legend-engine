8be5d8f38bfa4c3809289ccb7f608a5ac581794c
Parth Bhore <81606042+Paarth002@users.noreply.github.com>
2025-09-17 12:20:05 +0530

    Fix athena PCTs + maxPoolSize changes (#4096)

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-athena/legend-engine-xt-relationalStore-athena-PCT/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/athena/pct/Test_Relational_Athena_EssentialFunctions_PCT.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-athena/legend-engine-xt-relationalStore-athena-PCT/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/athena/pct/Test_Relational_Athena_EssentialFunctions_PCT.java
@@ -320,8 +320,7 @@ public class Test_Relational_Athena_EssentialFunctions_PCT extends PCTReportConf
             one("meta::pure::functions::string::tests::split::testSplit_Function_1__Boolean_1_", "\"No SQL translation exists for the PURE function 'split_String_1__String_1__String_MANY_'. \nIf you would like to add a SQL translation for the function then follow the step-by-step guide on the PURE wiki.\"", AdapterQualifier.unsupportedFeature),
 
             //Substring
-            one("meta::pure::functions::string::tests::substring::testStartEnd_Function_1__Boolean_1_", "\"\nexpected: 'he quick brown fox jumps over the lazy do'\nactual:   'the quick brown fox jumps over the lazy do'\"", AdapterQualifier.needsInvestigation),
-            one("meta::pure::functions::string::tests::substring::testStart_Function_1__Boolean_1_", "\"\nexpected: 'he quick brown fox jumps over the lazy dog'\nactual:   'the quick brown fox jumps over the lazy dog'\"", AdapterQualifier.needsInvestigation),
+            one("meta::pure::functions::string::tests::substring::testStartEnd_Function_1__Boolean_1_", "\"\nexpected: 'he quick brown fox jumps over the lazy do'\nactual:   'he quick brown fox jumps over the lazy dog'\"", AdapterQualifier.needsInvestigation),
 
             //To String
             one("meta::pure::functions::string::tests::toString::testClassToString_Function_1__Boolean_1_", "\"Cannot cast a collection of size 0 to multiplicity [1]\"", AdapterQualifier.needsInvestigation),
