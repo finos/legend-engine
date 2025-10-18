3992d327a8074b920c94aaf264b5a4f0c125d9a5
Pierre De Belen <pierre.debelen@gmail.com>
2025-10-18 00:15:49 -0400

    Support multiline expression with SQL expression

--- h2Server.sh
+++ h2Server.sh
@@ -1,1 +1,1 @@
-java -cp ~/.m2/repository/com/h2database/h2/2.1.214/h2-2.1.214.jar org.h2.tools.Server -tcp -tcpPort 1975 -ifNotExists
+java -cp ~/.m2/repository/com/h2database/h2/2.1.214/h2-2.1.214.jar org.h2.tools.Server -tcp -tcpPort 1976 -ifNotExists
