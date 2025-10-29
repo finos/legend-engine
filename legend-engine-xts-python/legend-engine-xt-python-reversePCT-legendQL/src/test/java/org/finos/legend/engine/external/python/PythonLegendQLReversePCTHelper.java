6938b8811796b35ccc9b0081ae3473dda22d50aa
gs-ssh16 <SaiSriharsha.Annepu@ny.email.gs.com>
2025-10-29 11:20:59 +0530

    Python - LegendQL - Reverse PCT

---
+++ legend-engine-xts-python/legend-engine-xt-python-reversePCT-legendQL/src/test/java/org/finos/legend/engine/external/python/PythonLegendQLReversePCTHelper.java
@@ -0,0 +1,129 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+package org.finos.legend.engine.external.python;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.map.MutableMap;
+import org.finos.legend.pure.generated.Root_meta_pure_test_pct_reversePCT_framework_Reverse;
+import org.finos.legend.pure.generated.Root_meta_pure_test_pct_reversePCT_framework_ReversesForSource;
+import org.finos.legend.pure.generated.Root_meta_pure_test_pct_reversePCT_framework_ReversesForTest;
+import org.finos.legend.pure.generated.platform_pure_essential_meta_graph_elementToPath;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.PackageableElement;
+import org.finos.legend.pure.m3.execution.test.PureTestBuilder;
+import org.finos.legend.pure.m3.execution.test.TestCollection;
+import org.finos.legend.pure.m3.pct.reports.config.PCTReportConfiguration;
+import org.finos.legend.pure.m3.pct.reports.config.exclusion.AdapterQualifier;
+import org.finos.legend.pure.m3.pct.reports.config.exclusion.ExclusionOneTest;
+import org.finos.legend.pure.m3.pct.reports.config.exclusion.ExclusionSpecification;
+import org.finos.legend.pure.m3.pct.reports.model.Adapter;
+import org.finos.legend.pure.m3.pct.shared.model.ReportScope;
+import org.finos.legend.pure.m3.tests.AbstractPureTestWithCoreCompiled;
+import org.finos.legend.pure.runtime.java.compiled.execution.CompiledExecutionSupport;
+import org.finos.legend.pure.runtime.java.compiled.execution.FunctionExecutionCompiled;
+import org.finos.legend.pure.runtime.java.compiled.execution.FunctionExecutionCompiledBuilder;
+import org.finos.legend.pure.runtime.java.compiled.testHelper.PureTestBuilderCompiled;
+
+import java.util.Enumeration;
+import java.util.Objects;
+
+import static org.finos.legend.engine.test.shared.framework.PureTestHelperFramework.wrapSuite;
+
+public class PythonLegendQLReversePCTHelper
+{
+    public static Test buildSuite(ReportScope reportScope, Adapter adapter, MutableList<ExclusionSpecification> expectedFailures)
+    {
+        return wrapSuite(
+                () -> true,
+                () ->
+                {
+                    FunctionExecutionCompiled functionExecutionCompiled = new FunctionExecutionCompiledBuilder().build();
+                    AbstractPureTestWithCoreCompiled.setUpRuntime(functionExecutionCompiled);
+                    CompiledExecutionSupport executionSupport = functionExecutionCompiled.getExecutionSupport();
+                    MutableMap<String, String> explodedExpectedFailures = PCTReportConfiguration.explodeExpectedFailures(expectedFailures, executionSupport.getProcessorSupport());
+                    return PureTestBuilderCompiled.buildPCTTestSuite(TestCollection.buildPCTTestCollection(reportScope._package, reportScope.filePath, executionSupport.getProcessorSupport()), explodedExpectedFailures, adapter.function, executionSupport);
+                },
+                () -> false,
+                Lists.mutable.empty()
+        );
+    }
+
+    public static MutableList<ExclusionSpecification> build(RichIterable<? extends Root_meta_pure_test_pct_reversePCT_framework_ReversesForSource> reverses)
+    {
+        if (reverses == null)
+        {
+            return Lists.mutable.empty();
+        }
+        return reverses.toList().flatCollect(x ->
+                x._reverses().collect(z ->
+                        {
+                            Root_meta_pure_test_pct_reversePCT_framework_Reverse rev = z._reverses().select(r -> r._expectedError() != null || !r._shouldBeSupported()).getFirst();
+                            if (rev != null)
+                            {
+                                return (ExclusionSpecification) new ExclusionOneTest(
+                                        z._testFunction(),
+                                        !rev._shouldBeSupported() ? "\"Should not be supported\"" : rev._expectedError(),
+                                        rev._shouldBeSupported() ? AdapterQualifier.needsInvestigation : AdapterQualifier.unsupportedFeature
+                                );
+                            }
+                            return null;
+                        }
+                ).select(Objects::nonNull));
+    }
+
+    public static MutableList<ExclusionSpecification> getMissingExpectedFailures(TestSuite ts, RichIterable<? extends Root_meta_pure_test_pct_reversePCT_framework_ReversesForSource> reverseInfo)
+    {
+        MutableList<String> allTests = Lists.mutable.empty();
+        collectTests(ts.tests(), allTests);
+        RichIterable<String> managedTests = collectManagedTests(reverseInfo);
+        allTests.removeAll(managedTests.toList());
+        return allTests.collect(x -> new ExclusionOneTest(x, "", AdapterQualifier.needsImplementation));
+    }
+
+    public static RichIterable<String> collectManagedTests(RichIterable<? extends Root_meta_pure_test_pct_reversePCT_framework_ReversesForSource> reverses)
+    {
+        if (reverses == null)
+        {
+            return Lists.mutable.empty();
+        }
+        return reverses.flatCollect(x ->
+                x._reverses().collect(Root_meta_pure_test_pct_reversePCT_framework_ReversesForTest::_testFunction
+                ).select(Objects::nonNull));
+    }
+
+    public static void collectTests(Enumeration<Test> w, MutableList<String> tests)
+    {
+        while (w.hasMoreElements())
+        {
+            Test test = w.nextElement();
+            if (test instanceof TestSuite)
+            {
+                collectTests(((TestSuite) test).tests(), tests);
+            }
+            else if (test instanceof PureTestBuilder.PureTestCase)
+            {
+                tests.add(platform_pure_essential_meta_graph_elementToPath.Root_meta_pure_functions_meta_elementToPath_PackageableElement_1__String_1_((PackageableElement) ((PureTestBuilder.PureTestCase) test).getCoreInstance(), ((PureTestBuilder.PureTestCase) test).getExecutionSupport()));
+            }
+            else
+            {
+                throw new RuntimeException(test.getClass().getName());
+            }
+        }
+    }
+}
---
+++ legend-engine-xts-python/legend-engine-xt-python-reversePCT-legendQL/src/test/java/org/finos/legend/engine/external/python/Test_External_PythonLegendQL_VariantFunctions_Reverse_PCT.java
@@ -0,0 +1,72 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.external.python;
+
+import junit.framework.Test;
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.list.MutableList;
+import org.finos.legend.pure.generated.Root_meta_pure_test_pct_reversePCT_framework_ReversesForSource;
+import org.finos.legend.pure.generated.core_external_python_reverse_pct_legend_ql_pythonReversePCTLegendQLApi;
+import org.finos.legend.pure.m3.PlatformCodeRepositoryProvider;
+import org.finos.legend.pure.m3.pct.reports.config.PCTReportConfiguration;
+import org.finos.legend.pure.m3.pct.reports.config.exclusion.ExclusionSpecification;
+import org.finos.legend.pure.m3.pct.reports.model.Adapter;
+import org.finos.legend.pure.m3.pct.shared.model.ReportScope;
+import org.finos.legend.pure.runtime.java.compiled.testHelper.PureTestBuilderCompiled;
+
+import static org.finos.legend.pure.runtime.java.compiled.testHelper.PureTestBuilderCompiled.getClassLoaderExecutionSupport;
+
+public class Test_External_PythonLegendQL_VariantFunctions_Reverse_PCT extends PCTReportConfiguration
+{
+    private static final ReportScope reportScope = PlatformCodeRepositoryProvider.variantFunctions;
+    private static final Adapter adapter = Core_External_PythonLegendQL_ReversePCTReportProvider.LegendQLAdapter;
+    private static final String platform = "compiled";
+    private static final RichIterable<? extends Root_meta_pure_test_pct_reversePCT_framework_ReversesForSource> reverseInfo = core_external_python_reverse_pct_legend_ql_pythonReversePCTLegendQLApi.Root_meta_external_python_reversePCT_legendQL_pythonLegendQLReversesVariant__ReversesForSource_MANY_(getClassLoaderExecutionSupport(Thread.currentThread().getContextClassLoader()));
+    private static final MutableList<ExclusionSpecification> expectedFailures = PythonLegendQLReversePCTHelper.build(reverseInfo);
+
+    static
+    {
+        expectedFailures.addAll(PythonLegendQLReversePCTHelper.getMissingExpectedFailures(PureTestBuilderCompiled.buildPCTTestSuite(reportScope, expectedFailures, adapter), reverseInfo));
+    }
+
+    public static Test suite()
+    {
+        return PythonLegendQLReversePCTHelper.buildSuite(reportScope, adapter, expectedFailures);
+    }
+
+    @Override
+    public MutableList<ExclusionSpecification> expectedFailures()
+    {
+        return expectedFailures;
+    }
+
+    @Override
+    public ReportScope getReportScope()
+    {
+        return reportScope;
+    }
+
+    @Override
+    public Adapter getAdapter()
+    {
+        return adapter;
+    }
+
+    @Override
+    public String getPlatform()
+    {
+        return platform;
+    }
+}
