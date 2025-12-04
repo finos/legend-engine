e163cb6b5a925f773f508c0bd580ef2f84fe3ad1
Parth Bhore <81606042+Paarth002@users.noreply.github.com>
2025-12-04 14:05:50 +0530

    Update DataSourceSpecification.java

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-execution/legend-engine-xt-relationalStore-executionPlan-connection/src/main/java/org/finos/legend/engine/plan/execution/stores/relational/connection/ds/DataSourceSpecification.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-execution/legend-engine-xt-relationalStore-executionPlan-connection/src/main/java/org/finos/legend/engine/plan/execution/stores/relational/connection/ds/DataSourceSpecification.java
@@ -52,7 +52,7 @@ public abstract class DataSourceSpecification
     // HikariCP Parameters:
     // Information parameters and its defaults: https://github.com/brettwooldridge/HikariCP#gear-configuration-knobs-baby
     public static final String HIKARICP_HOUSEKEEPING_PERIOD_MS = "com.zaxxer.hikari.housekeeping.periodMs";
-    protected static final int HIKARICP_DEFAULT_MAX_POOL_SIZE = 100;
+    protected static final int HIKARICP_DEFAULT_MAX_POOL_SIZE = Integer.parseInt(System.getProperty("legend.relational.maxPoolSize", "100"));
     protected static final int HIKARICP_DEFAULT_MIN_IDLE = 0;
     // minIdle 0 means housekeeper will always try to keep the pool empty if there are not really active connection
     private static final long HIKARICP_DEFAULT_HOUSEKEEPER_FREQ_IN_MS = SECONDS.toMillis(30L);
