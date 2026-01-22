4cbb5cf1aec6ddfa5361a4b0b132e2ddbdccbd06
Rafael Bey-Hernandez <24432403+rafaelbey@users.noreply.github.com>
2026-01-22 16:54:52 -0500

    Refactor to auto-generate function compiler dispatch details (#4372)
    * Refactor to auto-generate function compiler dispatch details
    
    * Fix test cases
    
    * skip calendar functions as they are wrong

--- legend-engine-core/legend-engine-core-base/legend-engine-core-language-pure/legend-engine-language-pure-compiler/src/main/java/org/finos/legend/engine/language/pure/compiler/toPureGraph/CoreCompilerExtension.java
+++ legend-engine-core/legend-engine-core-base/legend-engine-core-language-pure/legend-engine-language-pure-compiler/src/main/java/org/finos/legend/engine/language/pure/compiler/toPureGraph/CoreCompilerExtension.java
@@ -14,14 +14,18 @@
 
 package org.finos.legend.engine.language.pure.compiler.toPureGraph;
 
+import org.eclipse.collections.api.block.function.Function;
 import org.eclipse.collections.api.block.function.Function2;
 import org.eclipse.collections.api.block.function.Function3;
+import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.impl.factory.Maps;
 import org.eclipse.collections.impl.utility.ListIterate;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.data.core.EmbeddedDataCompilerHelper;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.extension.CompilerExtension;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.extension.Processor;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.handlers.FunctionHandlerDispatchBuilderInfo;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.handlers.Handlers;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.handlers.IncludedMappingHandler;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.handlers.MappingIncludedMappingHandler;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.handlers.StoreProviderCompilerHelper;
@@ -40,6 +44,7 @@ import org.finos.legend.pure.generated.Root_meta_pure_data_EmbeddedData;
 import org.finos.legend.pure.generated.Root_meta_pure_test_assertion_TestAssertion;
 import org.finos.legend.pure.m3.coreinstance.meta.external.store.model.PureInstanceSetImplementation;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.mapping.SetImplementation;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.valuespecification.ValueSpecification;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.store.Store;
 
 import java.util.Collections;
@@ -47,7 +52,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class CoreCompilerExtension implements CompilerExtension, EmbeddedDataCompilerHelper, StoreProviderCompilerHelper
+public class CoreCompilerExtension extends FunctionDispatchExtension implements CompilerExtension, EmbeddedDataCompilerHelper, StoreProviderCompilerHelper
 {
     @Override
     public MutableList<String> group()
@@ -135,4 +140,44 @@ public class CoreCompilerExtension implements CompilerExtension, EmbeddedDataCom
         });
     }
 
+    @Override
+    public List<Function<Handlers, List<FunctionHandlerDispatchBuilderInfo>>> getExtraFunctionHandlerDispatchBuilderInfoCollectors()
+    {
+        return Lists.mutable.withAll(super.getExtraFunctionHandlerDispatchBuilderInfoCollectors())
+                .with((handlers) -> Lists.mutable.with(
+                        // TODO this function are been use incorrectly, hence we cannot apply the proper compiler constrains that the auto-generated code will enforce
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::annualized_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::cme_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::cw_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::cw_fm_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::CYMinus2_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::CYMinus3_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::mtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::p12wa_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::p12mtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::p12wtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::p4wa_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::p4wtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::p52wtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::p52wa_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pma_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pmtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pqtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::priorDay_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::priorYear_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pw_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pw_fm_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pwa_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pwtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pymtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pyqtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pytd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pywa_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::pywtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::qtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::reportEndDay_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::wtd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true),
+                        new FunctionHandlerDispatchBuilderInfo("meta::pure::functions::date::calendar::ytd_Date_$0_1$__String_1__Date_1__Number_$0_1$__Number_$0_1$_", (List<ValueSpecification> ps) -> true)
+                ));
+    }
 }
