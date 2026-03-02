f10b1fc3fa199c00cc002827e0f975069160d697
Mauricio Uyaguari <lmauricio12@gmail.com>
2026-03-02 17:39:01 -0500

    add data product query execution context (#4489)

--- legend-engine-application-query/src/test/java/org/finos/legend/engine/application/query/utils/TestQuerySerialization.java
+++ legend-engine-application-query/src/test/java/org/finos/legend/engine/application/query/utils/TestQuerySerialization.java
@@ -15,6 +15,8 @@
 package org.finos.legend.engine.application.query.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.finos.legend.engine.application.query.model.DataProductModelAccessExecutionContext;
+import org.finos.legend.engine.application.query.model.DataProductNativeExecutionContext;
 import org.finos.legend.engine.application.query.model.Query;
 import org.finos.legend.engine.application.query.model.QueryDataSpaceExecutionContext;
 import org.finos.legend.engine.application.query.model.QueryExplicitExecutionContext;
@@ -118,6 +120,52 @@ public class TestQuerySerialization
             "  \"versionId\": \"0.0.0\"\n" +
             "}";
 
+    String DATA_PRODUCT_NATIVE_QUERY = "{\n" +
+            "  \"artifactId\": \"test-artifact\",\n" +
+            "  \"content\": \"content\",\n" +
+            "  \"createdAt\": 1713280515492,\n" +
+            "  \"defaultParameterValues\": [],\n" +
+            "  \"description\": \"description\",\n" +
+            "  \"gridConfig\": null,\n" +
+            "  \"groupId\": \"test.group\",\n" +
+            "  \"id\": \"1\",\n" +
+            "  \"lastUpdatedAt\": 1713280515492,\n" +
+            "  \"name\": \"query1\",\n" +
+            "  \"originalVersionId\": \"0.0.0\",\n" +
+            "  \"owner\": \"testUser\",\n" +
+            "  \"executionContext\": \n" +
+            "    {\n" +
+            "      \"_type\": \"dataProductNativeExecutionContext\",\n" +
+            "      \"dataProductPath\": \"my::dataProduct\",\n" +
+            "      \"executionKey\": \"nativeKey\"\n" +
+            "    }\n" +
+            "  ,\n" +
+            "  \"versionId\": \"0.0.0\"\n" +
+            "}";
+
+    String DATA_PRODUCT_MODEL_ACCESS_QUERY = "{\n" +
+            "  \"artifactId\": \"test-artifact\",\n" +
+            "  \"content\": \"content\",\n" +
+            "  \"createdAt\": 1713280515492,\n" +
+            "  \"defaultParameterValues\": [],\n" +
+            "  \"description\": \"description\",\n" +
+            "  \"gridConfig\": null,\n" +
+            "  \"groupId\": \"test.group\",\n" +
+            "  \"id\": \"1\",\n" +
+            "  \"lastUpdatedAt\": 1713280515492,\n" +
+            "  \"name\": \"query1\",\n" +
+            "  \"originalVersionId\": \"0.0.0\",\n" +
+            "  \"owner\": \"testUser\",\n" +
+            "  \"executionContext\": \n" +
+            "    {\n" +
+            "      \"_type\": \"dataProductModelAccessExecutionContext\",\n" +
+            "      \"dataProductPath\": \"my::dataProduct\",\n" +
+            "      \"accessPointGroupId\": \"group1\"\n" +
+            "    }\n" +
+            "  ,\n" +
+            "  \"versionId\": \"0.0.0\"\n" +
+            "}";
+
     @Test
     public void testDeserialization() throws Exception
     {
@@ -136,5 +184,49 @@ public class TestQuerySerialization
         Assert.assertEquals(dataSpaceExecutionContext.executionKey, "myKey");
         Query dataSpaceQuery2 = objectMapper.readValue(DATA_SPACE_QUERY_NO_KEY, Query.class);
         Assert.assertNull(((QueryDataSpaceExecutionContext)dataSpaceQuery2.executionContext).executionKey);
+    }
+
+    @Test
+    public void testDataProductNativeDeserialization() throws Exception
+    {
+        Query query = objectMapper.readValue(DATA_PRODUCT_NATIVE_QUERY, Query.class);
+        Assert.assertTrue(query.executionContext instanceof DataProductNativeExecutionContext);
+        DataProductNativeExecutionContext ctx = (DataProductNativeExecutionContext) query.executionContext;
+        Assert.assertEquals("my::dataProduct", ctx.dataProductPath);
+        Assert.assertEquals("nativeKey", ctx.executionKey);
+    }
+
+    @Test
+    public void testDataProductModelAccessDeserialization() throws Exception
+    {
+        Query query = objectMapper.readValue(DATA_PRODUCT_MODEL_ACCESS_QUERY, Query.class);
+        Assert.assertTrue(query.executionContext instanceof DataProductModelAccessExecutionContext);
+        DataProductModelAccessExecutionContext ctx = (DataProductModelAccessExecutionContext) query.executionContext;
+        Assert.assertEquals("my::dataProduct", ctx.dataProductPath);
+        Assert.assertEquals("group1", ctx.accessPointGroupId);
+    }
+
+    @Test
+    public void testDataProductNativeSerialization() throws Exception
+    {
+        DataProductNativeExecutionContext ctx = new DataProductNativeExecutionContext();
+        ctx.dataProductPath = "my::dataProduct";
+        ctx.executionKey = "nativeKey";
+        String json = objectMapper.writeValueAsString(ctx);
+        Assert.assertTrue(json.contains("\"dataProductPath\":\"my::dataProduct\""));
+        Assert.assertTrue(json.contains("\"executionKey\":\"nativeKey\""));
+        Assert.assertTrue(json.contains("\"_type\":\"dataProductNativeExecutionContext\""));
+    }
+
+    @Test
+    public void testDataProductModelAccessSerialization() throws Exception
+    {
+        DataProductModelAccessExecutionContext ctx = new DataProductModelAccessExecutionContext();
+        ctx.dataProductPath = "my::dataProduct";
+        ctx.accessPointGroupId = "group1";
+        String json = objectMapper.writeValueAsString(ctx);
+        Assert.assertTrue(json.contains("\"dataProductPath\":\"my::dataProduct\""));
+        Assert.assertTrue(json.contains("\"accessPointGroupId\":\"group1\""));
+        Assert.assertTrue(json.contains("\"_type\":\"dataProductModelAccessExecutionContext\""));
     }
 }
