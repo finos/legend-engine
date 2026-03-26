465d04a7e7b893acd64cca4d4474de5d206ec47a
Nweje, Wendy C [Engineering] <Chinwendu.Nweje@ln.email.gs.com>
2026-03-25 20:26:53 -0400

    fix: added expected failures for PCT tests

--- legend-engine-xts-deephaven/legend-engine-xt-deephaven-PCT/src/test/java/org/finos/legend/engine/plan/execution/stores/test/deephaven/pct/Test_Deephaven_StandardFunctions_PCT.java
+++ legend-engine-xts-deephaven/legend-engine-xt-deephaven-PCT/src/test/java/org/finos/legend/engine/plan/execution/stores/test/deephaven/pct/Test_Deephaven_StandardFunctions_PCT.java
@@ -36,6 +36,8 @@ public class Test_Deephaven_StandardFunctions_PCT extends PCTReportConfiguration
     private static final String platform = "compiled";
     private static final MutableList<ExclusionSpecification> expectedFailures = Lists.mutable.with(
             one("meta::pure::functions::math::tests::zScore::testZScore_Function_1__Boolean_1_", "\"function not supported yet: meta::pure::functions::multiplicity::toOne_T_MANY__T_1_\""),
+            one("meta::pure::functions::math::tests::zScore::testZScore_FloatValues_Function_1__Boolean_1_", "\"function not supported yet: meta::pure::functions::multiplicity::toOne_T_MANY__T_1_\""),
+            one("meta::pure::functions::math::tests::zScore::testZScore_SingleValuePartition_Function_1__Boolean_1_", "\"function not supported yet: meta::pure::functions::multiplicity::toOne_T_MANY__T_1_\""),
 
             one("meta::pure::functions::boolean::tests::inequalities::between::testBetween_DateTime_Function_1__Boolean_1_", "\"function not supported yet: meta::pure::functions::boolean::and_Boolean_1__Boolean_1__Boolean_1_\""),
             one("meta::pure::functions::boolean::tests::inequalities::between::testBetween_NumberFloat_Function_1__Boolean_1_", "\"function not supported yet: meta::pure::functions::boolean::and_Boolean_1__Boolean_1__Boolean_1_\""),
