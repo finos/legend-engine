90dbfd664108a4d747a9505ee4dfc365c8e49527
sahil37 <sahilshah37@hotmail.com>
2026-04-23 13:27:14 +0100

    add format date function with specific list of allowed formats

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-spanner/legend-engine-xt-relationalStore-spanner-PCT/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/spanner/pct/Test_Relational_Spanner_StandardFunctions_PCT.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-dbExtension/legend-engine-xt-relationalStore-spanner/legend-engine-xt-relationalStore-spanner-PCT/src/test/java/org/finos/legend/engine/plan/execution/stores/relational/test/spanner/pct/Test_Relational_Spanner_StandardFunctions_PCT.java
@@ -206,6 +206,9 @@ public class Test_Relational_Spanner_StandardFunctions_PCT extends PCTReportConf
             one("meta::pure::functions::tests::date::testDayOfWeek_Function_1__Boolean_1_", "[ERROR] column \"%A\" does not exist - Statement: 'select format_datetime(\"%A\", DATETIME(Date'1985-01-12'))'"),
             one("meta::pure::functions::tests::date::testDayOfWeek_Relation_Function_1__Boolean_1_", "\"[unsupported-api] The function 'toString' (state: [Select, false]) is not supported yet\""),
 
+            //formatDate
+            one("meta::pure::functions::date::tests::formatDate::testFormatDate_ISO8601_Function_1__Boolean_1_", "[ERROR] function format_timestamp(unknown, timestamp with time zone) does not exist\nHint: No function matches the given name and argument types. You might need to add explicit type casts. - Statement: 'select FORMAT_TIMESTAMP('%Y-%m-%dT%H:%M:%E9S',CAST(Timestamp'2024-08-15 15:33:42' AS TIMESTAMPTZ))'"),
+
             //zscore
             one("meta::pure::functions::math::tests::zScore::testZScore_Function_1__Boolean_1_", "[unsupported-api] Window Columns not supported for Database Type: Spanner"),
             one("meta::pure::functions::math::tests::zScore::testZScore_FloatValues_Function_1__Boolean_1_", "\"[unsupported-api] Window Columns not supported for Database Type: Spanner\""),
