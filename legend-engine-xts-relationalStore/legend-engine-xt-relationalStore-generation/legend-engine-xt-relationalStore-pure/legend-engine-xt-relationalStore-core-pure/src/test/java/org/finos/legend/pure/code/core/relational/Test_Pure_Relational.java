9cd359cf98e0e83e63462986e90b6a3ca9dd9509
ahallgs <Andrew.X.Hall@ny.email.gs.com>
2026-05-08 12:43:45 -0500

    Move failure message to processSemiStructuredArrayFirst

--- legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-generation/legend-engine-xt-relationalStore-pure/legend-engine-xt-relationalStore-core-pure/src/test/java/org/finos/legend/pure/code/core/relational/Test_Pure_Relational.java
+++ legend-engine-xts-relationalStore/legend-engine-xt-relationalStore-generation/legend-engine-xt-relationalStore-pure/legend-engine-xt-relationalStore-core-pure/src/test/java/org/finos/legend/pure/code/core/relational/Test_Pure_Relational.java
@@ -19,7 +19,6 @@ import org.finos.legend.pure.m3.execution.test.PureTestBuilder;
 import org.finos.legend.pure.runtime.java.compiled.testHelper.PureTestBuilderCompiled;
 import org.finos.legend.pure.m3.execution.test.TestCollection;
 import org.finos.legend.pure.runtime.java.compiled.execution.CompiledExecutionSupport;
-import org.junit.Ignore;
 
 public class Test_Pure_Relational
 {
