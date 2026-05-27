7630640857e9bc8d1796a7a9808dcb35ae56f363
Shah, Sahil A [Engineering] <Sahil.Shah@ln.email.gs.com>
2026-05-27 07:23:01 -0400

    propogate compile function from current compile context for DQ lambda generations

--- legend-engine-xts-dataquality/legend-engine-xt-dataquality-generation/src/main/java/org/finos/legend/engine/generation/dataquality/DataQualityProfilingLambdaGenerator.java
+++ legend-engine-xts-dataquality/legend-engine-xt-dataquality-generation/src/main/java/org/finos/legend/engine/generation/dataquality/DataQualityProfilingLambdaGenerator.java
@@ -14,12 +14,18 @@
 
 package org.finos.legend.engine.generation.dataquality;
 
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.HelperValueSpecificationBuilder;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.language.pure.grammar.from.PureGrammarParser;
 import org.finos.legend.engine.shared.core.operational.errorManagement.EngineException;
 import org.finos.legend.engine.shared.core.operational.errorManagement.ExceptionCategory;
+import org.finos.legend.pure.generated.PureCompiledLambda;
 import org.finos.legend.pure.generated.Root_meta_external_dataquality_DataQualityRelationValidation;
 import org.finos.legend.pure.generated.core_dataquality_generation_dataprofile;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.PackageableElement;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.LambdaFunction;
+import org.finos.legend.pure.m3.execution.ExecutionSupport;
+import org.finos.legend.pure.runtime.java.compiled.generation.processors.support.function.DefaultPureLambdaFunction1;
 
 public class DataQualityProfilingLambdaGenerator
 {
@@ -42,6 +48,19 @@ public class DataQualityProfilingLambdaGenerator
 
     private static org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.LambdaFunction<Object> generateDataProfileLambda(PureModel pureModel, Root_meta_external_dataquality_DataQualityRelationValidation packageableElement, boolean excludePlatformColumns)
     {
-        return (org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.LambdaFunction<Object>) core_dataquality_generation_dataprofile.Root_meta_external_dataquality_dataprofile_getProfilingLambda_DataQualityRelationValidation_1__Boolean_1__Boolean_1__LambdaFunction_1_(packageableElement, true, excludePlatformColumns, pureModel.getExecutionSupport());
+        return (org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.LambdaFunction<Object>) core_dataquality_generation_dataprofile.Root_meta_external_dataquality_dataprofile_getProfilingLambda_DataQualityRelationValidation_1__Boolean_1__Boolean_1__Function_1__LambdaFunction_1_(packageableElement, true, excludePlatformColumns, getLambdaCompiler(pureModel), pureModel.getExecutionSupport());
+    }
+
+    public static org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.Function<?> getLambdaCompiler(PureModel pureModel)
+    {
+        PureCompiledLambda stringLambdaCompiler = new PureCompiledLambda(pureModel.getExecutionSupport(), "", new DefaultPureLambdaFunction1<String, LambdaFunction<? extends Object>>()
+        {
+            @Override
+            public org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.function.LambdaFunction<? extends Object> value(String code, ExecutionSupport executionSupport)
+            {
+                return HelperValueSpecificationBuilder.buildLambda(PureGrammarParser.newInstance().parseLambda(code), pureModel.getContext());
+            }
+        });
+        return core_dataquality_generation_dataprofile.Root_meta_external_dataquality_dataprofile_getLambdaCompiler_Function_1__Function_1_(stringLambdaCompiler, pureModel.getExecutionSupport());
     }
 }
