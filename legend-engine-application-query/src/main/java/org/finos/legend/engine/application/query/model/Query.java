5bb4eff9f6bbb94aab4796be5ab245bb86ca6ed9
gs-gunjan <72594207+gs-gunjan@users.noreply.github.com>
2026-06-04 14:24:06 +0530

    cleaning up query model for deprecated fields (#4691)

--- legend-engine-application-query/src/main/java/org/finos/legend/engine/application/query/model/Query.java
+++ legend-engine-application-query/src/main/java/org/finos/legend/engine/application/query/model/Query.java
@@ -31,12 +31,6 @@ public class Query
     public String artifactId;
     public String versionId;
     public String originalVersionId;
-    // TODO: remove once migration completes
-    // keeping this only for backward compatibility
-    @Deprecated
-    public String mapping;
-    @Deprecated
-    public String runtime;
     public QueryExecutionContext executionContext;
     public String content;
     public Long lastUpdatedAt;
