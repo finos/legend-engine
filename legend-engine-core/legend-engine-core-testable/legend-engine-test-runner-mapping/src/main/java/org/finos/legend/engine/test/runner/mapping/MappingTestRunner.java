9e66dce32031ec74e4b794177183ec64445ae125
Jake Kim <60619826+jake-kim1@users.noreply.github.com>
2026-08-19 14:48:01 -0400

    Move simple element resolution of post processors to second pass of connection processor (#5089)

--- legend-engine-core/legend-engine-core-testable/legend-engine-test-runner-mapping/src/main/java/org/finos/legend/engine/test/runner/mapping/MappingTestRunner.java
+++ legend-engine-core/legend-engine-core-testable/legend-engine-test-runner-mapping/src/main/java/org/finos/legend/engine/test/runner/mapping/MappingTestRunner.java
@@ -23,6 +23,7 @@ import org.eclipse.collections.api.RichIterable;
 import org.eclipse.collections.api.list.MutableList;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.CompileContext;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.ConnectionFirstPassBuilder;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.ConnectionSecondPassBuilder;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.HelperValueSpecificationBuilder;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
 import org.finos.legend.engine.plan.execution.PlanExecutor;
@@ -101,9 +102,11 @@ public class MappingTestRunner
         {
             CompileContext context = this.pureModel.getContext();
             Store element = getStore(conn.element, conn.elementSourceInformation, context);
+            Root_meta_core_runtime_Connection pureConnection = conn.accept(connectionVisitor);
+            conn.accept(new ConnectionSecondPassBuilder(context, pureConnection));
             Root_meta_core_runtime_ConnectionStore connectionStore =
                     new Root_meta_core_runtime_ConnectionStore_Impl("")
-                            ._connection(conn.accept(connectionVisitor))
+                            ._connection(pureConnection)
                             ._element(element);
             this.runtime._connectionStoresAdd(connectionStore);
         });
